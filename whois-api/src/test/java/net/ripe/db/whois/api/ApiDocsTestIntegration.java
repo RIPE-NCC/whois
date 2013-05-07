e257b46c5c7953740ee1f03ce784604162a35d79
Ed Shryane <eshryane@ripe.net>
2013-05-07 16:22:19 +0200

    Allow aliased resources in embedded Jetty (allows loading API HTML files from symlinked directory).

--- whois-api/src/test/java/net/ripe/db/whois/api/ApiDocsTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/ApiDocsTestIntegration.java
@@ -1,21 +1,19 @@
 package net.ripe.db.whois.api;
 
-import com.jayway.awaitility.Awaitility;
 import net.ripe.db.whois.api.httpserver.Audience;
 import net.ripe.db.whois.common.IntegrationTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import java.util.concurrent.Callable;
-
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 @Category(IntegrationTest.class)
 public class ApiDocsTestIntegration extends AbstractRestClientTest {
     @Test
     public void checkInternalIndex() throws Exception {
-        final String index = get(Audience.INTERNAL, "");
+        final String index = createStaticResource(Audience.INTERNAL, "/").get(String.class);
 
         assertThat(index, containsString("<html"));
         assertThat(index, containsString("<title>RIPE WHOIS API</title>"));
@@ -27,7 +25,7 @@ public class ApiDocsTestIntegration extends AbstractRestClientTest {
 
     @Test
     public void checkExternalIndex() throws Exception {
-        final String index = get(Audience.PUBLIC, "");
+        final String index = createStaticResource(Audience.PUBLIC, "/").get(String.class);
 
         assertThat(index, containsString("<html"));
         assertThat(index, containsString("<title>RIPE WHOIS API</title>"));
@@ -35,22 +33,5 @@ public class ApiDocsTestIntegration extends AbstractRestClientTest {
         assertThat(index, not(containsString(">/logs/current<")));
         assertThat(index, containsString(">/metadata/sources<"));
         assertThat(index, containsString(">/delete/{source}/{objectType}/{key}<"));
-    }
-
-    private String get(final Audience audience, final String path) {
-        final StringBuffer result = new StringBuffer();
-        Awaitility.await().until(new Callable<Boolean>() {
-            @Override
-            public Boolean call() throws Exception {
-                try {
-                    final String index = createStaticResource(audience, path).get(String.class);
-                    result.append(index);
-                    return true;
-                } catch (Exception e) {
-                    return false;
-                }
-            }
-        }, is(true));
-        return result.toString();
     }
 }
