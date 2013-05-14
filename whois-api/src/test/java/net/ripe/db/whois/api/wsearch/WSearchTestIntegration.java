4ec2dffe92e89eb7f42b44857efd45e3f6bb18db
Angela Sjöholm <asjoholm@ripe.net>
2013-05-14 13:46:06 +0200

    integration test for #31 WSearch not possible to search on ip ranges

--- whois-api/src/test/java/net/ripe/db/whois/api/wsearch/WSearchTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/wsearch/WSearchTestIntegration.java
@@ -2,6 +2,8 @@ package net.ripe.db.whois.api.wsearch;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 import net.ripe.db.whois.api.AbstractIntegrationTest;
 import net.ripe.db.whois.api.httpserver.Audience;
 import net.ripe.db.whois.common.IntegrationTest;
@@ -9,6 +11,7 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -28,15 +31,21 @@ public class WSearchTestIntegration extends AbstractIntegrationTest {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormat.forPattern("yyyyMMdd");
     private static final DateTimeFormatter TIME_FORMAT = DateTimeFormat.forPattern("HHmmss");
     private static final String INPUT_FILE_NAME = "001.msg-in.txt.gz";
+    private Client client;
 
     @Autowired
     private LogFileIndex logFileIndex;
 
     @BeforeClass
-    public static void setup() throws IOException {
+    public static void setupClass() throws IOException {
         System.setProperty("api.key", "DB-RIPE-ZwBAFuR5JuBxQCnQ");
         System.setProperty("dir.wsearch.index", INDEX_DIR.getAbsolutePath() + "index");
         System.setProperty("dir.update.audit.log", LOG_DIR.getAbsolutePath());
+    }
+
+    @Before
+    public void setup() {
+        client = Client.create(new DefaultClientConfig());
     }
 
     @AfterClass
@@ -61,7 +70,9 @@ public class WSearchTestIntegration extends AbstractIntegrationTest {
     }
 
     private String doWSearch(final String searchTerm) throws IOException {
-        return doGetRequest(String.format("http://localhost:%s/api/logs?search=%s&date=&apiKey=DB-RIPE-ZwBAFuR5JuBxQCnQ", getPort(Audience.INTERNAL), URLEncoder.encode(searchTerm, "ISO-8859-1")), 200);
+        return client
+                .resource(String.format("http://localhost:%s/api/logs?search=%s&date=&apiKey=DB-RIPE-ZwBAFuR5JuBxQCnQ", getPort(Audience.INTERNAL), URLEncoder.encode(searchTerm, "ISO-8859-1")))
+                .get(String.class);
     }
 
     private void createLogFile(final String data) throws IOException {
