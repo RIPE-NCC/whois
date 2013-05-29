9a9642a28e2fc184152edab6b189463b6c686297
Andre Kampert <andre@cadimira.com>
2013-05-29 17:16:06 +0200

    More integration tests for --resource query

--- whois-db/src/test/groovy/spec/integration/GrsIntegrationSpec.groovy
+++ whois-db/src/test/groovy/spec/integration/GrsIntegrationSpec.groovy
@@ -2,6 +2,7 @@ package spec.integration
 
 import net.ripe.db.whois.common.IntegrationTest
 import net.ripe.db.whois.common.dao.jdbc.DatabaseHelper
+import net.ripe.db.whois.common.source.Source
 
 @org.junit.experimental.categories.Category(IntegrationTest.class)
 class GrsIntegrationSpec extends BaseSpec {
@@ -22,7 +23,7 @@ class GrsIntegrationSpec extends BaseSpec {
         }
     }
 
-    def "query sources"() {
+    def "query --list-sources"() {
       when:
         def response = query("--list-sources")
 
@@ -32,5 +33,53 @@ class GrsIntegrationSpec extends BaseSpec {
         response =~ "1-GRS"
         response =~ "2-GRS"
         response =~ "3-GRS"
+    }
+
+    def "query --resource AS10 match in GRS"() {
+      when:
+        addObjectForSource("1-GRS", "aut-num: AS1")
+        addObjectForSource("2-GRS", "aut-num: AS10")
+        addObjectForSource("3-GRS", "aut-num: AS100")
+        addObjectForSource("TEST", "aut-num: AS1000")
+
+        def response = query("--resource AS10")
+      then:
+        response =~ "aut-num:        AS10"
+        response != ~"No entries found"
+    }
+
+    def "query --resource AS10 match in 2 GRS sources"() {
+      when:
+        addObjectForSource("1-GRS", "aut-num: AS10")
+        addObjectForSource("2-GRS", "aut-num: AS10")
+        addObjectForSource("3-GRS", "aut-num: AS100")
+        addObjectForSource("TEST", "aut-num: AS1000")
+
+        def response = query("--resource AS10")
+      then:
+        response =~ "aut-num:        AS10"
+        response != ~"No entries found"
+    }
+
+    def "query --resource AS1000 match in alias, not in resource data"() {
+      when:
+        addObjectForSource("1-GRS", "aut-num: AS1")
+        addObjectForSource("2-GRS", "aut-num: AS10")
+        addObjectForSource("3-GRS", "aut-num: AS100")
+        addObjectForSource("TEST", "aut-num: AS1000")
+
+        def response = query("--resource AS1000")
+      then:
+        response != ~"aut-num:        AS1000"
+        response =~ "No entries found"
+    }
+
+    private def addObjectForSource(String sourceName, String rpslObject) {
+        try {
+            whoisFixture.sourceContext.current = Source.master(sourceName)
+            databaseHelper.addObject(rpslObject)
+        } finally {
+            whoisFixture.sourceContext.removeCurrentSource()
+        }
     }
 }
