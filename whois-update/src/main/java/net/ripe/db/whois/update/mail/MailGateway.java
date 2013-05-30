75cde6a6441fbaff6fbb5b46c29ee321ebffbd5e
Agoston Horvath <agoston@ripe.net>
2013-05-30 15:28:28 +0200

    removed unnecessary annotations from interface

--- whois-update/src/main/java/net/ripe/db/whois/update/mail/MailGateway.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/mail/MailGateway.java
@@ -1,13 +1,9 @@
 package net.ripe.db.whois.update.mail;
 
-import net.ripe.db.whois.common.aspects.RetryFor;
 import net.ripe.db.whois.update.domain.ResponseMessage;
-import org.springframework.mail.MailSendException;
 
 public interface MailGateway {
-    @RetryFor(value = MailSendException.class, attempts = 20, intervalMs = 10000)
     void sendEmail(String to, ResponseMessage responseMessage);
 
-    @RetryFor(value = MailSendException.class, attempts = 20, intervalMs = 10000)
     void sendEmail(String to, String subject, String text);
 }
