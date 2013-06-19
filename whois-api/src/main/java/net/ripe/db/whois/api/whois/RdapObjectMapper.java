a322dbfd79db813f1c9f2ab7827c58d6a9d2a8d0
inazumax <andrew@apnic.net>
2013-06-19 15:44:06 +1000

    getting closer to a solution. need to go looking at how the streaming marshaller works coz it is breaking the output.

--- whois-api/src/main/java/net/ripe/db/whois/api/whois/RdapObjectMapper.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/whois/RdapObjectMapper.java
@@ -1,10 +1,6 @@
 package net.ripe.db.whois.api.whois;
 
-import com.google.common.collect.Sets;
 import net.ripe.db.whois.api.whois.domain.RdapObject;
-import net.ripe.db.whois.common.domain.CIString;
-import net.ripe.db.whois.common.rpsl.AttributeType;
-import net.ripe.db.whois.common.rpsl.ObjectTemplate;
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.common.rpsl.RpslAttribute;
 import net.ripe.db.whois.common.rpsl.RpslObject;
@@ -50,12 +46,10 @@ public class RdapObjectMapper {
         ObjectType rpslObjectType = rpslObject.getType();
 
         if (rpslObjectType.getName().equals(ObjectType.PERSON.getName())) {
-            RpslAttribute primaryKey = getPrimaryKey(rpslObject);
-            CIString handle = CIString.ciString(stripWhitespace(primaryKey.getValue()));
 
             debug(rpslObject);
 
-            rdapObject.setHandle(handle);
+            rdapObject.setHandle(rpslObject.getKey());
 
         } else if (rpslObjectType.equals(ObjectType.ORGANISATION.getName())) {
 
@@ -64,19 +58,6 @@ public class RdapObjectMapper {
         } else if (rpslObjectType.equals(ObjectType.IRT.getName())) {
 
         }
-    }
-
-    private static RpslAttribute getPrimaryKey(final RpslObject rpslObject) {
-        final ObjectTemplate objectTemplate = ObjectTemplate.getTemplate(rpslObject.getType());
-        Iterator<AttributeType> iterator = Sets.intersection(objectTemplate.getKeyAttributes(), objectTemplate.getLookupAttributes()).iterator();
-        if (iterator.hasNext()) {
-            AttributeType primaryKey = iterator.next();
-            if (!iterator.hasNext()) {
-                return rpslObject.findAttribute(primaryKey);
-            }
-        }
-
-        throw new IllegalArgumentException("Couldn't find primary key attribute for type: " + rpslObject.getType());
     }
 
     private void debug (RpslObject rpslObject) {
@@ -88,9 +69,5 @@ public class RdapObjectMapper {
             ra = iter.next();
             System.out.println(ra.getKey() + ":" + ra.getValue());
         }
-    }
-
-    private String stripWhitespace (String str) {
-         return str.replace(" ","");
     }
 }
