b05e73ea388f8086942d5985b5e0f4d92fac8bcd
Angela Sjöholm <asjoholm@ripe.net>
2013-07-04 12:45:35 +0200

    cleanup of RdapResponseJsonTest

--- whois-api/src/test/java/net/ripe/db/whois/api/whois/rdap/RdapResponseJsonTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/whois/rdap/RdapResponseJsonTest.java
@@ -1,17 +1,11 @@
 package net.ripe.db.whois.api.whois.rdap;
 
 import com.Ostermiller.util.LineEnds;
-import com.google.common.collect.Maps;
 import net.ripe.db.whois.api.whois.StreamingMarshal;
-import net.ripe.db.whois.api.whois.rdap.domain.Entity;
-import net.ripe.db.whois.api.whois.rdap.domain.Event;
-import net.ripe.db.whois.api.whois.rdap.domain.Ip;
-import net.ripe.db.whois.api.whois.rdap.domain.Link;
-import net.ripe.db.whois.api.whois.rdap.domain.Nameserver;
-import net.ripe.db.whois.api.whois.rdap.domain.Notice;
-import net.ripe.db.whois.api.whois.rdap.domain.Remark;
+import net.ripe.db.whois.api.whois.rdap.domain.*;
 import org.codehaus.plexus.util.StringInputStream;
 import org.codehaus.plexus.util.StringOutputStream;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,18 +14,21 @@ import javax.xml.datatype.DatatypeFactory;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import static com.google.common.collect.Maps.immutableEntry;
+import static net.ripe.db.whois.api.whois.rdap.VcardObjectHelper.createHashMap;
 import static org.junit.Assert.assertEquals;
 
 public class RdapResponseJsonTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(RdapResponseJsonTest.class);
 
-    private static DatatypeFactory dataTypeFactory;
-    static {
+    private DatatypeFactory dataTypeFactory;
+
+    @Before
+    public void setup() {
         try {
             dataTypeFactory = DatatypeFactory.newInstance();
         } catch (Exception ex) {
@@ -41,10 +38,8 @@ public class RdapResponseJsonTest {
 
     @Test
     public void entity_vcard_serialization_test() throws Exception {
-        VcardObjectHelper.VcardBuilder builder = new VcardObjectHelper.VcardBuilder();
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-        String date = sdf.format(new Date(1372214924859L));
+        final VcardObjectHelper.VcardBuilder builder = new VcardObjectHelper.VcardBuilder();
+        final String date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(new Date(1372214924859L));
 
         builder.setVersion()
                 .setFn("Joe User")
@@ -53,30 +48,29 @@ public class RdapResponseJsonTest {
                 .setAnniversary(date)
                 .setGender("M")
                 .setKind("individual")
-                .addLang(VcardObjectHelper.createHashMap(Maps.immutableEntry("pref", "1")), "fr")
-                .addLang(VcardObjectHelper.createHashMap(Maps.immutableEntry("pref", "2")), "en")
+                .addLang(createHashMap(immutableEntry("pref", "1")), "fr")
+                .addLang(createHashMap(immutableEntry("pref", "2")), "en")
                 .setOrg("Example")
                 .setTitle("Research Scientist")
                 .setRole("Project Lead")
-                .addAdr(VcardObjectHelper.createHashMap(Maps.immutableEntry("type", "work")), builder.createAdrEntryValueType("",
+                .addAdr(createHashMap(immutableEntry("type", "work")), builder.createAdrEntryValueType("",
                         "Suite 1234",
                         "4321 Rue Somewhere",
                         "Quebec",
                         "QC",
                         "G1V 2M2",
                         "Canada"))
-                .addAdr(VcardObjectHelper.createHashMap(Maps.immutableEntry("pref", "1")), null)
-                .addTel(VcardObjectHelper.createHashMap(Maps.immutableEntry("type", new String[]{"work", "voice"})), "tel:+1-555-555-1234;ext=102")
-                .addTel(VcardObjectHelper.createHashMap(Maps.immutableEntry("type", new String[]{"work", "cell", "voice", "video", "text"})), "tel:+1-555-555-4321")
-                .setEmail(VcardObjectHelper.createHashMap(Maps.immutableEntry("type", "work")), "joe.user@example.com")
-                .setGeo(VcardObjectHelper.createHashMap(Maps.immutableEntry("type", "work")), "geo:46.772673,-71.282945")
-                .setKey(VcardObjectHelper.createHashMap(Maps.immutableEntry("type", "work")), "http://www.example.com/joe.user/joe.asc")
+                .addAdr(createHashMap(immutableEntry("pref", "1")), null)
+                .addTel(createHashMap(immutableEntry("type", new String[]{"work", "voice"})), "tel:+1-555-555-1234;ext=102")
+                .addTel(createHashMap(immutableEntry("type", new String[]{"work", "cell", "voice", "video", "text"})), "tel:+1-555-555-4321")
+                .setEmail(createHashMap(immutableEntry("type", "work")), "joe.user@example.com")
+                .setGeo(createHashMap(immutableEntry("type", "work")), "geo:46.772673,-71.282945")
+                .setKey(createHashMap(immutableEntry("type", "work")), "http://www.example.com/joe.user/joe.asc")
                 .setTz("-05:00")
-                .setUrl(VcardObjectHelper.createHashMap(Maps.immutableEntry("type", "work")), "http://example.org");
+                .setUrl(createHashMap(immutableEntry("type", "work")), "http://example.org");
 
-        List<Object> objs = builder.build();
-        StringOutputStream serializer = streamObject(objs);
-        String result = convertEOLToUnix(serializer);
+        final List<Object> objects = builder.build();
+        final String result = convertEOLToUnix(streamObject(objects));
 
         assertEquals("" +
                 "[ \"vcard\", [ [ \"version\", {\n" +
@@ -115,19 +109,20 @@ public class RdapResponseJsonTest {
 
     @Test
     public void nameserver_serialization_test() throws Exception {
-        Nameserver nameserver = new Nameserver();
+        final Nameserver nameserver = new Nameserver();
         nameserver.setHandle("handle");
         nameserver.setLdhName("ns1.xn--fo-5ja.example");
         nameserver.setUnicodeName("foo.example");
         nameserver.getStatus().add("active");
-        Nameserver.IpAddresses ipAddresses = new Nameserver.IpAddresses();
+
+        final Nameserver.IpAddresses ipAddresses = new Nameserver.IpAddresses();
         ipAddresses.getIpv4().add("192.0.2.1");
         ipAddresses.getIpv4().add("192.0.2.2");
         ipAddresses.getIpv6().add("2001:db8::123");
         nameserver.setIpAddresses(ipAddresses);
 
-        List<String> remarkList = new ArrayList<>();
-        Remark remarks1 = new Remark();
+        final List<String> remarkList = new ArrayList<>();
+        final Remark remarks1 = new Remark();
         remarkList.add("She sells sea shells down by the sea shore.");
         remarkList.add("Originally written by Terry Sullivan.");
 
@@ -135,7 +130,7 @@ public class RdapResponseJsonTest {
         nameserver.getRemarks().add(remarks1);
 
 
-        Link link = new Link();
+        final Link link = new Link();
         link.setHref("http://example.net/nameserver/xxxx");
         link.setValue("http://example.net/nameserver/xxxx");
         link.setRel("self");
@@ -143,25 +138,24 @@ public class RdapResponseJsonTest {
 
         nameserver.setPort43("whois.example.net");
 
-        Event event1 = new Event();
-        event1.setEventAction("registration");
+        final Event registrationEvent = new Event();
+        registrationEvent.setEventAction("registration");
 
-        GregorianCalendar gc = new GregorianCalendar();
-        gc.setTimeInMillis(1372214924859L);
+        final GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(1372214924859L);
 
-        event1.setEventDate(dataTypeFactory.newXMLGregorianCalendar(gc));
+        registrationEvent.setEventDate(dataTypeFactory.newXMLGregorianCalendar(calendar));
 
-        nameserver.getEvents().add(event1);
+        nameserver.getEvents().add(registrationEvent);
 
-        Event event2 = new Event();
-        event2.setEventAction("last changed");
-        event2.setEventDate(dataTypeFactory.newXMLGregorianCalendar(gc));
+        final Event lastChangedEvent = new Event();
+        lastChangedEvent.setEventAction("last changed");
+        lastChangedEvent.setEventDate(dataTypeFactory.newXMLGregorianCalendar(calendar));
 
-        event2.setEventActor("joe@example.com");
-        nameserver.getEvents().add(event2);
+        lastChangedEvent.setEventActor("joe@example.com");
+        nameserver.getEvents().add(lastChangedEvent);
 
-        StringOutputStream serializer = streamObject(nameserver);
-        String result = convertEOLToUnix(serializer);
+        final String result = convertEOLToUnix(streamObject(nameserver));
 
         assertEquals("" +
                 "{\n" +
@@ -195,7 +189,7 @@ public class RdapResponseJsonTest {
 
     @Test
     public void ip_serialization_test() throws Exception {
-        Ip ip = new Ip();
+        final Ip ip = new Ip();
         ip.setHandle("XXXX-RIR");
         ip.setParentHandle("YYYY-RIR");
         ip.setStartAddress("2001:db8::0");
@@ -206,61 +200,61 @@ public class RdapResponseJsonTest {
         ip.setCountry("AU");
         ip.getStatus().add("allocated");
 
-        List<String> remarkList = new ArrayList<>();
-        Remark remarks1 = new Remark();
+        final List<String> remarkList = new ArrayList<>();
+        final Remark remark = new Remark();
         remarkList.add("She sells sea shells down by the sea shore.");
         remarkList.add("Originally written by Terry Sullivan.");
 
-        remarks1.getDescription().addAll(remarkList);
-        ip.getRemarks().add(remarks1);
+        remark.getDescription().addAll(remarkList);
+        ip.getRemarks().add(remark);
 
 
-        Link link = new Link();
+        final Link link = new Link();
         link.setHref("http://example.net/ip/2001:db8::/48");
         link.setValue("http://example.net/ip/2001:db8::/48");
         link.setRel("self");
         ip.getLinks().add(link);
 
-        Link uplink = new Link();
+        final Link uplink = new Link();
         uplink.setHref("http://example.net/ip/2001:C00::/23");
         uplink.setValue("http://example.net/ip/2001:db8::/48");
         uplink.setRel("up");
         ip.getLinks().add(uplink);
 
-        Event event1 = new Event();
-        event1.setEventAction("registration");
+        final Event registrationEvent = new Event();
+        registrationEvent.setEventAction("registration");
 
-        GregorianCalendar gc = new GregorianCalendar();
-        gc.setTimeInMillis(1372214924859L);
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(1372214924859L);
 
-        event1.setEventDate(dataTypeFactory.newXMLGregorianCalendar(gc));
+        registrationEvent.setEventDate(dataTypeFactory.newXMLGregorianCalendar(calendar));
 
-        ip.getEvents().add(event1);
+        ip.getEvents().add(registrationEvent);
 
-        Event event2 = new Event();
-        event2.setEventAction("last changed");
-        event2.setEventDate(dataTypeFactory.newXMLGregorianCalendar(gc));
-        event2.setEventActor("joe@example.com");
-        ip.getEvents().add(event2);
+        final Event lastChangedEvent = new Event();
+        lastChangedEvent.setEventAction("last changed");
+        lastChangedEvent.setEventDate(dataTypeFactory.newXMLGregorianCalendar(calendar));
+        lastChangedEvent.setEventActor("joe@example.com");
+        ip.getEvents().add(lastChangedEvent);
 
-        Entity entity = new Entity();
+        final Entity entity = new Entity();
         entity.setHandle("XXXX");
         entity.getRoles().add("registrant");
 
-        entity.getRemarks().add(remarks1);
+        entity.getRemarks().add(remark);
 
-        entity.getEvents().add(event1);
-        entity.getEvents().add(event2);
+        entity.getEvents().add(registrationEvent);
+        entity.getEvents().add(lastChangedEvent);
 
         ip.getEntities().add(entity);
 
-        Link entityLink = new Link();
+        final Link entityLink = new Link();
         entityLink.setHref("http://example.net/entity/xxxx");
         entityLink.setValue("http://example.net/entity/xxxx");
         entityLink.setRel("self");
         entity.getLinks().add(entityLink);
 
-        VcardObjectHelper.VcardBuilder builder = new VcardObjectHelper.VcardBuilder();
+        final VcardObjectHelper.VcardBuilder builder = new VcardObjectHelper.VcardBuilder();
 
         builder.setVersion()
                 .setFn("Joe User")
@@ -280,8 +274,7 @@ public class RdapResponseJsonTest {
 
         entity.setVcardArray(builder.build());
 
-        StringOutputStream serializer = streamObject(ip);
-        String result = convertEOLToUnix(serializer);
+        final String result = convertEOLToUnix(streamObject(ip));
 
         assertEquals("" +
                 "{\n" +
@@ -348,16 +341,14 @@ public class RdapResponseJsonTest {
     }
 
 
-
     @Test
     public void notices_serialization_test() throws Exception {
-
-        Notice notices = new Notice();
+        final Notice notices = new Notice();
         notices.setTitle("Beverage policy");
         notices.getDescription().add("Beverages with caffeine for keeping horses awake.");
         notices.getDescription().add("Very effective.");
 
-        Link link = new Link();
+        final Link link = new Link();
         link.setValue("http://example.com/context_uri");
         link.setRel("self");
         link.setHref("http://example.com/target_uri_href");
@@ -369,8 +360,7 @@ public class RdapResponseJsonTest {
         link.setType("application/json");
         notices.setLinks(link);
 
-        StringOutputStream serializer = streamObject(notices);
-        String result = convertEOLToUnix(serializer);
+        String result = convertEOLToUnix(streamObject(notices));
 
         assertEquals("" +
                 "{\n" +
@@ -390,19 +380,21 @@ public class RdapResponseJsonTest {
 
 
     private StringOutputStream streamObject(Object o) {
-        StreamingMarshal streamingMarshal = new RdapStreamingMarshalJson();
-        StringOutputStream serializer = new StringOutputStream();
+        final StreamingMarshal streamingMarshal = new RdapStreamingMarshalJson();
+        final StringOutputStream serializer = new StringOutputStream();
+
         streamingMarshal.open(serializer);
         streamingMarshal.start("");
         streamingMarshal.writeObject(o);
         streamingMarshal.close();
+
         return serializer;
     }
 
     private String convertEOLToUnix(StringOutputStream serializer) throws IOException {
-        StringOutputStream resultStream = new StringOutputStream();
+        final StringOutputStream resultStream = new StringOutputStream();
         LineEnds.convert(new StringInputStream(serializer.toString()), resultStream, LineEnds.STYLE_UNIX);
+
         return resultStream.toString();
     }
-
 }
