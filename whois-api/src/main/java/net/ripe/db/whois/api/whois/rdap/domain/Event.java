456b2f470187c57d048cf63b78eab62deac1599d
Ed Shryane <eshryane@ripe.net>
2013-07-26 14:37:11 +0200

    Event - eventAction and eventDate are required, eventActor is optional.

--- whois-api/src/main/java/net/ripe/db/whois/api/whois/rdap/domain/Event.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/whois/rdap/domain/Event.java
@@ -5,6 +5,7 @@ import org.joda.time.LocalDateTime;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
@@ -18,10 +19,13 @@ import java.io.Serializable;
 })
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
 public class Event implements Serializable {
+    @XmlElement(required = true)
     protected Action eventAction;
     @XmlSchemaType(name = "dateTime")
     @XmlJavaTypeAdapter(DateTimeAdapter.class)
+    @XmlElement(required = true)
     protected LocalDateTime eventDate;
+    @XmlElement(required = false)
     protected String eventActor;
 
     public Action getEventAction() {
