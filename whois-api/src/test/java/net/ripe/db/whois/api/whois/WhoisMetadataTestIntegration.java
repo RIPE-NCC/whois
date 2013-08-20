b26e66e96f3dd67747049ffa34c166fee4d7266e
Ed Shryane <eshryane@ripe.net>
2013-08-20 15:23:12 +0200

    Added tests for metadata JSON response.

--- whois-api/src/test/java/net/ripe/db/whois/api/whois/WhoisMetadataTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/whois/WhoisMetadataTestIntegration.java
@@ -10,13 +10,14 @@ import java.net.HttpURLConnection;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 @Category(IntegrationTest.class)
 public class WhoisMetadataTestIntegration extends AbstractIntegrationTest {
 
     @Test
-    public void getTemplate() throws Exception {
+    public void getTemplateXml() throws Exception {
         final String s = doGetRequest(getUrl("templates/peering-set.xml"), HttpURLConnection.HTTP_OK);
         assertThat("has service", s, containsString("service=\"getObjectTemplate\""));
         assertThat("has key", s, containsString("<attribute name=\"peering-set\" requirement=\"MANDATORY\" cardinality=\"SINGLE\" keys=\"PRIMARY_KEY LOOKUP_KEY\"/>"));
@@ -24,6 +25,21 @@ public class WhoisMetadataTestIntegration extends AbstractIntegrationTest {
         assertThat("has source", s, containsString("<source id=\"ripe\"/>"));
         assertThat("has template", s, containsString("<template type=\"peering-set\">"));
     }
+
+    @Test
+    public void getTemplateJson() throws Exception {
+        final String s = doGetRequest(getUrl("templates/peering-set.json"), HttpURLConnection.HTTP_OK);
+
+        assertThat(s, containsString("\"service\" : \"getObjectTemplate\""));
+        assertThat(s, containsString("/metadata/templates/peering-set"));
+        assertThat(s, containsString("\"type\" : \"peering-set\""));
+        assertThat(s, containsString("\"name\" : \"peering-set\""));
+        assertThat(s, containsString("\"requirement\" : \"MANDATORY\""));
+        assertThat(s, containsString("\"cardinality\" : \"SINGLE\""));
+        assertThat(s, containsString("\"keys\" : [ \"PRIMARY_KEY\", \"LOOKUP_KEY\" ]"));
+        assertThat(s, not(containsString("template-resources")));
+    }
+
 
     @Test
     public void getTemplateDefaultsToXml() throws Exception {
@@ -43,14 +59,24 @@ public class WhoisMetadataTestIntegration extends AbstractIntegrationTest {
     }
 
     @Test
-    public void getSource() throws Exception {
+    public void getSourcesXml() throws Exception {
         final String s = doGetRequest(getUrl("sources.xml"), HttpURLConnection.HTTP_OK);
-        System.err.println(s);
+
         assertThat("has service", s, containsString("service=\"getSupportedDataSources\""));
         assertThat("has source", s, containsString("<source name=\"RIPE\" id=\"ripe\"/>"));
         assertThat("has grs-source", s, containsString("<source name=\"TEST-GRS\" id=\"test-grs\" grs-id=\"test-grs\"/>"));
         assertThat("has sources", s, containsString("<sources>"));
         assertThat("has grs-sources", s, containsString("<grs-sources>"));
+    }
+
+    @Test
+    public void getSourcesJson() throws Exception {
+        final String s = doGetRequest(getUrl("sources.json"), HttpURLConnection.HTTP_OK);
+
+        assertThat(s, containsString("\"name\" : \"RIPE\""));
+        assertThat(s, containsString("\"name\" : \"TEST\""));
+        assertThat(s, containsString("\"name\" : \"TEST-GRS\""));
+        assertThat(s, not(containsString("whois-resources")));
     }
 
     private String getUrl(final String command) {
