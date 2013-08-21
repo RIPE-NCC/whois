c3034ad0464c5dd5b1d2d5d38f51a3aab186b9d9
Ed Shryane <eshryane@ripe.net>
2013-08-21 18:34:13 +0200

    Fixed test.

--- whois-api/src/test/java/net/ripe/db/whois/api/whois/WhoisObjectMapperTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/whois/WhoisObjectMapperTest.java
@@ -78,9 +78,9 @@ public class WhoisObjectMapperTest {
                 new Attribute("admin-c", "TP1-TEST", null, "person", new Link("locator", "http://rest.db.ripe.net/lookup/test/person/TP1-TEST")),
                 new Attribute("upd-to", "dbtest@ripe.net", null, null, null),
                 new Attribute("auth", "MD5-PW", "Filtered", null, null),
-                new Attribute("auth", "PGPKEY-28F6CD6C", null, "key-cert", new Link("locator", "http://rest-test.db.ripe.net/lookup/test/key-cert/PGPKEY-28F6CD6C")),
-                new Attribute("mnt-by", "TST-MNT", null, "mntner", new Link("locator", "http://rest-test.db.ripe.net/lookup/test/mntner/TST-MNT")),
-                new Attribute("referral-by", "TST-MNT", null, "mntner", new Link("locator", "http://rest-test.db.ripe.net/lookup/test/mntner/TST-MNT")),
+                new Attribute("auth", "PGPKEY-28F6CD6C", null, "key-cert", new Link("locator", "http://rest.db.ripe.net/lookup/test/key-cert/PGPKEY-28F6CD6C")),
+                new Attribute("mnt-by", "TST-MNT", null, "mntner", new Link("locator", "http://rest.db.ripe.net/lookup/test/mntner/TST-MNT")),
+                new Attribute("referral-by", "TST-MNT", null, "mntner", new Link("locator", "http://rest.db.ripe.net/lookup/test/mntner/TST-MNT")),
                 new Attribute("changed", "dbtest@ripe.net", null, null, null),
                 new Attribute("source", "TEST", "Filtered", null, null)
         ));
