7c41cad291a540d342081bd2fe58c63ed304b181
Agoston Horvath <agoston@ripe.net>
2013-09-04 16:59:17 +0200

    added test for IpInterval

--- whois-commons/src/test/java/net/ripe/db/whois/common/domain/IpIntervalTest.java
+++ whois-commons/src/test/java/net/ripe/db/whois/common/domain/IpIntervalTest.java
@@ -34,6 +34,21 @@ public class IpIntervalTest {
     }
 
     @Test
+    public void parseReverseDomain() {
+        assertThat(IpInterval.parseReverseDomain("0.0.193.in-ADDR.arpA").toString(), is("193.0.0.0/24"));
+        assertThat(IpInterval.parseReverseDomain("a.b.0.0.1.iP6.arpA").toString(), is("100b:a000::/20"));
+        assertThat(IpInterval.parseReverseDomain("0.0.193.in-ADDR.arpA.").toString(), is("193.0.0.0/24"));
+        assertThat(IpInterval.parseReverseDomain("a.b.0.0.1.iP6.arpA.").toString(), is("100b:a000::/20"));
+    }
+
+    @Test
+    public void parse() {
+        assertThat(IpInterval.parse("193.0.0/24").toString(), is("193.0.0.0/24"));
+        assertThat(IpInterval.parse("00ab:cd::").toString(), is("ab:cd::/128"));
+    }
+
+
+    @Test
     public void ipInterval_ipv6() throws Exception {
         final IpInterval<?> subject = IpInterval.asIpInterval(InetAddress.getByName("3ffe:6a88:85a3:08d3:1319:8a2e:0370:7344"));
 
