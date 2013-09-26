de75e7481f9011ccadff1b68e12f89a8501d136b
Agoston Horvath <agoston@ripe.net>
2013-09-26 15:50:44 +0200

    fixed bug in format string :O

--- whois-api/src/test/java/net/ripe/db/whois/api/whois/WhoisRestServiceTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/whois/WhoisRestServiceTestIntegration.java
@@ -1759,6 +1759,6 @@ public class WhoisRestServiceTestIntegration extends AbstractRestClientTest {
 
     @Override
     protected WebTarget createResource(final Audience audience, final String path) {
-        return client.target(String.format("http://localhost:%s/%s", getPort(audience), path));
+        return client.target(String.format("http://localhost:%d/%s", getPort(audience), path));
     }
 }
