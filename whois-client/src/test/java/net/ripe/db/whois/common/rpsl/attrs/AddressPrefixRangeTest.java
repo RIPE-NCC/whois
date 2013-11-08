5b945162d4f64d8f6d43c685590381e4ed76c5e9
Agoston Horvath <agoston@ripe.net>
2013-11-08 12:34:49 +0100

    moved attribute parser tests to whois-client

--- whois-client/src/test/java/net/ripe/db/whois/common/rpsl/attrs/AddressPrefixRangeTest.java
+++ whois-client/src/test/java/net/ripe/db/whois/common/rpsl/attrs/AddressPrefixRangeTest.java
@@ -1,4 +1,4 @@
-package net.ripe.db.whois.common.domain.attrs;
+package net.ripe.db.whois.common.rpsl.attrs;
 
 import net.ripe.db.whois.common.ip.Ipv4Resource;
 import net.ripe.db.whois.common.ip.Ipv6Resource;
