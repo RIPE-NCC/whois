14d9a6743043165ed63c888515478981578066b0
Ed Shryane <eshryane@ripe.net>
2013-11-28 18:21:37 +0100

    Fixed failing tests (missing propOrder in WhoisResources).

--- whois-client/src/main/java/net/ripe/db/whois/api/rest/domain/WhoisResources.java
+++ whois-client/src/main/java/net/ripe/db/whois/api/rest/domain/WhoisResources.java
@@ -13,6 +13,7 @@ import java.util.List;
         "parameters",
         "objects",
         "sources",
+        "errors",
         "grsSources",
         "geolocationAttributes",
         "versions",
