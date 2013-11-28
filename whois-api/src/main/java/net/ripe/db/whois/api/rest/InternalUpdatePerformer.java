75f57c9e93dd599e8afce5cbe5dc6f41f28f220c
Ed Shryane <eshryane@ripe.net>
2013-11-28 16:22:03 +0100

    Removed unused field.

--- whois-api/src/main/java/net/ripe/db/whois/api/rest/InternalUpdatePerformer.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rest/InternalUpdatePerformer.java
@@ -3,7 +3,6 @@ package net.ripe.db.whois.api.rest;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import net.ripe.db.whois.common.DateTimeProvider;
-import net.ripe.db.whois.common.dao.RpslObjectDao;
 import net.ripe.db.whois.common.Message;
 import net.ripe.db.whois.common.Messages;
 import net.ripe.db.whois.common.rpsl.RpslObject;
