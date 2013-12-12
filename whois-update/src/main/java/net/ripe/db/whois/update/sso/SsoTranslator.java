48176352f82bf50da4d8295a208a7d336e6dca6f
Angela Sjöholm <asjoholm@ripe.net>
2013-12-12 19:44:51 +0100

    using jaxb marshalling for crowd responses

--- whois-update/src/main/java/net/ripe/db/whois/update/sso/SsoTranslator.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/sso/SsoTranslator.java
@@ -1,6 +1,8 @@
 package net.ripe.db.whois.update.sso;
 
+import com.google.common.base.Predicate;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
 import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.ObjectType;
@@ -17,7 +19,15 @@ import org.springframework.stereotype.Component;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.ByteArrayInputStream;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -25,13 +35,22 @@ public class SsoTranslator {
     public static final Splitter SPACE_SPLITTER = Splitter.on(' ');
     private final String restUrl;
     private final Client client;
+    private final Unmarshaller uuidUnmarshaller;
+    private final Unmarshaller usernameUnmarshaller;
 
     @Autowired
     public SsoTranslator(@Value("${rest.crowd.url}") final String translatorUrl,
-                         @Value("${rest.crowd.username}") final String crowdAuthUser,
+                         @Value("${rest.crowd.user}") final String crowdAuthUser,
                          @Value("${rest.crowd.password}") final String crowdAuthPassword) {
         this.restUrl = translatorUrl;
         client = ClientBuilder.newBuilder().register(new HttpBasicAuthFilter(crowdAuthUser, crowdAuthPassword)).build();
+
+        try {
+            uuidUnmarshaller = JAXBContext.newInstance(CrowdResponse.class).createUnmarshaller();
+            usernameUnmarshaller = JAXBContext.newInstance(CrowdUser.class).createUnmarshaller();
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     public String getUuidForUsername(final String username) {
@@ -40,7 +59,7 @@ public class SsoTranslator {
                 restUrl,
                 username);
 
-        String response = "";
+        String response;
         try {
             response = client.target(url).request().get(String.class);
         } catch (NotFoundException e) {
@@ -66,19 +85,22 @@ public class SsoTranslator {
         return extractUsername(response);
     }
 
-//    TODO
     private String extractUUID(final String response) {
-        final StringBuilder builder = new StringBuilder(response);
-        int pre = builder.indexOf("<value>", builder.indexOf("name=uuid"));
-        int post = builder.indexOf("</value>", pre);
-        return builder.substring(pre + "<value>".length(), post);
+        try {
+            final CrowdResponse crowdResponse = (CrowdResponse)uuidUnmarshaller.unmarshal(new ByteArrayInputStream(response.getBytes()));
+            return crowdResponse.getUUID();
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
-//    TODO
     private String extractUsername(final String response) {
-        int pre = response.indexOf("name=");
-        int post = response.indexOf("\">", pre);
-        return response.substring(pre + "name=\"".length(), post);
+        try {
+            final CrowdUser crowdUser = (CrowdUser)usernameUnmarshaller.unmarshal(new ByteArrayInputStream(response.getBytes()));
+            return crowdUser.getName();
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     public void populate(final Update update, final UpdateContext updateContext) {
@@ -127,6 +149,65 @@ public class SsoTranslator {
             return rpslObject;
         } else {
             return new RpslObjectBuilder(rpslObject).replaceAttributes(replace).get();
+        }
+    }
+
+
+    @XmlRootElement(name = "attributes")
+    private static class CrowdResponse {
+        @XmlElement(name = "attribute")
+        private List<CrowdAttribute> attributes;
+
+        public List<CrowdAttribute> getAttributes() {
+            return attributes;
+        }
+
+        public String getUUID() {
+            final CrowdAttribute attributeElement = Iterables.find(attributes, new Predicate<CrowdAttribute>() {
+                @Override
+                public boolean apply(final CrowdAttribute input) {
+                    return input.getName().equals("uuid");
+                }
+            });
+
+            return attributeElement.getValues().get(0).getValue();
+        }
+    }
+
+    @XmlRootElement
+    private static class CrowdAttribute {
+        @XmlElement
+        private List<CrowdValue> values;
+
+        @XmlAttribute(name="name")
+        private String name;
+
+        public List<CrowdValue> getValues() {
+            return values;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    @XmlRootElement
+    private static class CrowdValue {
+        @XmlElement(name="value")
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    @XmlRootElement(name = "user")
+    private static class CrowdUser {
+        @XmlAttribute(name="name")
+        private String name;
+
+        public String getName() {
+            return name;
         }
     }
 }
