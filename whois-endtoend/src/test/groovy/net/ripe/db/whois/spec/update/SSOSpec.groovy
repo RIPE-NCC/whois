a4cfb18b4164807f46c49e8203c5446b5d60552a
denis <denis@ripe.net>
2013-12-17 15:07:33 +0100

    added more auth: SSO tests

--- whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/update/SSOSpec.groovy
+++ whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/update/SSOSpec.groovy
@@ -531,4 +531,126 @@ class SSOSpec extends BaseQueryUpdateSpec {
         print(fullObj)
     }
 
+    def "replace SSO with invalid SSO in mntner with existing SSO"() {
+        given:
+        syncUpdate(getTransient("ONE-SSO-MNT") + "password: sso")
+
+        expect:
+        query_object_matches("-r -BG -T mntner ONE-SSO-MNT", "mntner", "ONE-SSO-MNT", "auth:\\s*SSO")
+        query_object_matches("-r -BG -T mntner ONE-SSO-MNT", "mntner", "ONE-SSO-MNT", "auth:\\s*MD5")
+
+        when:
+        def message = syncUpdate("""\
+                mntner:      ONE-SSO-MNT
+                descr:       MNTNER for test
+                admin-c:     TP1-TEST
+                upd-to:      updto_test@ripe.net
+                auth:        MD5-PW \$1\$ekjY/4Nb\$Jb.THskSsMVVLX5NnU7T80  #sso
+                auth:        SSO unknown@ripe.net
+                mnt-by:      ONE-SSO-MNT
+                referral-by: ONE-SSO-MNT
+                changed:     dbtest@ripe.net 20010601
+                source:      TEST
+
+                password: sso
+                """.stripIndent()
+        )
+
+        then:
+        def ack = new AckResponse("", message)
+
+        ack.summary.nrFound == 1
+        ack.summary.assertSuccess(0, 0, 0, 0, 0)
+        ack.summary.assertErrors(1, 0, 1, 0)
+
+        ack.countErrorWarnInfo(1, 0, 0)
+        ack.errors.any {it.operation == "Modify" && it.key == "[mntner] ONE-SSO-MNT"}
+        ack.errorMessagesFor("Modify", "[mntner] ONE-SSO-MNT") ==
+                ["Mandatory attribute \"auth\" is missing"]
+
+        query_object_matches("-r -BG -T mntner ONE-SSO-MNT", "mntner", "ONE-SSO-MNT", "auth:\\s*SSO")
+        query_object_matches("-r -BG -T mntner ONE-SSO-MNT", "mntner", "ONE-SSO-MNT", "auth:\\s*MD5")
+        def fullObj = databaseHelper.lookupObject(ObjectType.MNTNER, "ONE-SSO-MNT")
+        print(fullObj)
+    }
+
+    def "create mntner with pw and SSO"() {
+        given:
+
+        expect:
+        queryObjectNotFound("-r -BG -T mntner NO-SSO-MNT", "mntner", "NO-SSO-MNT")
+
+        when:
+        def message = syncUpdate("""\
+                mntner:      NO-SSO-MNT
+                descr:       MNTNER for test
+                admin-c:     TP1-TEST
+                upd-to:      updto_test@ripe.net
+                auth:        MD5-PW \$1\$yntkntNY\$k8Fr7y5mq17LQcbL4CNLf.  #sso
+                auth:        SSO db-test@ripe.net
+                mnt-by:      NO-SSO-MNT
+                referral-by: NO-SSO-MNT
+                changed:     dbtest@ripe.net 20010601
+                source:      TEST
+
+                password: sso
+                """.stripIndent()
+        )
+
+        then:
+        def ack = new AckResponse("", message)
+
+        ack.summary.nrFound == 1
+        ack.summary.assertSuccess(1, 1, 0, 0, 0)
+        ack.summary.assertErrors(0, 0, 0, 0)
+
+        ack.countErrorWarnInfo(0, 0, 0)
+        ack.successes.any {it.operation == "Create" && it.key == "[mntner] NO-SSO-MNT"}
+
+        query_object_matches("-r -BG -T mntner NO-SSO-MNT", "mntner", "NO-SSO-MNT", "auth:\\s*SSO")
+        query_object_matches("-r -BG -T mntner NO-SSO-MNT", "mntner", "NO-SSO-MNT", "auth:\\s*MD5")
+        def fullObj = databaseHelper.lookupObject(ObjectType.MNTNER, "NO-SSO-MNT")
+        print(fullObj)
+    }
+
+    def "create mntner with pw and invalid SSO"() {
+        given:
+
+        expect:
+        queryObjectNotFound("-r -BG -T mntner NO-SSO-MNT", "mntner", "NO-SSO-MNT")
+
+        when:
+        def message = syncUpdate("""\
+                mntner:      NO-SSO-MNT
+                descr:       MNTNER for test
+                admin-c:     TP1-TEST
+                upd-to:      updto_test@ripe.net
+                auth:        MD5-PW \$1\$yntkntNY\$k8Fr7y5mq17LQcbL4CNLf.  #sso
+                auth:        SSO unknown@ripe.net
+                mnt-by:      NO-SSO-MNT
+                referral-by: NO-SSO-MNT
+                changed:     dbtest@ripe.net 20010601
+                source:      TEST
+
+                password: sso
+                """.stripIndent()
+        )
+
+        then:
+        def ack = new AckResponse("", message)
+
+        ack.summary.nrFound == 1
+        ack.summary.assertSuccess(0, 0, 0, 0, 0)
+        ack.summary.assertErrors(1, 1, 0, 0)
+
+        ack.countErrorWarnInfo(1, 0, 0)
+        ack.errors.any {it.operation == "Create" && it.key == "[mntner] NO-SSO-MNT"}
+        ack.errorMessagesFor("Create", "[mntner] NO-SSO-MNT") ==
+                ["Mandatory attribute \"auth\" is missing"]
+
+        queryObjectNotFound("-r -BG -T mntner NO-SSO-MNT", "mntner", "NO-SSO-MNT")
+        def fullObj = databaseHelper.lookupObject(ObjectType.MNTNER, "NO-SSO-MNT")
+        print(fullObj)
+    }
+
 }
