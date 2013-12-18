62d1eb898f00c9cd3e2ccc289ff127368aac08bf
Agoston Horvath <agoston@ripe.net>
2013-12-18 14:40:31 +0100

    updated defaultabusec oneshot script for today's run

--- whois-oneshot/src/main/java/net/ripe/db/defaultabusec/DefaultAbuseC.java
+++ whois-oneshot/src/main/java/net/ripe/db/defaultabusec/DefaultAbuseC.java
@@ -1,17 +1,20 @@
 package net.ripe.db.defaultabusec;
 
-import com.google.common.collect.Lists;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import net.ripe.db.whois.api.rest.RestClient;
-import net.ripe.db.whois.api.rest.domain.AbuseContact;
 import net.ripe.db.whois.common.io.RpslObjectFileReader;
 import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.common.rpsl.RpslAttribute;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.internal.api.abusec.AbuseCService;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PatternLayout;
 
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 public class DefaultAbuseC {
@@ -23,6 +26,8 @@ public class DefaultAbuseC {
     private static final AbuseCService abuseCService = new AbuseCService(restClient, "RIPE");
 
     public static void main(final String[] argv) throws Exception {
+        setupLogging();
+
         final OptionSet options = setupOptionParser().parse(argv);
         int batchSize = Integer.parseInt(options.valueOf(ARG_BATCHSIZE).toString());
         String fileName = options.valueOf(ARG_ORGFILE).toString();
@@ -40,30 +45,49 @@ public class DefaultAbuseC {
                 continue;
             }
 
-            if (object.getType() != ObjectType.ORGANISATION) {
-                throw new IllegalArgumentException("Found non-organisation object: " + nextObject);
-            }
+            try {
+                if (object.getType() != ObjectType.ORGANISATION) {
+                    throw new IllegalArgumentException("Found non-organisation object: " + nextObject);
+                }
 
-            if (!object.getValueForAttribute(AttributeType.ORG_TYPE).toLowerCase().equals("lir")) {
-                continue;
-            }
+                if (!object.getValueForAttribute(AttributeType.ORG_TYPE).toLowerCase().equals("lir")) {
+                    continue;
+                }
 
-            List<RpslAttribute> attributes = object.findAttributes(AttributeType.E_MAIL);
-            if (attributes.size() > 1) {
-                System.err.println("More than 1 email in " + object.getFormattedKey() + ", picking first");
-            }
-            String email = attributes.get(0).getCleanValue().toString();
+                List<RpslAttribute> attributes = object.findAttributes(AttributeType.E_MAIL);
+                if (attributes.size() > 1) {
+                    System.err.println("More than 1 email in " + object.getFormattedKey() + ", picking first");
+                }
+                String email = attributes.get(0).getCleanValue().toString();
 
-            String orgkey = object.getKey().toString();
-            abuseCService.createAbuseRole(orgkey, email);
+                String orgkey = object.getKey().toString();
+                Response abuseRole = abuseCService.createAbuseRole(orgkey, email);
+                System.err.println(orgkey + ": " + abuseRole.toString());
 
-            System.out.println(orgkey + "\t" + email);
+                if (abuseRole.getStatus() != 200) {
+                    continue;
+                }
 
-            if (batchSize-- <= 0) {
-                System.err.println("Batch size limit reached, ending run");
-                break;
+                System.out.println(orgkey + "|" + email);
+
+                if (batchSize-- <= 0) {
+                    System.err.println("Batch size limit reached, ending run");
+                    break;
+                }
+            } catch (Exception e) {
+                System.err.println("Error processing " + object.getFormattedKey());
+                e.printStackTrace(System.err);
             }
         }
+    }
+
+    private static void setupLogging() {
+        LogManager.getRootLogger().setLevel(Level.INFO);
+        ConsoleAppender console = new ConsoleAppender();
+        console.setLayout(new PatternLayout("%d [%c|%C{1}] %m%n"));
+        console.setThreshold(Level.INFO);
+        console.activateOptions();
+        LogManager.getRootLogger().addAppender(console);
     }
 
     private static OptionParser setupOptionParser() {
