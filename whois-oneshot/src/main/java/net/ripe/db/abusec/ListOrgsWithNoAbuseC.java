f6e17c36d5c1c6f6535eb12c6744ba9b8dcae233
Angela Sjöholm <asjoholm@ripe.net>
2014-01-09 10:48:41 +0100

    added connection details and query for retrieving sponsoring lir emails

--- whois-oneshot/src/main/java/net/ripe/db/abusec/ListOrgsWithNoAbuseC.java
+++ whois-oneshot/src/main/java/net/ripe/db/abusec/ListOrgsWithNoAbuseC.java
@@ -1,8 +1,11 @@
 package net.ripe.db.abusec;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+import com.mysql.jdbc.Driver;
 import net.ripe.db.LogUtil;
 import net.ripe.db.whois.api.rest.RestClient;
+import net.ripe.db.whois.common.dao.jdbc.JdbcStreamingHelper;
 import net.ripe.db.whois.common.domain.io.Downloader;
 import net.ripe.db.whois.common.io.RpslObjectFileReader;
 import net.ripe.db.whois.common.rpsl.AttributeType;
@@ -11,8 +14,15 @@ import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.common.rpsl.attrs.Inet6numStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 public class ListOrgsWithNoAbuseC {
     private static final Logger LOGGER = LoggerFactory.getLogger(ListOrgsWithNoAbuseC.class);
@@ -66,5 +76,44 @@ public class ListOrgsWithNoAbuseC {
 
             }
         }
+    }
+
+    private JdbcTemplate setupRSNGTemplate(final String custdbConnectionPassword) throws SQLException {
+        final DataSource dataSource = new SimpleDriverDataSource(new Driver(), "jdbc:postgresql://duck.ripe.net/custdb", "dbase", custdbConnectionPassword);
+        return new JdbcTemplate(dataSource);
+    }
+
+    private Map<String, String> getSponsoringLirEmailPerOrganisationId(final JdbcTemplate jdbcTemplate) {
+        final Map<String, String> emailPerSponsoredOrganisation = Maps.newHashMap();
+
+        JdbcStreamingHelper.executeStreaming(jdbcTemplate,
+                "SELECT min(cm.contact_medium_value), a.oid\n" +
+                        "FROM contactmedium cm INNER JOIN contactdetails cd ON cd.id = cm.contactdetails_id\n" +
+                        "INNER JOIN organisation o ON cd.contact_organisation_id = o.id\n" +
+                        "INNER JOIN membership m ON m.id = o.membership_id\n" +
+                        "inner join\n" +
+                        "(\n" +
+                        " select asn.organisation_id as oid, asn.membership_id as mid\n" +
+                        " from resourcedb.asnresource asn\n" +
+                        " union\t\n" +
+                        " select ipv4.organisation_id as oid, ipv4.membership_id as mid\n" +
+                        " from resourcedb.ipv4assignmentresource ipv4\n" +
+                        " union\n" +
+                        " select ipv6.organisation_id as oid, ipv6.membership_id as mid\n" +
+                        " from resourcedb.ipv6assignmentresource ipv6\n" +
+                        ") a on m.id = a.mid\n" +
+                        "\n" +
+                        "where cm.contact_medium_type = 'EMAIL'\n" +
+                        "and a.oid is not null\n" +
+                        "and a.oid <> ''\n" +
+                        "group by a.oid\n" +
+                        "order by 2;",
+                new RowCallbackHandler() {
+                    @Override
+                    public void processRow(final ResultSet rs) throws SQLException {
+                        emailPerSponsoredOrganisation.put(rs.getString(1), rs.getString(2));
+                    }
+                });
+        return emailPerSponsoredOrganisation;
     }
 }
