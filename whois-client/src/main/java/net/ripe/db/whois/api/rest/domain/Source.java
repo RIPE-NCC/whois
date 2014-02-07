1e1ed3a36e74b67764a19bb7a9520703c9b91d41
Ed Shryane <eshryane@ripe.net>
2014-02-07 11:09:29 +0100

    Removed unused GrsMirror

--- whois-client/src/main/java/net/ripe/db/whois/api/rest/domain/Source.java
+++ whois-client/src/main/java/net/ripe/db/whois/api/rest/domain/Source.java
@@ -1,26 +1,17 @@
 package net.ripe.db.whois.api.rest.domain;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.google.common.collect.Lists;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
-import java.util.List;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "", propOrder = {
-        "grsMirror"
-})
 @XmlRootElement(name = "source")
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class Source {
 
-    @XmlElement(name = "grs-mirror")
-    private List<GrsMirror> grsMirror = Lists.newArrayList();
     @XmlAttribute(required = true)
     private String name;
     @XmlAttribute(required = true)
