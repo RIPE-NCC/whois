6a15158a999a8e466cfd348f4f6b962a3cc2c9f4
Agoston Horvath <agoston@ripe.net>
2014-02-13 12:01:53 +0100

    Made sure WhoisLogTest is always reporting the exact same message as WhoisLog by removing duplication

--- whois-query/src/main/java/net/ripe/db/whois/query/handler/WhoisLog.java
+++ whois-query/src/main/java/net/ripe/db/whois/query/handler/WhoisLog.java
@@ -4,6 +4,7 @@ import net.ripe.db.whois.common.profiles.WhoisProfile;
 import net.ripe.db.whois.query.domain.QueryCompletionInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.helpers.MessageFormatter;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -15,17 +16,21 @@ import java.net.InetAddress;
 public class WhoisLog {
     private final Logger logger = LoggerFactory.getLogger(WhoisLog.class);
 
+    protected String formatMessage(final String api, final int personalObjects, final int nonPersonalObjects, @Nullable final QueryCompletionInfo completionInfo, final long executionTime, @Nullable final InetAddress remoteAddress, final Integer channelId, final String queryString) {
+        return MessageFormatter.arrayFormat("{} PW-{}-INFO <{}+{}+0> {} {}ms [{}] --  {}",
+                new Object[]{
+                        String.format("%10d", channelId),
+                        api,
+                        personalObjects,
+                        nonPersonalObjects,
+                        completionInfo == null ? "" : completionInfo.name(),
+                        executionTime,
+                        remoteAddress != null ? remoteAddress.getHostAddress() : "NONE",
+                        queryString
+                }).getMessage();
+    }
+
     public void logQueryResult(final String api, final int personalObjects, final int nonPersonalObjects, @Nullable final QueryCompletionInfo completionInfo, final long executionTime, @Nullable final InetAddress remoteAddress, final Integer channelId, final String queryString) {
-        logger.info(
-                "{} PW-{}-INFO <{}+{}+0> {} {}ms [{}] --  {}",
-                String.format("%10d", channelId),
-                api,
-                personalObjects,
-                nonPersonalObjects,
-                completionInfo == null ? "" : completionInfo.name(),
-                executionTime,
-                remoteAddress != null ? remoteAddress.getHostAddress() : "NONE",
-                queryString
-        );
+        logger.info(formatMessage(api, personalObjects, nonPersonalObjects, completionInfo, executionTime, remoteAddress, channelId, queryString));
     }
 }
