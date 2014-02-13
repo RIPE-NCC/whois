af0d3d3cd6b09d5f9e66bc75f28403db72d7b282
Ed Shryane <eshryane@ripe.net>
2014-02-13 16:40:48 +0100

    Replaced AuditLoggerIntegrationTest (rely on RestClient, rather than JAX-RS client).

--- whois-api/src/test/java/net/ripe/db/whois/api/log/AuditLogTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/log/AuditLogTestIntegration.java
@@ -5,7 +5,6 @@ import net.ripe.db.whois.api.AbstractIntegrationTest;
 import net.ripe.db.whois.api.rest.RestClient;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.common.support.FileHelper;
-import org.hamcrest.Matchers;
 import org.joda.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import java.io.File;
 
 import static net.ripe.db.whois.common.rpsl.RpslObjectFilter.buildGenericObject;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
 
 public class AuditLogTestIntegration extends AbstractIntegrationTest {
@@ -52,45 +52,48 @@ public class AuditLogTestIntegration extends AbstractIntegrationTest {
     }
 
     @Test
-    public void update_passes_x_forwarded_for() {
-        final RpslObject updatedPerson = buildGenericObject(TEST_PERSON, "remarks: i will be back");
-
-        restClient.request()
-                .addHeader(HttpHeaders.X_FORWARDED_FOR, "10.20.30.40")
-                .addParam("password", "test")
-                .update(updatedPerson);
-
-        String audit = FileHelper.fetchGzip(new File(auditLog + "/20010204/170000.rest_10.20.30.40_0/000.audit.xml.gz"));
-
-        assertThat(audit, Matchers.containsString("<message><![CDATA[Header: X-Forwarded-For=10.20.30.40]]></message>"));
-    }
-
-    @Test
-    public void delete_passes_x_forwarded_for() {
+    public void create_gets_logged() {
         final RpslObject secondPerson = buildGenericObject(TEST_PERSON, "nic-hdl: TP2-TEST");
-        databaseHelper.addObject(secondPerson);
-
-        restClient.request()
-                .addHeader(HttpHeaders.X_FORWARDED_FOR, "10.20.30.40")
-                .addParam("password", "test")
-                .delete(secondPerson);
-
-        String audit = FileHelper.fetchGzip(new File(auditLog + "/20010204/170000.rest_10.20.30.40_0/000.audit.xml.gz"));
-
-        assertThat(audit, Matchers.containsString("<message><![CDATA[Header: X-Forwarded-For=10.20.30.40]]></message>"));
-    }
-
-    @Test
-    public void create_passes_x_forwarded_for() {
-        final RpslObject secondPerson = buildGenericObject(TEST_PERSON, "nic-hdl: TP2-TEST");
-
         restClient.request()
                 .addHeader(HttpHeaders.X_FORWARDED_FOR, "10.20.30.40")
                 .addParam("password", "test")
                 .create(secondPerson);
 
-        String audit = FileHelper.fetchGzip(new File(auditLog + "/20010204/170000.rest_10.20.30.40_0/000.audit.xml.gz"));
+        final String audit = FileHelper.fetchGzip(new File(auditLog + "/20010204/170000.rest_10.20.30.40_0/000.audit.xml.gz"));
 
-        assertThat(audit, Matchers.containsString("<message><![CDATA[Header: X-Forwarded-For=10.20.30.40]]></message>"));
+        assertThat(audit, containsString("<message><![CDATA[Header: X-Forwarded-For=10.20.30.40]]></message>"));
+    }
+
+    @Test
+    public void update_gets_logged() {
+        final RpslObject updatedPerson = buildGenericObject(TEST_PERSON, "remarks: i will be back");
+        restClient.request()
+                .addHeader(HttpHeaders.X_FORWARDED_FOR, "10.20.30.40")
+                .addParam("password", "test")
+                .update(updatedPerson);
+
+        final String audit = FileHelper.fetchGzip(new File(auditLog + "/20010204/170000.rest_10.20.30.40_0/000.audit.xml.gz"));
+
+        assertThat(audit, containsString("<query"));
+        assertThat(audit, containsString("<sql"));
+        assertThat(audit, containsString("<message><![CDATA[Header: X-Forwarded-For=10.20.30.40]]></message>"));
+        assertThat(audit, containsString("<message><![CDATA[/whois/TEST/person/TP1-TEST?password=test]]></message>"));
+    }
+
+    @Test
+    public void delete_gets_logged() {
+        final RpslObject secondPerson = buildGenericObject(TEST_PERSON, "nic-hdl: TP2-TEST");
+        databaseHelper.addObject(secondPerson);
+        restClient.request()
+                .addHeader(HttpHeaders.X_FORWARDED_FOR, "10.20.30.40")
+                .addParam("password", "test")
+                .delete(secondPerson);
+
+        final String audit = FileHelper.fetchGzip(new File(auditLog + "/20010204/170000.rest_10.20.30.40_0/000.audit.xml.gz"));
+
+        assertThat(audit, containsString("<query"));
+        assertThat(audit, containsString("<sql"));
+        assertThat(audit, containsString("<message><![CDATA[Header: X-Forwarded-For=10.20.30.40]]></message>"));
+        assertThat(audit, containsString("<message><![CDATA[/whois/TEST/person/TP2-TEST?password=test]]></message>"));
     }
 }
