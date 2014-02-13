4c1c07f27cda12075492219d799c078584126eba
Ed Shryane <eshryane@ripe.net>
2014-02-13 15:43:52 +0100

    #152 - Log request URI (including any query parameters) in the update audit log.

--- whois-api/src/test/java/net/ripe/db/whois/api/rest/InternalUpdatePerformerTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/rest/InternalUpdatePerformerTest.java
@@ -6,21 +6,15 @@ import net.ripe.db.whois.common.Message;
 import net.ripe.db.whois.common.Messages;
 import net.ripe.db.whois.update.handler.UpdateRequestHandler;
 import net.ripe.db.whois.update.log.LoggerContext;
-import org.apache.commons.collections.iterators.IteratorEnumeration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.Enumeration;
-
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InternalUpdatePerformerTest {
@@ -38,17 +32,23 @@ public class InternalUpdatePerformerTest {
 
     @Test
     public void log_http_headers() {
-        HttpServletRequest requestMock = mock(HttpServletRequest.class);
-        when(requestMock.getHeaderNames()).thenReturn(enumerate("name"));
-        when(requestMock.getHeaders("name")).thenReturn(enumerate("value"));
+        final MockHttpServletRequest request = new MockHttpServletRequest("PUT", "/ripe/mntner/TEST-MNT");
+        request.addHeader("name", "value");
 
-        InternalUpdatePerformer.logHttpHeaders(loggerContextMock, requestMock);
+        InternalUpdatePerformer.logHttpHeaders(loggerContextMock, request);
 
         verify(loggerContextMock).log(new Message(Messages.Type.INFO, "Header: name=value"));
         verifyNoMoreInteractions(loggerContextMock);
     }
 
-    private Enumeration<String> enumerate(final String... values) {
-        return new IteratorEnumeration(Arrays.asList(values).iterator());
+    @Test
+    public void log_http_uri() {
+        final MockHttpServletRequest request = new MockHttpServletRequest("GET", "/ripe/mntner/RIPE-DBM-MNT");
+        request.setQueryString("password=secret&unfiltered");
+
+        InternalUpdatePerformer.logHttpUri(loggerContextMock, request);
+
+        verify(loggerContextMock).log(new Message(Messages.Type.INFO, "/ripe/mntner/RIPE-DBM-MNT?password=secret&unfiltered"));
+        verifyNoMoreInteractions(loggerContextMock);
     }
 }
