a455d3fd1febbcac4801b845573b79e9a8461e91
Agoston Horvath <agoston@ripe.net>
2014-03-13 17:12:18 +0100

    reviewed restservicehelper

--- whois-api/src/main/java/net/ripe/db/whois/api/rest/RestServiceHelper.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rest/RestServiceHelper.java
@@ -1,29 +1,40 @@
 package net.ripe.db.whois.api.rest;
 
+import com.google.common.base.Splitter;
 import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
 
 public class RestServiceHelper {
-
-    private static final String FILTER_QUERY_STRING = "([&]{0}password=[^&]*[&]?)|([&]password=[^&]*)";
+    private static final Splitter AMPERSAND_SPLITTER = Splitter.on('&').omitEmptyStrings();
+    private static final Splitter EQUALS_SPLITTER = Splitter.on('=').omitEmptyStrings();
 
     private RestServiceHelper() {
         // do not instantiate
     }
 
     public static String getRequestURL(final HttpServletRequest request) {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(request.getRequestURL());
-        final String queryString = filter(request.getQueryString());
-        if (queryString.length() > 0) {
-            builder.append('?').append(queryString);
+        final String queryString = request.getQueryString();
+        final StringBuffer requestURL = request.getRequestURL();
+
+        if (StringUtils.isBlank(queryString)) {
+            return requestURL.toString();
         }
+
+        final StringBuilder builder = new StringBuilder(requestURL);
+        char separator = '?';
+
+        for (String next : AMPERSAND_SPLITTER.split(queryString)) {
+            final Iterator<String> iterator = EQUALS_SPLITTER.split(next).iterator();
+            if (iterator.hasNext() && iterator.next().equalsIgnoreCase("password")) {
+                continue;
+            }
+
+            builder.append(separator).append(next);
+            separator = '&';
+        }
+
         return builder.toString();
     }
-
-    private static String filter(final String queryString) {
-        return !StringUtils.isEmpty(queryString) ? queryString.replaceAll(FILTER_QUERY_STRING, "") : "";
-    }
-
 }
