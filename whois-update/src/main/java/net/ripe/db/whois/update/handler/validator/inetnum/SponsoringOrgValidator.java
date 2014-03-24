1227dc867f865e0ce760d79132f2e3b38a068337
Frank Visser <frank.visser@gmail.com>
2014-03-24 17:02:39 +0100

    small code cleanups: static imports of  the constants

--- whois-update/src/main/java/net/ripe/db/whois/update/handler/validator/inetnum/SponsoringOrgValidator.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/handler/validator/inetnum/SponsoringOrgValidator.java
@@ -1,7 +1,6 @@
 package net.ripe.db.whois.update.handler.validator.inetnum;
 
 import com.google.common.base.Objects;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import net.ripe.db.whois.common.dao.RpslObjectDao;
 import net.ripe.db.whois.common.domain.CIString;
@@ -19,6 +18,14 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static net.ripe.db.whois.common.rpsl.AttributeType.MNT_BY;
+import static net.ripe.db.whois.common.rpsl.AttributeType.ORG_TYPE;
+import static net.ripe.db.whois.common.rpsl.AttributeType.SPONSORING_ORG;
+import static net.ripe.db.whois.common.rpsl.ObjectType.*;
+import static net.ripe.db.whois.update.domain.Action.CREATE;
+import static net.ripe.db.whois.update.domain.Action.MODIFY;
+
 @Component
 public class SponsoringOrgValidator implements BusinessRuleValidator {
     private final Maintainers maintainers;
@@ -32,12 +39,12 @@ public class SponsoringOrgValidator implements BusinessRuleValidator {
 
     @Override
     public List<Action> getActions() {
-        return Lists.newArrayList(Action.CREATE, Action.MODIFY);
+        return newArrayList(CREATE, MODIFY);
     }
 
     @Override
     public List<ObjectType> getTypes() {
-        return Lists.newArrayList(ObjectType.INETNUM, ObjectType.INET6NUM, ObjectType.AUT_NUM);
+        return newArrayList(INETNUM, INET6NUM, AUT_NUM);
     }
 
     @Override
@@ -49,17 +56,17 @@ public class SponsoringOrgValidator implements BusinessRuleValidator {
 
         final RpslObject updatedObject = update.getUpdatedObject();
 
-        if (updatedObject.containsAttribute(AttributeType.SPONSORING_ORG)) {
-            final RpslObject sponsoringOrganisation = objectDao.getByKey(ObjectType.ORGANISATION, updatedObject.getValueForAttribute(AttributeType.SPONSORING_ORG));
+        if (updatedObject.containsAttribute(SPONSORING_ORG)) {
+            final RpslObject sponsoringOrganisation = objectDao.getByKey(ORGANISATION, updatedObject.getValueForAttribute(SPONSORING_ORG));
 
-            if (!sponsoringOrganisation.getValueForAttribute(AttributeType.ORG_TYPE).equals("LIR")) {
+            if (!sponsoringOrganisation.getValueForAttribute(ORG_TYPE).equals("LIR")) {
                 updateContext.addMessage(update, UpdateMessages.sponsoringOrgNotLIR());
             }
         }
 
         final boolean hasRsMaintainer = !Sets.intersection(
                 maintainers.getRsMaintainers(),
-                updatedObject.getValuesForAttribute(AttributeType.MNT_BY)).isEmpty();
+                updatedObject.getValuesForAttribute(MNT_BY)).isEmpty();
 
         if (!hasRsMaintainer && !update.isOverride()) {
             updateContext.addMessage(update, UpdateMessages.sponsoringOrgChanged());
@@ -67,10 +74,10 @@ public class SponsoringOrgValidator implements BusinessRuleValidator {
     }
 
     private boolean sponsoringOrgHasChanged(final PreparedUpdate update) {
-        final CIString refSponsoringOrg = update.getReferenceObject().getValueOrNullForAttribute(AttributeType.SPONSORING_ORG);
-        final CIString updSponsoringOrg = update.getUpdatedObject().getValueOrNullForAttribute(AttributeType.SPONSORING_ORG);
-        final boolean presentOnCreate = update.getAction() == Action.CREATE && (refSponsoringOrg != null && !refSponsoringOrg.equals(""));
+        final CIString refSponsoringOrg = update.getReferenceObject().getValueOrNullForAttribute(SPONSORING_ORG);
+        final CIString updSponsoringOrg = update.getUpdatedObject().getValueOrNullForAttribute(SPONSORING_ORG);
+        final boolean presentOnCreate = update.getAction() == CREATE && (refSponsoringOrg != null && !refSponsoringOrg.equals(""));
 
-        return presentOnCreate || (update.getAction() == Action.MODIFY && !Objects.equal(refSponsoringOrg, updSponsoringOrg));
+        return presentOnCreate || (update.getAction() == MODIFY && !Objects.equal(refSponsoringOrg, updSponsoringOrg));
     }
 }
