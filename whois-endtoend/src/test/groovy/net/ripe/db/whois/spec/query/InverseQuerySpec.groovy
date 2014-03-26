fb53451e38aa1aa4ad4c535d44881274ff06e1d3
Agoston Horvath <agoston@ripe.net>
2014-03-26 11:19:17 +0100

    moved all "endtoend" tests that do not rely on ripe ncc internal resources (e.g. crowd) to integration test.
    After review, the remaining SSO tests are also to be correctly coined as integration tests,
    making room for actual end-to-end tests that would test against RC and production environments.

--- whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/query/InverseQuerySpec.groovy
+++ whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/query/InverseQuerySpec.groovy
@@ -1,10 +1,10 @@
 package net.ripe.db.whois.spec.query
 
-import net.ripe.db.whois.common.EndToEndTest
+import net.ripe.db.whois.common.IntegrationTest
 import net.ripe.db.whois.spec.BaseQueryUpdateSpec
 import net.ripe.db.whois.spec.BasicFixtures
 
-@org.junit.experimental.categories.Category(EndToEndTest.class)
+@org.junit.experimental.categories.Category(IntegrationTest.class)
 class InverseQuerySpec extends BaseQueryUpdateSpec {
 
     @Override
