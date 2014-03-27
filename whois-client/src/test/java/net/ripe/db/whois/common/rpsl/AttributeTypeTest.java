7b8fcb831813e66d6ae80f4f29532b07af46f038
Agoston Horvath <agoston@ripe.net>
2014-03-27 10:59:36 +0100

    adjusted tests

--- whois-client/src/test/java/net/ripe/db/whois/common/rpsl/AttributeTypeTest.java
+++ whois-client/src/test/java/net/ripe/db/whois/common/rpsl/AttributeTypeTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 public class AttributeTypeTest {
     @Test
@@ -31,21 +30,5 @@ public class AttributeTypeTest {
     @Test
     public void getByNameOrNull_supports_shortkeys() {
         assertThat(AttributeType.getByNameOrNull("*as"), is(AttributeType.AS_SET));
-    }
-
-    @Test
-    public void generated_attributes_accept_any_syntax() {
-        for (final ObjectType objectType : ObjectType.values()) {
-            final ObjectTemplate objectTemplate = ObjectTemplate.getTemplate(objectType);
-
-            for (final AttributeTemplate attributeTemplate : objectTemplate.getAttributeTemplates()) {
-                if (attributeTemplate.getRequirement().equals(AttributeTemplate.Requirement.GENERATED) && objectType != ObjectType.AUT_NUM) {
-                    final AttributeSyntax syntax = attributeTemplate.getAttributeType().getSyntax();
-                    if (!(syntax instanceof AttributeSyntax.AnySyntax) && !syntax.equals(AttributeSyntax.GENERATED_SYNTAX)) {
-                        fail("Generated attributes should accept any or generated syntax: " + attributeTemplate.getAttributeType());
-                    }
-                }
-            }
-        }
     }
 }
