12a9eb7a5e84422bf457739d4bfdf4ccb3db3b06
Ed Shryane <eshryane@ripe.net>
2014-04-03 17:05:25 +0200

    Accept invalid attributes on comparison.

--- whois-update/src/test/java/net/ripe/db/whois/update/handler/KeycertAttributeGeneratorTest.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/handler/KeycertAttributeGeneratorTest.java
@@ -179,6 +179,55 @@ public class KeycertAttributeGeneratorTest {
     }
 
     @Test
+    public void invalid_attribute_is_ignored() {
+        RpslObject keycert = RpslObject.parse(
+                "key-cert:     PGPKEY-5763950D\n" +
+                "invalid-attribute:      invalid\n" +
+                "certif:       -----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
+                "certif:       Version: GnuPG v1.4.12 (Darwin)\n" +
+                "certif:       \n" +
+                "certif:       mQENBFC0yvUBCACn2JKwa5e8Sj3QknEnD5ypvmzNWwYbDhLjmD06wuZxt7Wpgm4+\n" +
+                "certif:       yO68swuow09jsrh2DAl2nKQ7YaODEipis0d4H2i0mSswlsC7xbmpx3dRP/yOu4WH\n" +
+                "certif:       2kZciQYxC1NY9J3CNIZxgw6zcghJhtm+LT7OzPS8s3qp+w5nj+vKY09A+BK8yHBN\n" +
+                "certif:       E+VPeLOAi+D97s+Da/UZWkZxFJHdV+cAzQ05ARqXKXeadfFdbkx0Eq2R0RZm9R+L\n" +
+                "certif:       A9tPUhtw5wk1gFMsN7c5NKwTUQ/0HTTgA5eyKMnTKAdwhIY5/VDxUd1YprnK+Ebd\n" +
+                "certif:       YNZh+L39kqoUL6lqeu0dUzYp2Ll7R2IURaXNABEBAAG0I25vcmVwbHlAcmlwZS5u\n" +
+                "certif:       ZXQgPG5vcmVwbHlAcmlwZS5uZXQ+iQE4BBMBAgAiBQJQtMr1AhsDBgsJCAcDAgYV\n" +
+                "certif:       CAIJCgsEFgIDAQIeAQIXgAAKCRC7zLstV2OVDdjSCACYAyyWr83Df/zzOWGP+qMF\n" +
+                "certif:       Vukj8xhaM5f5MGb9FjMKClo6ezT4hLjQ8hfxAAZxndwAXoz46RbDUsAe/aBwdwKB\n" +
+                "certif:       0owcacoaxUd0i+gVEn7CBHPVUfNIuNemcrf1N7aqBkpBLf+NINZ2+3c3t14k1BGe\n" +
+                "certif:       xCInxEqHnq4zbUmunCNYjHoKbUj6Aq7janyC7W1MIIAcOY9/PvWQyf3VnERQImgt\n" +
+                "certif:       0fhiekCr6tRbANJ4qFoJQSM/ACoVkpDvb5PHZuZXf/v+XB1DV7gZHjJeZA+Jto5Z\n" +
+                "certif:       xrmS5E+HEHVBO8RsBOWDlmWCcZ4k9olxp7/z++mADXPprmLaK8vjQmiC2q/KOTVA\n" +
+                "certif:       uQENBFC0yvUBCADTYI6i4baHAkeY2lR2rebpTu1nRHbIET20II8/ZmZDK8E2Lwyv\n" +
+                "certif:       eWold6pAWDq9E23J9xAWL4QUQRQ4V+28+lknMySXbU3uFLXGAs6W9PrZXGcmy/12\n" +
+                "certif:       pZ+82hHckh+jN9xUTtF89NK/wHh09SAxDa/ST/z/Dj0k3pQWzgBdi36jwEFtHhck\n" +
+                "certif:       xFwGst5Cv8SLvA9/DaP75m9VDJsmsSwh/6JqMUb+hY71Dr7oxlIFLdsREsFVzVec\n" +
+                "certif:       YHsKINlZKh60dA/Br+CC7fClBycEsR4Z7akw9cPLWIGnjvw2+nq9miE005QLqRy4\n" +
+                "certif:       dsrwydbMGplaE/mZc0d2WnNyiCBXAHB5UhmZABEBAAGJAR8EGAECAAkFAlC0yvUC\n" +
+                "certif:       GwwACgkQu8y7LVdjlQ1GMAgAgUohj4q3mAJPR6d5pJ8Ig5E3QK87z3lIpgxHbYR4\n" +
+                "certif:       HNaR0NIV/GAt/uca11DtIdj3kBAj69QSPqNVRqaZja3NyhNWQM4OPDWKIUZfolF3\n" +
+                "certif:       eY2q58kEhxhz3JKJt4z45TnFY2GFGqYwFPQ94z1S9FOJCifL/dLpwPBSKucCac9y\n" +
+                "certif:       6KiKfjEehZ4VqmtM/SvN23GiI/OOdlHL/xnU4NgZ90GHmmQFfdUiX36jWK99LBqC\n" +
+                "certif:       RNW8V2MV+rElPVRHev+nw7vgCM0ewXZwQB/bBLbBrayx8LzGtMvAo4kDJ1kpQpip\n" +
+                "certif:       a/bmKCK6E+Z9aph5uoke8bKoybIoQ2K3OQ4Mh8yiI+AjiQ==\n" +
+                "certif:       =HQmg\n" +
+                "certif:       -----END PGP PUBLIC KEY BLOCK-----\n" +
+                "mnt-by:       UPD-MNT\n" +
+                "notify:       noreply@ripe.net\n" +
+                "changed:      noreply@ripe.net 20120213\n" +
+                "source:       TEST\n");
+        when(keyWrapperFactory.createKeyWrapper(keycert, update, updateContext)).thenReturn(PgpPublicKeyWrapper.parse(keycert));
+
+        final RpslObject updatedObject = subject.generateAttributes(keycert, update, updateContext);
+
+        validateAttributeType(updatedObject, AttributeType.METHOD, "PGP");
+        validateAttributeType(updatedObject, AttributeType.FINGERPR, "884F 8E23 69E5 E6F1 9FB3  63F4 BBCC BB2D 5763 950D");
+        validateAttributeType(updatedObject, AttributeType.OWNER, "noreply@ripe.net <noreply@ripe.net>");
+    }
+
+
+    @Test
     public void public_key_with_multiple_owners() {
         RpslObject keycert = RpslObject.parse(
                 "key-cert:   PGPKEY-5763950D\n" +
