1d3778a38cc9330eaf8094610c02a1b6b46baaa5
Agoston Horvath <agoston@ripe.net>
2014-04-10 12:31:33 +0200

    add deployment comment

--- whois-commons/src/main/resources/patch/whois-1.73-2.sql
+++ whois-commons/src/main/resources/patch/whois-1.73-2.sql
@@ -1,5 +1,5 @@
 --
--- Table structure for table `sponsoring_org`
+-- with the ~20K records in the table in production, this executed under 1 second when run on RC
 --
 
 ALTER TABLE organisation_id DROP PRIMARY KEY, DROP KEY `range_end`, DROP KEY `space`, ADD PRIMARY KEY (`range_id`), ADD UNIQUE KEY `space` (`space`, `source`);
