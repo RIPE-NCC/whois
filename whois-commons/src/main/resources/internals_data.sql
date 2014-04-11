3427a37fab675670d845b9ca9e33c2d3b411258f
Frank Visser <frank.visser@gmail.com>
2014-04-11 11:30:13 +0200

    fixed SponsorSpec faiing tests. Added the AS to the internals_data.sql file

--- whois-commons/src/main/resources/internals_data.sql
+++ whois-commons/src/main/resources/internals_data.sql
@@ -8,6 +8,8 @@ INSERT INTO authoritative_resource (source, resource) VALUES ('test', 'AS300');
 INSERT INTO authoritative_resource (source, resource) VALUES ('test', 'AS650');
 INSERT INTO authoritative_resource (source, resource) VALUES ('test', '::/0');
 INSERT INTO authoritative_resource (source, resource) VALUES ('test', '0.0.0.0/0');
+INSERT INTO authoritative_resource (source, resource) VALUES ('test', 'AS222');
+INSERT INTO authoritative_resource (source, resource) VALUES ('test', 'AS333');
 -- legacy autnums
 INSERT INTO legacy_autnums (autnum) VALUES ('103');
 INSERT INTO legacy_autnums (autnum) VALUES ('12666');
