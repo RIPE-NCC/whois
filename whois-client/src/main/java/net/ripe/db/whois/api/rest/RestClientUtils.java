1e5d40a1514a7da1ed430480d1535f9beb780330
Ed Shryane <eshryane@ripe.net>
2014-04-24 19:27:41 +0200

    Before calling JAX-RS request, encode only curly braces, which are always treated by JerseyWebTarget as template variable delimiters.

--- whois-client/src/main/java/net/ripe/db/whois/api/rest/RestClientUtils.java
+++ whois-client/src/main/java/net/ripe/db/whois/api/rest/RestClientUtils.java
@@ -1,19 +1,35 @@
 package net.ripe.db.whois.api.rest;
 
 import com.google.common.base.Function;
-import org.glassfish.jersey.uri.UriComponent;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 public class RestClientUtils {
     private RestClientUtils() {
+        // do not instantiate
     }
 
-    public static final Function<String, String> ENCODING_FUNCTION = new Function<String, String>() {
+    public static final Function<String, String> CURLY_BRACES_ENCODING_FUNCTION = new Function<String, String>() {
         @Override
         public String apply(final String input) {
-            return UriComponent.encode(input, UriComponent.Type.QUERY_PARAM, false);
+            // JerseyWebTarget treats curly braces as template variable delimiters - encode them instead so they are skipped.
+            final StringBuilder builder = new StringBuilder(input.length());
+            for (int i = 0; i < input.length(); i++) {
+                final char c = input.charAt(i);
+                switch (c) {
+                    case '{':
+                        builder.append("%7B");
+                        break;
+                    case '}':
+                        builder.append("%7D");
+                        break;
+                    default:
+                        builder.append(c);
+                        break;
+                }
+            }
+            return builder.toString();
         }
     };
 
