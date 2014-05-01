2dc2a1ac67b94fe01d23fc7005eb8cfe81fc8a6d
Angela Sjöholm <asjoholm@ripe.net>
2014-05-01 11:39:41 +0200

    story 70427926: As a DB user I want to be informed of the status change of my autnum objects that will happen, implemented the inetnum part

--- whois-oneshot/src/main/java/net/ripe/db/updateobjects/AutnumMassupdateEmailListGenerator.java
+++ whois-oneshot/src/main/java/net/ripe/db/updateobjects/AutnumMassupdateEmailListGenerator.java
@@ -17,6 +17,7 @@ import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -27,28 +28,45 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ *  Generates list of resources per emails on this format
+ *    test@ripe.net | AS123, AS245, AS378
+ *
+ *  Example commandline arguments:
+ *  --output-autnum autnumemails --output-inetnum legacyinetnumemails --db-password <rdonly password>
+ *
+ *  Expects to read a file called 'legacyInetnums' which contains inetnums in this format:
+ *    159.100.128.0 - 159.100.159.255
+ *    one inetnum per line
+ */
 public class AutnumMassupdateEmailListGenerator {
     private static final Logger LOGGER = LoggerFactory.getLogger("AutnumMassupdateEmailListGenerator");
-    private static final String ARG_OUTPUT_FILENAME = "output-filename";
+    private static final String ARG_OUTPUTFILE_AUTNUM = "output-autnum";
+    private static final String ARG_OUTPUTFILE_INETNUM = "output-inetnum";
     private static final String ARG_DBPASSWORD = "db-password";
 
     public static void main(final String[] args) throws Exception {
         LogUtil.initLogger();
 
         final OptionSet options = setupOptionParser().parse(args);
-        final File outputFile = new File((String)options.valueOf(ARG_OUTPUT_FILENAME));
-        initOutputFile(outputFile);
+        final File outputAutnum = new File((String)options.valueOf(ARG_OUTPUTFILE_AUTNUM));
+        final File outputInetnum = new File((String)options.valueOf(ARG_OUTPUTFILE_INETNUM));
+        initOutputFile(outputAutnum);
+        initOutputFile(outputInetnum);
 
-        final AutnumMassupdateEmailListGenerator emailGenerator = new AutnumMassupdateEmailListGenerator(outputFile, (String)options.valueOf(ARG_DBPASSWORD));
-        emailGenerator.executeForAutnum();
+        final AutnumMassupdateEmailListGenerator emailGenerator = new AutnumMassupdateEmailListGenerator((String)options.valueOf(ARG_DBPASSWORD));
+        emailGenerator.executeForAutnum(outputAutnum);
+        emailGenerator.executeForInetnum(outputInetnum);
     }
 
     private static OptionParser setupOptionParser() {
         final OptionParser parser = new OptionParser();
-        parser.accepts(ARG_OUTPUT_FILENAME).withRequiredArg().required();
+        parser.accepts(ARG_OUTPUTFILE_AUTNUM).withRequiredArg().required();
+        parser.accepts(ARG_OUTPUTFILE_INETNUM).withRequiredArg().required();
         parser.accepts(ARG_DBPASSWORD).withRequiredArg().required();
         return parser;
     }
@@ -60,13 +78,11 @@ public class AutnumMassupdateEmailListGenerator {
     }
 
     private final JdbcTemplate jdbcTemplate;
-    private final File outputFile;
-    public AutnumMassupdateEmailListGenerator(final File outputFile, final String dbPassword) throws SQLException {
-        this.outputFile = outputFile;
+    public AutnumMassupdateEmailListGenerator(final String dbPassword) throws SQLException {
         jdbcTemplate = new JdbcTemplate(new SimpleDriverDataSource(new Driver(), "jdbc:mysql://dbc-whois5.ripe.net/WHOIS_UPDATE_RIPE", "rdonly", dbPassword));
     }
 
-    public Map<CIString, Set<CIString>> executeForAutnum() {
+    public void executeForAutnum(final File output) {
         //         email        autnumkeys
         final Map<CIString, Set<CIString>> autnumsPerEmail = Maps.newHashMap();
         JdbcStreamingHelper.executeStreaming(jdbcTemplate,
@@ -86,31 +102,54 @@ public class AutnumMassupdateEmailListGenerator {
                             return;
                         }
 
-                        final Set<CIString> emailAddresses = Sets.newHashSet();
-                        final IndexStrategy indexStrategy = IndexStrategies.get(AttributeType.MNT_BY);
-                        for (CIString mntBy : object.getValuesForAttribute(AttributeType.MNT_BY)) {
-                            for (RpslObjectInfo objectInfo : indexStrategy.findInIndex(jdbcTemplate, mntBy, ObjectType.MNTNER)) {
-                                final RpslObject mntner = JdbcRpslObjectOperations.getObjectById(jdbcTemplate, objectInfo.getObjectId());
-                                emailAddresses.addAll(mntner.getValuesForAttribute(AttributeType.UPD_TO));
-                            }
-                        }
-
-                        for (CIString emailAddress : emailAddresses) {
-                            Set<CIString> autnums = autnumsPerEmail.get(emailAddress);
-                            if (autnums == null) {
-                                autnums = Sets.newHashSet();
-                            }
-                            autnums.add(object.getKey());
-                            autnumsPerEmail.put(emailAddress, autnums);
-                        }
+                        collectResourcesPerEmail(object, autnumsPerEmail);
                     }
                 });
 
-        print(autnumsPerEmail);
-        return autnumsPerEmail;
+        print(autnumsPerEmail, output);
     }
 
-    private void print(final Map<CIString, Set<CIString>> emailAutnumsMap) {
+    public void executeForInetnum(final File output) throws IOException {
+//                 email         inetnums
+        final Map<CIString, Set<CIString>> inetnumsPerEmail = Maps.newHashMap();
+        final LineIterator iterator = FileUtils.lineIterator(new File("legacyInetnums"));
+        while (iterator.hasNext()) {
+            final String line = iterator.nextLine().trim();
+            final IndexStrategy indexStrategy = IndexStrategies.get(AttributeType.INETNUM);
+            final List<RpslObjectInfo> inetnumInfos = indexStrategy.findInIndex(jdbcTemplate, line, ObjectType.INETNUM);
+            if (inetnumInfos.size() != 1) {
+                LOGGER.info("skipping {}, found {} in index", line, inetnumInfos.size());
+                continue;
+            }
+
+            final RpslObject inetnum = JdbcRpslObjectOperations.getObjectById(jdbcTemplate, inetnumInfos.get(0).getObjectId());
+            collectResourcesPerEmail(inetnum, inetnumsPerEmail);
+        }
+
+        print(inetnumsPerEmail, output);
+    }
+
+    private void collectResourcesPerEmail(final RpslObject object, final Map<CIString, Set<CIString>> resourcesPerEmail) {
+        final Set<CIString> emailAddresses = Sets.newHashSet();
+        final IndexStrategy indexStrategy = IndexStrategies.get(AttributeType.MNT_BY);
+        for (CIString mntBy : object.getValuesForAttribute(AttributeType.MNT_BY)) {
+            for (RpslObjectInfo objectInfo : indexStrategy.findInIndex(jdbcTemplate, mntBy, ObjectType.MNTNER)) {
+                final RpslObject mntner = JdbcRpslObjectOperations.getObjectById(jdbcTemplate, objectInfo.getObjectId());
+                emailAddresses.addAll(mntner.getValuesForAttribute(AttributeType.UPD_TO));
+            }
+        }
+
+        for (CIString emailAddress : emailAddresses) {
+            Set<CIString> inetnums = resourcesPerEmail.get(emailAddress);
+            if (inetnums == null) {
+                inetnums = Sets.newHashSet();
+            }
+            inetnums.add(object.getKey());
+            resourcesPerEmail.put(emailAddress, inetnums);
+        }
+    }
+
+    private void print(final Map<CIString, Set<CIString>> emailAutnumsMap, final File outputFile) {
         final Joiner joiner = Joiner.on(',');
         final boolean append = Boolean.TRUE;
 
