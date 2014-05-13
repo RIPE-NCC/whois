dd6a16d05e57698d49a2a8c462b8eee58ee402f2
Ed Shryane <eshryane@ripe.net>
2014-05-13 14:31:58 +0200

    Moved test (ref.733d752b1ced)

--- whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/update/InetnumSpec.groovy
+++ whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/update/InetnumSpec.groovy
@@ -1,10 +1,10 @@
 package net.ripe.db.whois.spec.update
 
-import net.ripe.db.whois.common.EndToEndTest
 import net.ripe.db.whois.common.IntegrationTest
 import net.ripe.db.whois.spec.BaseQueryUpdateSpec
 import net.ripe.db.whois.spec.domain.AckResponse
 import net.ripe.db.whois.spec.domain.Message
+import net.ripe.db.whois.spec.domain.SyncUpdate
 import spock.lang.Ignore
 
 @org.junit.experimental.categories.Category(IntegrationTest.class)
@@ -5454,4 +5454,52 @@ class InetnumSpec extends BaseQueryUpdateSpec {
         query_object_not_matches("-rGBT inetnum 192.168.200.0 - 192.168.200.255", "inetnum", "192.168.200.0 - 192.168.200.255", "just added")
     }
 
+    def "create and modify, more specific, without needing mnt-lower"() {
+        given:
+          databaseHelper.addObject("""\
+                    inetnum:    192.168.0.0 - 192.168.255.255
+                    netname:    RIPE-NCC
+                    status:     ASSIGNED PI
+                    descr:      description
+                    country:    NL
+                    admin-c:    TP1-TEST
+                    tech-c:     TP1-TEST
+                    mnt-by:     LIR-MNT
+                    changed:    ripe@test.net 20120505
+                    source:     TEST
+                    """.stripIndent())
+          whoisFixture.reloadTrees()
+        when:
+          def created = syncUpdate(new SyncUpdate(data: """\
+                        inetnum:    192.168.0.0 - 192.168.0.255
+                        netname:    RIPE-NCC
+                        status:     ASSIGNED PI
+                        descr:      description
+                        country:    DK
+                        admin-c:    TP1-TEST
+                        tech-c:     TP1-TEST
+                        mnt-by:     LIR-MNT
+                        changed:    ripe@test.net 20120505
+                        source:     TEST
+                        password: lir
+                    """.stripIndent()))
+        then:
+          created =~ /Create SUCCEEDED: \[inetnum\] 192.168.0.0 - 192.168.0.255/
+        when:
+          def modified = syncUpdate(new SyncUpdate(data: """\
+                        inetnum:    192.168.0.0 - 192.168.0.255
+                        netname:    RIPE-NCC
+                        status:     ASSIGNED PI
+                        descr:      description (updated)
+                        country:    DK
+                        admin-c:    TP1-TEST
+                        tech-c:     TP1-TEST
+                        mnt-by:     LIR-MNT
+                        changed:    ripe@test.net 20120505
+                        source:     TEST
+                        password: lir
+                    """.stripIndent()))
+        then:
+          modified =~ /Modify SUCCEEDED: \[inetnum\] 192.168.0.0 - 192.168.0.255/
+    }
 }
