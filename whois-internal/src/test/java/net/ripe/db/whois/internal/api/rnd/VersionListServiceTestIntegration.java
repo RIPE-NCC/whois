58428f3d4584144cb6c4f8eaf635e01c3d4ef31f
Angela Sjöholm <asjoholm@ripe.net>
2014-06-06 10:55:42 +0200

    added testcases and finals

--- whois-internal/src/test/java/net/ripe/db/whois/internal/api/rnd/VersionListServiceTestIntegration.java
+++ whois-internal/src/test/java/net/ripe/db/whois/internal/api/rnd/VersionListServiceTestIntegration.java
@@ -15,6 +15,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class VersionListServiceTestIntegration extends AbstractInternalTest {
     }
 
     @Test
-    public void listVersions_created_and_updated() {
+    public void listVersions_created_updated() {
         final RpslObjectUpdateInfo add = updateDao.createObject(RpslObject.parse("" +
                 "aut-num: AS3333\n" +
                 "source: TEST"));
@@ -57,5 +58,57 @@ public class VersionListServiceTestIntegration extends AbstractInternalTest {
         final LocalDateTime toDate = LocalDateTime.parse(to, formatter);
 
         assertThat(Period.fieldDifference(fromDate, toDate).getDays(), is(3));
+    }
+
+    @Test
+    public void listVersions_created_deleted() {
+        final RpslObjectUpdateInfo add = updateDao.createObject(RpslObject.parse("" +
+                "aut-num: AS3333\n" +
+                "source: TEST"));
+        testDateTimeProvider.setTime(new LocalDateTime().plusDays(3));
+        updateDao.deleteObject(add.getObjectId(), "AS3333");
+
+        final WhoisResources result = RestTest.target(getPort(), "api/rnd/test/AUT-NUM/AS3333/versions", null, apiKey)
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(WhoisResources.class);
+
+        assertThat(result.getErrorMessages(), hasSize(0));
+        final List<WhoisVersionInternal> versions = result.getVersionsInternal().getVersions();
+        assertThat(versions, hasSize(2));
+
+        // which order should they be shown in?
+        assertThat(versions.get(0).getOperation().equals("DEL") || versions.get(1).getOperation().equals("DEL"), is(true));
+    }
+
+    @Test
+    public void listVersions_created_deleted_recreated() {
+        final RpslObject rpslObject = RpslObject.parse("" +
+                "aut-num: AS3333\n" +
+                "source: TEST");
+        final RpslObjectUpdateInfo add = updateDao.createObject(rpslObject);
+        testDateTimeProvider.setTime(new LocalDateTime().plusDays(3));
+        updateDao.deleteObject(add.getObjectId(), "AS3333");
+        updateDao.createObject(rpslObject);
+
+        final WhoisResources result = RestTest.target(getPort(), "api/rnd/test/AUT-NUM/AS3333/versions", null, apiKey)
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(WhoisResources.class);
+
+        assertThat(result.getErrorMessages(), hasSize(0));
+        final List<WhoisVersionInternal> versions = result.getVersionsInternal().getVersions();
+        assertThat(versions, hasSize(3));
+    }
+
+    @Test
+    public void listVersions_key_does_not_exist() {
+        try {
+            RestTest.target(getPort(), "api/rnd/test/AUT-NUM/AS3333/versions", null, apiKey)
+                .request(MediaType.APPLICATION_XML)
+                .get(WhoisResources.class);
+
+        } catch (ClientErrorException e) {
+            assertThat(e.getResponse().getStatus(), is(404));
+            //more assertions perhaps
+        }
     }
 }
