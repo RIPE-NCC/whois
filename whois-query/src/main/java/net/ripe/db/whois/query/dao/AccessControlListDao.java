16c556ec4fa8446b7c3426d6bf449c823a4894f6
Angela Sjöholm <asjoholm@ripe.net>
2014-06-30 11:50:19 +0200

    merging issue-240 to master

--- whois-query/src/main/java/net/ripe/db/whois/query/dao/AccessControlListDao.java
+++ whois-query/src/main/java/net/ripe/db/whois/query/dao/AccessControlListDao.java
@@ -2,17 +2,18 @@ package net.ripe.db.whois.query.dao;
 
 import net.ripe.db.whois.common.domain.BlockEvent;
 import net.ripe.db.whois.common.domain.BlockEvents;
+import net.ripe.db.whois.common.ip.IpInterval;
 import net.ripe.db.whois.query.acl.IpResourceConfiguration;
 import org.joda.time.LocalDate;
 
 import java.util.List;
 
 public interface AccessControlListDao extends IpResourceConfiguration.Loader {
-    void saveAclEvent(String prefix, LocalDate date, int limit, BlockEvent.Type type);
+    void saveAclEvent(IpInterval<?> interval, LocalDate date, int limit, BlockEvent.Type type);
 
     List<BlockEvents> getTemporaryBlocks(LocalDate blockTime);
 
-    void savePermanentBlock(String prefix, LocalDate date, int limit, String comment);
+    void savePermanentBlock(IpInterval<?> interval, LocalDate date, int limit, String comment);
 
     void removePermanentBlocksBefore(LocalDate date);
 
