54c2424a2797079db7cd606a392202b28ca62520
Angela Sjöholm <asjoholm@ripe.net>
2014-07-01 10:34:48 +0200

    added some finals

--- whois-commons/src/test/java/net/ripe/db/whois/common/support/DummyWhoisClient.java
+++ whois-commons/src/test/java/net/ripe/db/whois/common/support/DummyWhoisClient.java
@@ -7,7 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.FileCopyUtils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
@@ -42,21 +46,21 @@ public class DummyWhoisClient {
     }
 
 
-    public DummyWhoisClient(int port) {
+    public DummyWhoisClient(final int port) {
         this.port = port;
         this.host = "localhost";
     }
 
-    public DummyWhoisClient(String host, int port) {
+    public DummyWhoisClient(final String host, final int port) {
         this.port = port;
         this.host = host;
     }
 
-    public String sendQuery(String query) throws IOException {
+    public String sendQuery(final String query) throws IOException {
         return sendQuery(query, Charsets.ISO_8859_1);
     }
 
-    public String sendQuery(String query, Charset charset) throws IOException {
+    public String sendQuery(final String query, final Charset charset) throws IOException {
         return sendQuery(query, charset, -1);
     }
 
@@ -67,9 +71,9 @@ public class DummyWhoisClient {
             socket.setSoTimeout(timeoutMs);
         }
 
-        PrintWriter serverWriter = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), charset));
-        StringWriter responseWriter = new StringWriter();
+        final PrintWriter serverWriter = new PrintWriter(socket.getOutputStream(), true);
+        final BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), charset));
+        final StringWriter responseWriter = new StringWriter();
 
         serverWriter.println(query);
 
