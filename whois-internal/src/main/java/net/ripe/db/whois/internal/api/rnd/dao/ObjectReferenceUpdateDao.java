5c3174aaa04ebef015adc82b9bb90cf6c600aa0d
Ed Shryane <eshryane@ripe.net>
2014-08-05 15:02:09 +0200

    Refactored

--- whois-internal/src/main/java/net/ripe/db/whois/internal/api/rnd/dao/ObjectReferenceUpdateDao.java
+++ whois-internal/src/main/java/net/ripe/db/whois/internal/api/rnd/dao/ObjectReferenceUpdateDao.java
@@ -2,22 +2,22 @@ package net.ripe.db.whois.internal.api.rnd.dao;
 
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.internal.api.rnd.domain.ObjectVersion;
+import org.joda.time.DateTime;
 
 import java.util.List;
 
 public interface ObjectReferenceUpdateDao {
 
-    void createReference(final ObjectVersion from, final ObjectVersion to);
-    List<ObjectVersion> findIncomingReferences(final ObjectVersion objectVersion);
-    List<ObjectVersion> findOutgoingReferences(final ObjectVersion objectVersion);
-
     void createVersion(final ObjectVersion objectVersion);
     void updateVersionToTimestamp(final ObjectVersion objectVersion, final long endTimestamp);
-    void deleteVersion(final ObjectVersion objectVersion);
+    void createReference(final ObjectVersion from, final ObjectVersion to);
 
     List<ObjectVersion> getVersions(final long fromTimestamp);
     List<ObjectVersion> getVersions(final String pkey, final ObjectType objectType);
-    List<ObjectVersion> getVersions(final String pkey, final ObjectType objectType, final long fromTimestamp, final long toTimestamp);
+    List<ObjectVersion> getVersions(final String pkey, final ObjectType objectType, final DateTime fromDate, final DateTime toDate);
     ObjectVersion getVersion(final ObjectType objectType, final String pkey, final int revision);
 
+    List<ObjectVersion> findIncomingReferences(final ObjectVersion objectVersion);
+
+    long getLatestVersionTimestamp();
 }
