e965ec14715388670c568fa97a48b67366537bf0
Ed Shryane <eshryane@ripe.net>
2014-08-08 15:37:22 +0200

    Created internals table to store email links.

--- whois-commons/src/main/resources/internals_schema.sql
+++ whois-commons/src/main/resources/internals_schema.sql
@@ -40,3 +40,16 @@ CREATE TABLE `legacy_autnums` (
   `autnum` varchar(16) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `email_links`;
+CREATE TABLE `email_links` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `hash` varchar(256) NOT NULL,
+  `data` varchar(256) NOT NULL,
+  `creation_date` int(10) unsigned NOT NULL DEFAULT '0',
+  `expiry_date` int(10) unsigned NOT NULL DEFAULT '0',
+  `created_by` varchar(256),
+  `expired_by` varchar(256),
+  PRIMARY KEY (`id`),
+  KEY(`hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
