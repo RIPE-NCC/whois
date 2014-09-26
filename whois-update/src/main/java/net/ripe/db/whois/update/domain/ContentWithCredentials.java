265cddbdf97f863aecb9595f347b5038cde75879
Theodoros Polychniatis <tpolychnia@ripe.net>
2014-09-26 16:35:31 +0200

    half way fixing #254

--- whois-update/src/main/java/net/ripe/db/whois/update/domain/ContentWithCredentials.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/domain/ContentWithCredentials.java
@@ -1,6 +1,7 @@
 package net.ripe.db.whois.update.domain;
 
 import javax.annotation.concurrent.Immutable;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,15 +9,24 @@ import java.util.List;
 public class ContentWithCredentials {
     final String content;
     final List<Credential> credentials;
+    final Charset charset;
 
     public ContentWithCredentials(final String content) {
         this.content = content;
+        this.charset = null;
         this.credentials = Collections.emptyList();
     }
 
     public ContentWithCredentials(final String content, final List<Credential> credentials) {
         this.content = content;
         this.credentials = Collections.unmodifiableList(credentials);
+        this.charset = null;
+    }
+
+    public ContentWithCredentials(final String content, final List<Credential> credentials, final Charset charset) {
+        this.content = content;
+        this.credentials = Collections.unmodifiableList(credentials);
+        this.charset = charset;
     }
 
     public String getContent() {
@@ -25,5 +35,9 @@ public class ContentWithCredentials {
 
     public List<Credential> getCredentials() {
         return credentials;
+    }
+
+    public Charset getCharset() {
+        return charset;
     }
 }
