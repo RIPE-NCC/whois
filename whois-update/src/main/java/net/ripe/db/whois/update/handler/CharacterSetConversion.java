b0d0c212832f2bac21369b32e87dddf6dab09a2a
Oleg Muravskiy <oleg@ripe.net>
2014-10-02 09:28:49 +0200

    Syntactic sugar.

--- whois-update/src/main/java/net/ripe/db/whois/update/handler/CharacterSetConversion.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/handler/CharacterSetConversion.java
@@ -3,13 +3,10 @@ package net.ripe.db.whois.update.handler;
 import java.nio.charset.Charset;
 
 public class CharacterSetConversion {
-    public static final boolean isConvertableIntoLatin1( String value) {
-        boolean status = true;
 
-        Charset latin1Set = Charset.forName("ISO8859-1");
-        if(value != null  && ! latin1Set.newEncoder().canEncode(value)) {
-            status = false;
-        }
-        return status;
+    private final static Charset latin1Set = Charset.forName("ISO8859-1");
+
+    public static boolean isConvertableIntoLatin1(String value) {
+        return (value == null) || latin1Set.newEncoder().canEncode(value);
     }
 }
