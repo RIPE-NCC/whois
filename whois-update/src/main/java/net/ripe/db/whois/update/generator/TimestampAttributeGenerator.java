e96647269617724af3d2aa67a3e3b32f28ec4f85
Theodoros Polychniatis <tpolychnia@ripe.net>
2015-02-20 14:26:13 +0100

    used import static and added some finals

--- whois-update/src/main/java/net/ripe/db/whois/update/generator/TimestampAttributeGenerator.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/generator/TimestampAttributeGenerator.java
@@ -11,10 +11,15 @@ import net.ripe.db.whois.update.domain.Update;
 import net.ripe.db.whois.update.domain.UpdateContext;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
 import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static net.ripe.db.whois.common.rpsl.AttributeType.CREATED;
+import static net.ripe.db.whois.common.rpsl.AttributeType.LAST_MODIFIED;
+import static net.ripe.db.whois.update.domain.Action.CREATE;
+import static net.ripe.db.whois.update.domain.Action.DELETE;
+import static net.ripe.db.whois.update.domain.Action.MODIFY;
 
 @Component
 public class TimestampAttributeGenerator extends AttributeGenerator {
@@ -27,28 +32,29 @@ public class TimestampAttributeGenerator extends AttributeGenerator {
 
     @Override
     public RpslObject generateAttributes(final RpslObject originalObject, final RpslObject updatedObject, final Update update, final UpdateContext updateContext) {
-        RpslObjectBuilder builder = new RpslObjectBuilder(updatedObject);
+        final RpslObjectBuilder builder = new RpslObjectBuilder(updatedObject);
 
-        Action action = updateContext.getAction(update);
-        if (action == Action.CREATE || action == Action.MODIFY || action == Action.DELETE) {
+        final Action action = updateContext.getAction(update);
+
+        if (action == CREATE || action == MODIFY || action == DELETE) {
             if( updatedObject.containsAttribute(AttributeType.CREATED)) {
-                updateContext.addMessage(update, ValidationMessages.suppliedAttributeReplacedWithGeneratedValue(AttributeType.CREATED));
-                builder.removeAttributeType(AttributeType.CREATED);
+                updateContext.addMessage(update, ValidationMessages.suppliedAttributeReplacedWithGeneratedValue(CREATED));
+                builder.removeAttributeType(CREATED);
             }
-            if( updatedObject.containsAttribute(AttributeType.LAST_MODIFIED)) {
-                updateContext.addMessage(update, ValidationMessages.suppliedAttributeReplacedWithGeneratedValue(AttributeType.LAST_MODIFIED));
-                builder.removeAttributeType(AttributeType.LAST_MODIFIED);
+            if( updatedObject.containsAttribute(LAST_MODIFIED)) {
+                updateContext.addMessage(update, ValidationMessages.suppliedAttributeReplacedWithGeneratedValue(LAST_MODIFIED));
+                builder.removeAttributeType(LAST_MODIFIED);
             }
         }
 
-        if (action == Action.CREATE || action == Action.MODIFY ) {
-            DateTime now = dateTimeProvider.getCurrentUtcTime();
-            String timestampString = now.toString(ISODateTimeFormat.dateTimeNoMillis().withZone(DateTimeZone.UTC));
+        if (action == CREATE || action == MODIFY ) {
+            final DateTime now = dateTimeProvider.getCurrentUtcTime();
+            final String timestampString = now.toString(ISODateTimeFormat.dateTimeNoMillis().withZone(DateTimeZone.UTC));
 
-            if (updateContext.getAction(update) == Action.CREATE) {
-                builder.addAttributeSorted(new RpslAttribute(AttributeType.CREATED, timestampString));
+            if (updateContext.getAction(update) == CREATE) {
+                builder.addAttributeSorted(new RpslAttribute(CREATED, timestampString));
             }
-            builder.addAttributeSorted(new RpslAttribute(AttributeType.LAST_MODIFIED, timestampString));
+            builder.addAttributeSorted(new RpslAttribute(LAST_MODIFIED, timestampString));
         }
 
         return builder.get();
