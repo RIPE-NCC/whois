96aab4c116a5a098effe6338599cef6349cb63ce
Edward Shryane <eshryane@ripe.net>
2015-03-02 12:46:44 +0100

    Use a single variable internally in TestDateTimeProvider (LocalDateTime stores its time as UTC internally). Simplified integration tests (compare attribute values as Strings).

--- whois-api/src/test/java/net/ripe/db/whois/api/rest/TimestampsOffRestServiceTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/rest/TimestampsOffRestServiceTestIntegration.java
@@ -7,15 +7,15 @@ import net.ripe.db.whois.api.rest.domain.WhoisResources;
 import net.ripe.db.whois.api.rest.mapper.FormattedClientAttributeMapper;
 import net.ripe.db.whois.api.rest.mapper.WhoisObjectMapper;
 import net.ripe.db.whois.common.IntegrationTest;
-import net.ripe.db.whois.common.TestDateTimeProvider;
 import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.RpslAttribute;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.common.rpsl.RpslObjectBuilder;
 import net.ripe.db.whois.common.rpsl.TestTimestampsMode;
 import net.ripe.db.whois.common.rpsl.ValidationMessages;
-import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,6 +33,8 @@ import static org.junit.Assert.assertThat;
 
 @Category(IntegrationTest.class)
 public class TimestampsOffRestServiceTestIntegration extends AbstractIntegrationTest {
+
+    private static final DateTimeFormatter UTC_DATE_TIME_FORMATTER = ISODateTimeFormat.dateTimeNoMillis().withZone(DateTimeZone.UTC);
 
     private static final RpslObject PAULETH_PALTHEN = RpslObject.parse("" +
             "person:    Pauleth Palthen\n" +
@@ -78,7 +80,6 @@ public class TimestampsOffRestServiceTestIntegration extends AbstractIntegration
             "source:    TEST\n");
 
     @Autowired private WhoisObjectMapper whoisObjectMapper;
-    @Autowired private TestDateTimeProvider dateTimeProvider;
     @Autowired private TestTimestampsMode testTimestampsMode;
 
     @Before
@@ -88,19 +89,16 @@ public class TimestampsOffRestServiceTestIntegration extends AbstractIntegration
         databaseHelper.addObject(OWNER_MNT);
         databaseHelper.updateObject(TEST_PERSON);
         databaseHelper.updateObject(TEST_ROLE);
-        dateTimeProvider.setTime(new DateTime());
+        testDateTimeProvider.setTime(LocalDateTime.parse("2001-02-04T17:00:00"));
     }
 
     @Test
     public void mode_off_created_last_modified_raise_warnings() throws Exception {
-
         testTimestampsMode.setTimestampsOff(true);
 
-        final String currentDate = ISODateTimeFormat.dateTimeNoMillis().withZone(DateTimeZone.UTC).print(dateTimeProvider.getCurrentUtcTime());
-
         final RpslObject object = new RpslObjectBuilder(PAULETH_PALTHEN)
-                .addAttributeAfter(new RpslAttribute("created", currentDate), AttributeType.MNT_BY)
-                .addAttributeAfter(new RpslAttribute("last-modified", currentDate), AttributeType.MNT_BY)
+                .addAttributeAfter(new RpslAttribute("created", "2001-02-04T17:00:00Z"), AttributeType.MNT_BY)
+                .addAttributeAfter(new RpslAttribute("last-modified", "2001-02-04T17:00:00Z"), AttributeType.MNT_BY)
                 .get();
 
         try {
