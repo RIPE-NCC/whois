a81bbbcbfdc726212edb146fdf6015b3384df582
Angela Sjöholm <asjoholm@ripe.net>
2015-03-02 13:51:56 +0100

    refactored to add warnings on attribute level

--- whois-update/src/main/java/net/ripe/db/whois/update/generator/TimestampAttributeGenerator.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/generator/TimestampAttributeGenerator.java
@@ -1,5 +1,6 @@
 package net.ripe.db.whois.update.generator;
 
+import com.google.common.collect.Lists;
 import net.ripe.db.whois.common.DateTimeProvider;
 import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.RpslAttribute;
@@ -15,6 +16,8 @@ import org.joda.time.DateTimeZone;
 import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import static net.ripe.db.whois.common.rpsl.AttributeType.CREATED;
 import static net.ripe.db.whois.common.rpsl.AttributeType.LAST_MODIFIED;
@@ -35,43 +38,54 @@ public class TimestampAttributeGenerator extends AttributeGenerator {
 
     @Override
     public RpslObject generateAttributes(final RpslObject originalObject, final RpslObject updatedObject, final Update update, final UpdateContext updateContext) {
-
         if (timestampsMode.isTimestampsOff()) {
             return updatedObject;
         }
 
         final RpslObjectBuilder builder = new RpslObjectBuilder(updatedObject);
+        List<AttributeType> warnFor = null;
 
         final Action action = updateContext.getAction(update);
         if (action == CREATE || action == MODIFY) {
-            cleanupTimestampAttributes(builder, updatedObject, update, updateContext, true);
+            warnFor = cleanupTimestampAttributesAndGatherWarnings(builder, updatedObject, true);
         } else if (action == DELETE) {
-            cleanupTimestampAttributes(builder, updatedObject, update, updateContext, false);
+            warnFor = cleanupTimestampAttributesAndGatherWarnings(builder, updatedObject, false);
         }
 
         if (action == CREATE || action == MODIFY || action == DELETE) {
             generateTimestampAttributes(builder, originalObject, update, updateContext);
         }
 
-        return builder.get();
+        final RpslObject resultObject = builder.get();
+        addWarnings(warnFor, resultObject, update, updateContext);
+
+        return resultObject;
     }
 
-    private void cleanupTimestampAttributes(final RpslObjectBuilder builder, final RpslObject updatedObject, final Update update, final UpdateContext updateContext, final boolean withWarnings) {
-        if (updatedObject.containsAttribute(AttributeType.CREATED)) {
-            builder.removeAttributeType(CREATED);
-            if (withWarnings) {
-                updateContext.addMessage(update, ValidationMessages.suppliedAttributeReplacedWithGeneratedValue(CREATED));
-            }
-        }
-        if (updatedObject.containsAttribute(LAST_MODIFIED)) {
-            builder.removeAttributeType(LAST_MODIFIED);
-            if (withWarnings) {
-                updateContext.addMessage(update, ValidationMessages.suppliedAttributeReplacedWithGeneratedValue(LAST_MODIFIED));
-            }
+    private void addWarnings(final List<AttributeType> warnFor, final RpslObject rpslObject, final Update update, final UpdateContext updateContext) {
+        for (AttributeType attributeType : warnFor) {
+            updateContext.addMessage(update, rpslObject.findAttribute(attributeType), ValidationMessages.suppliedAttributeReplacedWithGeneratedValue(attributeType));
         }
     }
 
-    private void generateTimestampAttributes( final RpslObjectBuilder builder, final RpslObject originalObject, final Update update, final UpdateContext updateContext) {
+    private List<AttributeType> cleanupTimestampAttributesAndGatherWarnings(final RpslObjectBuilder builder, final RpslObject updatedObject, final boolean addWarnings) {
+        final List<AttributeType> warnFor = Lists.newArrayList();
+        removeAttribute(updatedObject, builder, CREATED, warnFor, addWarnings);
+        removeAttribute(updatedObject, builder, LAST_MODIFIED, warnFor, addWarnings);
+
+        return warnFor;
+    }
+
+    private void removeAttribute(final RpslObject rpslObject, final RpslObjectBuilder builder, final AttributeType attributeType, final List<AttributeType> warnFor, final boolean addWarnings) {
+        if (rpslObject.containsAttribute(attributeType)) {
+            builder.removeAttributeType(attributeType);
+            if (addWarnings) {
+                warnFor.add(attributeType);
+            }
+        }
+    }
+
+    private void generateTimestampAttributes(final RpslObjectBuilder builder, final RpslObject originalObject, final Update update, final UpdateContext updateContext) {
         final Action action = updateContext.getAction(update);
 
         final DateTime now = dateTimeProvider.getCurrentDateTimeUtc();
