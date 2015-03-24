b019c647186888a1cb5806103d313c8378158f71
Marc Grol <mgrol@xebia.com>
2015-03-24 10:01:27 +0100

    Use timestamps-off-mode also when generating  split-files

--- whois-scheduler/src/main/java/net/ripe/db/whois/scheduler/task/export/ExportFileWriter.java
+++ whois-scheduler/src/main/java/net/ripe/db/whois/scheduler/task/export/ExportFileWriter.java
@@ -3,8 +3,10 @@ package net.ripe.db.whois.scheduler.task.export;
 import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
 import net.ripe.db.whois.common.domain.Tag;
+import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.common.rpsl.RpslObject;
+import net.ripe.db.whois.common.rpsl.RpslObjectBuilder;
 import net.ripe.db.whois.query.QueryMessages;
 import net.ripe.db.whois.query.domain.TagResponseObject;
 import org.slf4j.Logger;
@@ -27,11 +29,14 @@ class ExportFileWriter {
     private final FilenameStrategy filenameStrategy;
     private final DecorationStrategy decorationStrategy;
     private final Map<String, Writer> writerMap = Maps.newHashMap();
+    // TODO: remove once timestamps are always on (MG)
+    private final boolean timestampsOff;
 
-    protected ExportFileWriter(final File baseDir, final FilenameStrategy filenameStrategy, final DecorationStrategy decorationStrategy) {
+    protected ExportFileWriter(final File baseDir, final FilenameStrategy filenameStrategy, final DecorationStrategy decorationStrategy, final boolean timestampsOff) {
         this.baseDir = baseDir;
         this.filenameStrategy = filenameStrategy;
         this.decorationStrategy = decorationStrategy;
+        this.timestampsOff = timestampsOff;
 
         for (final ObjectType objectType : ObjectType.values()) {
             final String filename = filenameStrategy.getFilename(objectType);
@@ -47,16 +52,32 @@ class ExportFileWriter {
         final String filename = filenameStrategy.getFilename(object.getType());
         final Writer writer = getWriter(filename);
 
-        final RpslObject decoratedObject = decorationStrategy.decorate(object);
+        RpslObject decoratedObject = decorationStrategy.decorate(object);
         if (decoratedObject != null) {
+            // TODO: remove once timestamps are always on (MG)
+            RpslObject strippedDecoratedObject = decoratedObject;
+            if( timestampsOff == true ) {
+                strippedDecoratedObject = stripTimestampAttributes(decoratedObject);
+            }
             writer.write('\n');
-            decoratedObject.writeTo(writer);
+            strippedDecoratedObject.writeTo(writer);
 
             if (!tags.isEmpty()) {
                 writer.write('\n');
                 writer.write(new TagResponseObject(decoratedObject.getKey(), tags).toString());
             }
         }
+    }
+
+    private RpslObject stripTimestampAttributes(final RpslObject object) {
+        RpslObjectBuilder builder = new RpslObjectBuilder(object);
+        if (object.containsAttribute(AttributeType.LAST_MODIFIED)) {
+            builder.removeAttributeType(AttributeType.LAST_MODIFIED);
+        }
+        if (object.containsAttribute(AttributeType.CREATED)) {
+            builder.removeAttributeType(AttributeType.CREATED);
+        }
+        return builder.get();
     }
 
     void close() {
