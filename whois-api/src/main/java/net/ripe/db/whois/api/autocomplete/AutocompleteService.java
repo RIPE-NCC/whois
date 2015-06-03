40a1faa1c846c6afa1d92c6febd3347c4a3d6034
Edward Shryane <eshryane@ripe.net>
2015-06-03 16:19:51 +0200

    Autowired AutocompleteService. Expose freetext search so that Autocomplete can use it. Added integration tests (search by string & object type)

--- whois-api/src/main/java/net/ripe/db/whois/api/autocomplete/AutocompleteService.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/autocomplete/AutocompleteService.java
@@ -1,54 +1,58 @@
 package net.ripe.db.whois.api.autocomplete;
 
+import com.google.common.base.Strings;
+import net.ripe.db.whois.api.freetext.FreeTextSearch;
+import net.ripe.db.whois.api.freetext.SearchResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 /**
  * Autocomplete - Suggestions - Typeahead API
  */
 @Component
+@Path("/autocomplete")
 public class AutocompleteService {
-//
-//    private static final Logger LOGGER = LoggerFactory.getLogger(AutocompleteService.class);
-//
-//    private final FreeTextIndex freeTextIndex;
-//    private final AnalyzingInfixSuggester suggester;
-//
-//    @Autowired
-//    public AutocompleteService(final FreeTextIndex freeTextIndex) throws IOException {
-//        this.freeTextIndex = freeTextIndex;
-//        this.suggester = freeTextIndex.createSuggester();
-//    }
-//
-//    public void lookup(final String request, final PrintWriter writer) throws Exception {
-//        LOGGER.info("lookup: {}", request);
-//
-//
-//        suggester.build(new DocumentDictionary(freeTextIndex.getReader(), "primary-key", "primary-key"));
-//
-//        final String name = "AA";
-//
-//       try {
-//            List<Lookup.LookupResult> results;
-//            HashSet<BytesRef> contexts = new HashSet<BytesRef>();
-//            contexts.add(new BytesRef("primary-key".getBytes("UTF8")));
-//
-//            // Do the actual lookup.  We ask for the top 2 results.
-//
-//            final int hits = 1;
-//            final boolean doHighlights = false;
-//           final boolean allTermsRequired = false;
-//
-//            results = suggester.lookup(name, hits, allTermsRequired, doHighlights);
-//
-//           LOGGER.info("results = {}", results.size());
-//
-//            for (Lookup.LookupResult result : results) {
-//                LOGGER.info("RESULT = " + result.key);
-//            }
-//        } catch (IOException e) {
-//            LOGGER.error(e.getMessage(), e);
-//        }
-//
-//    }
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AutocompleteService.class);
+
+    private static final int MINIMUM_PREFIX_LENGTH = 3;
+
+    private static final int RESULTS_LIMIT = 10;
+
+    private final FreeTextSearch freeTextSearch;
+
+    @Autowired
+    public AutocompleteService(final FreeTextSearch freeTextSearch) throws IOException {
+        this.freeTextSearch = freeTextSearch;
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response lookup(
+                @QueryParam("q") final String queryString,
+                @QueryParam("t") final ObjectTypeParameter objectType) {
+
+        if (Strings.isNullOrEmpty(queryString) || queryString.length() <= MINIMUM_PREFIX_LENGTH) {
+            throw new IllegalArgumentException("q parameter is required, and must be at least " + MINIMUM_PREFIX_LENGTH + " characters long");
+        }
+
+        final SearchResponse searchResponse;
+        try {
+            searchResponse = freeTextSearch.freeTextSearch(String.format("q=%s+AND+object-type%%3A%s", queryString, objectType.toString()));
+        } catch (IOException e) {
+            throw new IllegalStateException("Query failed.");
+        }
+
+        return Response.ok("num found = " + searchResponse.getResult().getNumFound()).build();
+    }
 }
