4dc2110d2019de0565ea50918b001b44671fad93
Edward Shryane <eshryane@ripe.net>
2015-08-03 15:54:46 +0200

    Fixed compile error

--- whois-client/src/test/java/net/ripe/db/whois/common/sso/CrowdClientTest.java
+++ whois-client/src/test/java/net/ripe/db/whois/common/sso/CrowdClientTest.java
@@ -56,7 +56,7 @@ public class CrowdClientTest {
     @Test
     public void login_success() {
         final String token = "xyz";
-        when(builder.post(any(Entity.class), any(Class.class))).thenReturn(
+        when(builder.<CrowdSession>post(any(Entity.class), any(Class.class))).thenReturn(
             new CrowdSession(
                 new CrowdUser("test@ripe.net", "Test User", true), token, "2033-01-30T16:38:27.369+11:00"));
 
@@ -85,7 +85,9 @@ public class CrowdClientTest {
 
     @Test
     public void get_user_session_success() throws Exception {
-        when(builder.get(CrowdSession.class)).thenReturn(new CrowdSession(new CrowdUser("test@ripe.net", "Test User", true), null, "2033-01-30T16:38:27.369+11:00"));
+        when(builder.get(CrowdSession.class)).thenReturn(
+            new CrowdSession(
+                new CrowdUser("test@ripe.net", "Test User", true), null, "2033-01-30T16:38:27.369+11:00"));
 
         final UserSession session = subject.getUserSession("token");
 
