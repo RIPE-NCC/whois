04662daea0b35f6d3fe93926954db7464c8bfcae
Thiago da Cruz <tdacruzper@ripe.net>
2015-11-12 17:01:09 +0100

    Create feature toggle for changed att (work in progress)

---
+++ whois-commons/src/main/java/net/ripe/db/whois/common/ChangedAttrFeatureToggle.java
@@ -0,0 +1,20 @@
+package net.ripe.db.whois.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChangedAttrFeatureToggle {
+
+    final private Boolean isAvailable;
+
+    @Autowired
+    public ChangedAttrFeatureToggle(@Value("${feature.toggle.changed.attr.available}") Boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public boolean isChangedAttrAvailable() {
+        return isAvailable;
+    }
+}
