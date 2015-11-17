6ba504ec4854f221211ee9c9d14bdff4661d0a18
Marc Grol <mgrol@xebia.com>
2015-11-17 14:46:53 +0100

    Added intermediate mode and ignore old mode

---
+++ whois-endtoend/src/test/java/net/ripe/db/whois/changedphase3/util/DumpRunner.java
@@ -0,0 +1,14 @@
+package net.ripe.db.whois.changedphase3.util;
+
+public class DumpRunner extends AbstactScenarioRunner {
+
+    public DumpRunner(final Context context) {
+        super(context);
+    }
+
+    public String getProtocolName() {
+        return "Dump";
+    }
+
+
+}
