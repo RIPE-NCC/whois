be3f278a5038bc0328c76fbffc207fe84184b82b
Marc Grol <mgrol@xebia.com>
2015-11-17 09:33:47 +0100

    Added tests for all old-mode mail-updates scenarios

--- whois-api/src/test/java/net/ripe/db/whois/api/changedphase3/AbstractChangedPhase3IntegrationTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/changedphase3/AbstractChangedPhase3IntegrationTest.java
@@ -1,11 +1,13 @@
 package net.ripe.db.whois.api.changedphase3;
 
 import net.ripe.db.whois.api.AbstractIntegrationTest;
+import net.ripe.db.whois.api.MailUpdatesTestSupport;
 import net.ripe.db.whois.api.changedphase3.util.Context;
 import net.ripe.db.whois.api.rest.mapper.WhoisObjectMapper;
 import net.ripe.db.whois.common.IntegrationTest;
 import net.ripe.db.whois.common.MaintenanceMode;
 import net.ripe.db.whois.common.rpsl.RpslObject;
+import net.ripe.db.whois.update.mail.MailSenderStub;
 import org.junit.Before;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +32,8 @@ public abstract class AbstractChangedPhase3IntegrationTest extends AbstractInteg
             "source:        TEST");
     @Autowired protected MaintenanceMode maintenanceMode;
     @Autowired private WhoisObjectMapper whoisObjectMapper;
+    @Autowired MailUpdatesTestSupport mailUpdatesTestSupport;
+    @Autowired MailSenderStub mailSenderStub;
     protected Context context;
 
     @Before
@@ -38,6 +42,6 @@ public abstract class AbstractChangedPhase3IntegrationTest extends AbstractInteg
         databaseHelper.addObject(OWNER_MNTNER);
         databaseHelper.updateObject(TEST_PERSON);
         maintenanceMode.set("FULL,FULL");
-        context = new Context(getPort(),getPort(),whoisObjectMapper );
+        context = new Context(getPort(),getPort(),whoisObjectMapper,mailUpdatesTestSupport,mailSenderStub );
     }
 }
--- whois-api/src/test/java/net/ripe/db/whois/api/changedphase3/util/MailUpdateRunner.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/changedphase3/util/MailUpdateRunner.java
@@ -1,5 +1,16 @@
 package net.ripe.db.whois.api.changedphase3.util;
 
+import net.ripe.db.whois.common.rpsl.RpslObject;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import java.io.IOException;
+
+import static net.ripe.db.whois.common.rpsl.RpslObjectFilter.buildGenericObject;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
 public class MailUpdateRunner extends AbstactScenarioRunner {
 
     public MailUpdateRunner(Context context) {
@@ -11,14 +22,68 @@ public class MailUpdateRunner extends AbstactScenarioRunner {
     }
 
     public void create(Scenario scenario) {
+        try {
+            verifyPreCondition(scenario);
 
+            RpslObject objectForScenario = objectForScenario(scenario);
+
+            final String response = context.getMailUpdatesTestSupport().insert("NEW", buildGenericObject(objectForScenario, "mntner: TESTING-MNT").toString() + "\npassword:123");
+            final MimeMessage message = context.getMailSenderStub().getMessage(response);
+
+            if (scenario.getResult() == Scenario.Result.SUCCESS) {
+                assertThat(message.getContent().toString(), containsString("Create SUCCEEDED: [mntner] TESTING-MNT"));
+                verifyPostCondition(scenario, Scenario.Result.SUCCESS);
+            } else {
+                assertThat(message.getContent().toString(), containsString("***Error:"));
+                verifyPostCondition(scenario, Scenario.Result.FAILED);
+            }
+
+        } catch( MessagingException  | IOException exc ) {
+            verifyPostCondition(scenario, Scenario.Result.FAILED);
+        }
     }
 
     public void modify(Scenario scenario) {
+        try {
+            verifyPreCondition(scenario);
 
+            RpslObject objectForScenario = addRemarks(objectForScenario(scenario));
+
+            final String response = context.getMailUpdatesTestSupport().insert("MODIFY", buildGenericObject(objectForScenario, "mntner: TESTING-MNT").toString() + "password:123");
+            final MimeMessage message = context.getMailSenderStub().getMessage(response);
+
+            if (scenario.getResult() == Scenario.Result.SUCCESS) {
+                assertThat(message.getContent().toString(), containsString("Modify SUCCEEDED: [mntner] TESTING-MNT"));
+                verifyPostCondition(scenario, Scenario.Result.SUCCESS);
+            } else {
+                assertThat(message.getContent().toString(), containsString("***Error:"));
+                verifyPostCondition(scenario, Scenario.Result.FAILED);
+            }
+
+        } catch( MessagingException  | IOException exc ) {
+            verifyPostCondition(scenario, Scenario.Result.FAILED);
+        }
     }
 
     public void delete(Scenario scenario) {
+        try {
+            verifyPreCondition(scenario);
 
+            RpslObject objectForScenario = objectForScenario(scenario);
+
+            final String response = context.getMailUpdatesTestSupport().insert("DELETE", buildGenericObject(objectForScenario, "mntner: TESTING-MNT").toString() + "delete: testing\npassword:123");
+            final MimeMessage message = context.getMailSenderStub().getMessage(response);
+
+            if (scenario.getResult() == Scenario.Result.SUCCESS) {
+                assertThat(message.getContent().toString(), containsString("Delete SUCCEEDED: [mntner] TESTING-MNT"));
+                verifyPostCondition(scenario, Scenario.Result.SUCCESS);
+            } else {
+                assertThat(message.getContent().toString(), containsString("***Error:"));
+                verifyPostCondition(scenario, Scenario.Result.FAILED);
+            }
+
+        } catch( MessagingException  | IOException exc ) {
+            verifyPostCondition(scenario, Scenario.Result.FAILED);
+        }
     }
 }
