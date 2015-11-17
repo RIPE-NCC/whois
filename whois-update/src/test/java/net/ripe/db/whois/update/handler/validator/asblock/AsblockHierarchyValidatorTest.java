22e1209377ddfd6bc9f1e7eeaacca1aeed2f4708
Marc Grol <mgrol@xebia.com>
2015-11-17 14:12:25 +0100

    Fixed tests for missing ObjectTemplateDependentTest

--- whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/asblock/AsblockHierarchyValidatorTest.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/asblock/AsblockHierarchyValidatorTest.java
@@ -2,6 +2,7 @@ package net.ripe.db.whois.update.handler.validator.asblock;
 
 import com.google.common.collect.Lists;
 import net.ripe.db.whois.common.dao.jdbc.JdbcRpslObjectDao;
+import net.ripe.db.whois.common.rpsl.ObjectTemplateDependentTest;
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.update.authentication.Subject;
@@ -22,7 +23,7 @@ import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 
-public class AsblockHierarchyValidatorTest {
+public class AsblockHierarchyValidatorTest extends ObjectTemplateDependentTest {
 
     @Mock PreparedUpdate update;
     @Mock UpdateContext updateContext;
--- whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/organisation/AbuseCNoLimitWarningValidatorTest.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/organisation/AbuseCNoLimitWarningValidatorTest.java
@@ -1,5 +1,6 @@
 package net.ripe.db.whois.update.handler.validator.organisation;
 
+import net.ripe.db.whois.common.rpsl.ObjectTemplateDependentTest;
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.update.domain.Action;
@@ -17,7 +18,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-public class AbuseCNoLimitWarningValidatorTest {
+public class AbuseCNoLimitWarningValidatorTest extends ObjectTemplateDependentTest {
     @Mock PreparedUpdate update;
     @Mock UpdateContext updateContext;
     @InjectMocks AbuseCNoLimitWarningValidator subject;
