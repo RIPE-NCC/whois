4b31e816801400cee5acb6131022bd9fe81273a8
Thiago da Cruz <tdacruzper@ripe.net>
2015-11-17 13:18:01 +0100

    Fix test

--- whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/domain/NServerValidatorTest.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/domain/NServerValidatorTest.java
@@ -1,6 +1,7 @@
 package net.ripe.db.whois.update.handler.validator.domain;
 
 import net.ripe.db.whois.common.rpsl.AttributeType;
+import net.ripe.db.whois.common.rpsl.ObjectTemplateDependentTest;
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.update.domain.Action;
@@ -18,7 +19,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-public class NServerValidatorTest {
+public class NServerValidatorTest extends ObjectTemplateDependentTest {
     @Mock PreparedUpdate update;
     @Mock UpdateContext updateContext;
 
