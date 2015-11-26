3fad72d576d1e3e3621e17926f9b0fd8db15bbcf
Edward Shryane <eshryane@ripe.net>
2015-11-26 13:15:52 +0100

    Test RpslObjectBuilder

---
+++ whois-client/src/test/java/net/ripe/db/whois/common/rpsl/RpslObjectBuilderTest.java
@@ -0,0 +1,51 @@
+package net.ripe.db.whois.common.rpsl;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RpslObjectBuilderTest {
+
+    @Test
+    public void remove_attribute_type_no_match() {
+        assertThat(
+            new RpslObjectBuilder(
+                "mntner: OWNER-MNT\n" +
+                "descr: test\n" +
+                "source: TEST").removeAttributeType(AttributeType.REMARKS).get(),
+            is(RpslObject.parse(
+                "mntner: OWNER-MNT\n" +
+                "descr: test\n" +
+                "source: TEST")));
+    }
+
+    @Test
+    public void remove_attribute_type_single_match() {
+        assertThat(
+            new RpslObjectBuilder(
+                "mntner: OWNER-MNT\n" +
+                "remarks: first\n" +
+                "descr: test\n" +
+                "source: TEST").removeAttributeType(AttributeType.REMARKS).get(),
+            is(RpslObject.parse(
+                "mntner: OWNER-MNT\n" +
+                "descr: test\n" +
+                "source: TEST")));
+    }
+
+    @Test
+    public void remove_attribute_type_multiple_matches() {
+        assertThat(
+            new RpslObjectBuilder(
+                "mntner: OWNER-MNT\n" +
+                "remarks: first\n" +
+                "descr: test\n" +
+                "remarks: second\n" +
+                "source: TEST").removeAttributeType(AttributeType.REMARKS).get(),
+            is(RpslObject.parse(
+                "mntner: OWNER-MNT\n" +
+                "descr: test\n" +
+                "source: TEST")));
+    }
+}
