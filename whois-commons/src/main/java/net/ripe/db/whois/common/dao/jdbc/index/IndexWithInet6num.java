90b0141d11eb79a65317f02d92c61a65e09e1bbb
Edward Shryane <eshryane@ripe.net>
2015-11-27 16:24:43 +0100

    There is still an issue when making a comparison between a Long value, and a VARCHAR column in the database. Always first convert to a String. Added an extra test (different values must not match).

--- whois-commons/src/main/java/net/ripe/db/whois/common/dao/jdbc/index/IndexWithInet6num.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/dao/jdbc/index/IndexWithInet6num.java
@@ -8,14 +8,18 @@ import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
 
 class IndexWithInet6num extends IndexStrategyWithSingleLookupTable {
+
     public IndexWithInet6num(final AttributeType attributeType) {
         super(attributeType, "inet6num");
     }
 
+    // MariaDB bug workaround: if 64-bit integer has its msb bit set, the comparison fails
+    // Always use Long.toString() instead of passing long for VARCHAR column
     @Override
     public int addToIndex(final JdbcTemplate jdbcTemplate, final RpslObjectInfo objectInfo, final RpslObject object, final String value) {
         final Ipv6Resource resource = Ipv6Resource.parse(objectInfo.getKey());
@@ -27,8 +31,8 @@ class IndexWithInet6num extends IndexStrategyWithSingleLookupTable {
         return jdbcTemplate.update(
                 "INSERT INTO inet6num (object_id, i6_msb, i6_lsb, prefix_length, netname) VALUES (?, ?, ?, ?, ?)",
                 objectInfo.getObjectId(),
-                Ipv6Resource.msb(resource.begin()),
-                Ipv6Resource.lsb(resource.begin()),
+                Long.toString(Ipv6Resource.msb(resource.begin())),
+                Long.toString(Ipv6Resource.lsb(resource.begin())),
                 resource.getPrefixLength(),
                 netname);
     }
@@ -40,6 +44,8 @@ class IndexWithInet6num extends IndexStrategyWithSingleLookupTable {
             return Collections.emptyList();
         }
 
+        // MariaDB bug workaround: if 64-bit integer has its msb bit set, the comparison fails
+        // Always use Long.toString() instead of passing long for VARCHAR column
         return jdbcTemplate.query(" " +
                 "SELECT l.object_id, l.object_type, l.pkey " +
                 "  FROM inet6num " +
@@ -47,11 +53,12 @@ class IndexWithInet6num extends IndexStrategyWithSingleLookupTable {
                 "  WHERE i6_msb = ? AND i6_lsb = ? AND prefix_length = ? " +
                 "  AND l.sequence_id != 0 ",
                 new RpslObjectInfoResultSetExtractor(),
-                Ipv6Resource.msb(resource.begin()),
-                Ipv6Resource.lsb(resource.begin()),
+                Long.toString(Ipv6Resource.msb(resource.begin())),
+                Long.toString(Ipv6Resource.lsb(resource.begin())),
                 resource.getPrefixLength());
     }
 
+    @Nullable
     private Ipv6Resource parseIpv6Resource(final String s) {
         try {
             return Ipv6Resource.parse(s);
