505163a0b4d4622eb5511658b263aa710648b022
Edward Shryane <eshryane@ripe.net>
2016-02-09 14:18:25 +0100

    Remove changed

--- whois-scheduler/src/test/java/net/ripe/db/whois/scheduler/task/grs/GrsSourceImporterTest.java
+++ whois-scheduler/src/test/java/net/ripe/db/whois/scheduler/task/grs/GrsSourceImporterTest.java
@@ -187,7 +187,6 @@ public class GrsSourceImporterTest {
                 "admin-c:        Not available\n" +
                 "tech-c:         See MAINT-AS1263\n" +
                 "mnt-by:         MAINT-AS1263\n" +
-                "changed:        DB-admin@merit.edu 19950201\n" +
                 "source:         APNIC-GRS"));
 
         verify(sanitizer).sanitize(any(RpslObject.class), any(ObjectMessages.class));
