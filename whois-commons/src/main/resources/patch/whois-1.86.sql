47fb8f77007c7db83be44996b5e5e080db1959b4
Marc Grol <mgrol@xebia.com>
2016-03-03 14:48:44 +0100

    Added specific patch script for whois release 1-86

--- whois-commons/src/main/resources/patch/whois-1.86.sql
+++ whois-commons/src/main/resources/patch/whois-1.86.sql
@@ -1,4 +1,6 @@
 
+use WHOIS_UPDATE_RIPE;
+
 DROP TABLE IF EXISTS `transfer_update_lock`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -10,5 +12,13 @@ CREATE TABLE `transfer_update_lock` (
 
 INSERT INTO transfer_update_lock VALUES (0);
 
-TRUNCATE version;
-INSERT INTO version VALUES ('whois-1.86');
+truncate ACL_RIPE.version; INSERT INTO ACL_RIPE.version VALUES ('whois-1.86');
+truncate INTERNALS_RIPE.version; INSERT INTO INTERNALS_RIPE.version VALUES ('whois-1.86');
+truncate MAILUPDATES_RIPE.version; INSERT INTO MAILUPDATES_RIPE.version VALUES ('whois-1.86');
+truncate WHOIS_MIRROR_AFRINIC_GRS.version; INSERT INTO WHOIS_MIRROR_AFRINIC_GRS.version VALUES ('whois-1.86');
+truncate WHOIS_MIRROR_APNIC_GRS.version; INSERT INTO WHOIS_MIRROR_APNIC_GRS.version VALUES ('whois-1.86');
+truncate WHOIS_MIRROR_ARIN_GRS.version; INSERT INTO WHOIS_MIRROR_ARIN_GRS.version VALUES ('whois-1.86');
+truncate WHOIS_MIRROR_JPIRR_GRS.version; INSERT INTO WHOIS_MIRROR_JPIRR_GRS.version VALUES ('whois-1.86');
+truncate WHOIS_MIRROR_LACNIC_GRS.version; INSERT INTO WHOIS_MIRROR_LACNIC_GRS.version VALUES ('whois-1.86');
+truncate WHOIS_MIRROR_RADB_GRS.version; INSERT INTO WHOIS_MIRROR_RADB_GRS.version VALUES ('whois-1.86');
+truncate WHOIS_UPDATE_RIPE.version; INSERT INTO WHOIS_UPDATE_RIPE.version VALUES ('whois-1.86');
