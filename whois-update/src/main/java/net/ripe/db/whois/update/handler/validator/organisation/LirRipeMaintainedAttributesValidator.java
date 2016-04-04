e86d523b2a23f3215383493b0802e9aa95fd2398
Harrie Hazewinkel <hhazewinkel@gmail.com>
2016-04-04 13:46:03 +0200

    Add logging context

--- whois-update/src/main/java/net/ripe/db/whois/update/handler/validator/organisation/LirRipeMaintainedAttributesValidator.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/handler/validator/organisation/LirRipeMaintainedAttributesValidator.java
@@ -13,12 +13,15 @@ import net.ripe.db.whois.update.domain.PreparedUpdate;
 import net.ripe.db.whois.update.domain.UpdateContext;
 import net.ripe.db.whois.update.domain.UpdateMessages;
 import net.ripe.db.whois.update.handler.validator.BusinessRuleValidator;
+import net.ripe.db.whois.update.log.LoggerContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-
 @Component
+// Validates that RIPE NCC maintained attributes are not changed for an LIR
 public class LirRipeMaintainedAttributesValidator implements BusinessRuleValidator {
+
+    private final LoggerContext loggerContext;
 
     private static final ImmutableList<Action> ACTIONS = ImmutableList.of(Action.MODIFY);
     private static final ImmutableList<ObjectType> TYPES = ImmutableList.of(ObjectType.ORGANISATION);
@@ -31,21 +34,29 @@ public class LirRipeMaintainedAttributesValidator implements BusinessRuleValidat
             AttributeType.E_MAIL,
             AttributeType.ORG_NAME);
 
+    @Autowired
+    public LirRipeMaintainedAttributesValidator(final LoggerContext loggerContext) {
+        this.loggerContext = loggerContext;
+    }
+
     @Override
     public void validate(final PreparedUpdate update, final UpdateContext updateContext) {
         final Subject subject = updateContext.getSubject(update);
         if (subject.hasPrincipal(Principal.OVERRIDE_MAINTAINER)) {
+            log(update, "organisation update with override");
             return;
         }
 
         final RpslObject originalObject = update.getReferenceObject();
         if (!LIR.equals(originalObject.getValueForAttribute(AttributeType.ORG_TYPE))) {
+            log(update, "organisation update is not for an LIR");
             return;
         }
 
         final RpslObject updatedObject = update.getUpdatedObject();
         ATTRIBUTES.forEach(attributeType -> {
             if (orgAttributeChanged(originalObject, updatedObject, attributeType)) {
+                log(update, "organisation update has ripe maintained attribute");
                 updateContext.addMessage(update, UpdateMessages.authorisationRequiredForAttrChange(attributeType));
             }
         });
@@ -57,6 +68,10 @@ public class LirRipeMaintainedAttributesValidator implements BusinessRuleValidat
         return !Iterables.elementsEqual(
                 Iterables.filter(originalObject.getValuesForAttribute(attributeType), CIString.class),
                 Iterables.filter(updatedObject.getValuesForAttribute(attributeType), CIString.class));
+    }
+
+    private void log(final PreparedUpdate update, final String message) {
+        loggerContext.logString(update.getUpdate(), getClass().getCanonicalName(), message);
     }
 
     @Override
