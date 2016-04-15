39ba4f062f6b38190f218b85337cd86aef192cc9
Rob Miller <rmiller@ripe.net>
2016-04-15 17:59:36 +0200

    stricter checking of ds-rdata: domain attr so bind doesn't break on bad digests

---
+++ whois-client/src/main/java/net/ripe/db/whois/common/rpsl/attrs/DsRdataDigestType.java
@@ -0,0 +1,49 @@
+package net.ripe.db.whois.common.rpsl.attrs;
+
+
+import java.util.regex.Pattern;
+
+public enum DsRdataDigestType {
+    SHA1(1, "^[0-9a-fA-F]{40}$"),
+    SHA256(2, "^[0-9a-fA-F]{64}$"),
+    GOSTR341194(3, "^[0-9a-fA-F]{64}$"),
+    SHA384(4, "^[0-9a-fA-F]{96}$");
+
+    private final int digestType;
+    private final String validationPattern;
+
+    DsRdataDigestType(int digestType, String validationPattern) {
+        this.digestType = digestType;
+        this.validationPattern = validationPattern;
+    }
+
+    public static boolean typeSupported(int digestType) {
+        switch (digestType) {
+            case 1: // SHA-1
+            case 2: // SHA-256
+            case 3: // GOST R 34.11-94
+            case 4: // SHA-384
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public static boolean validateLength(int digestType, String digest) {
+        switch(digestType) {
+            case 1: // SHA-1
+                return Pattern.matches(SHA1.validationPattern, digest);
+            case 2: // SHA-256
+                return Pattern.matches(SHA256.validationPattern, digest);
+            case 3: // GOST R 34.11-94
+                return Pattern.matches(GOSTR341194.validationPattern, digest);
+            case 4: // SHA-384
+                return Pattern.matches(SHA384.validationPattern, digest);
+            default:
+                return false; // should never get here
+        }
+    }
+
+
+
+}
--- whois-client/src/test/java/net/ripe/db/whois/common/rpsl/attrs/DsRdataTest.java
+++ whois-client/src/test/java/net/ripe/db/whois/common/rpsl/attrs/DsRdataTest.java
@@ -1,9 +1,15 @@
 package net.ripe.db.whois.common.rpsl.attrs;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import javax.ws.rs.BadRequestException;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class DsRdataTest {
 
@@ -19,5 +25,49 @@ public class DsRdataTest {
                 is("9520 8 1 EA17B8C10043303DDE17B55AAB18FBDFF2066176"));
         assertThat(DsRdata.parse("9520 8 2 ( 59EEB479C70A53DC1B14786F0360AD9DB6CF477C73B0 E4FCB12788DE2F2E528F )").toString(),
                 is("9520 8 2 59EEB479C70A53DC1B14786F0360AD9DB6CF477C73B0E4FCB12788DE2F2E528F"));
+        // unknown type
+        assertThat(DsRdata.parse("33841 10 5 B2B9053FFDE225CDB87012E914164E68B389EDC2821103546926859E76EAA259B2B9053FFDE225CDB87012E914164E68B389EDC2821103546926859E76EAA259B2B9053FFDE225CDB87012E914164E68B389EDC2821103546926859E76EAA259").toString(),
+                is("33841 10 5 B2B9053FFDE225CDB87012E914164E68B389EDC2821103546926859E76EAA259B2B9053FFDE225CDB87012E914164E68B389EDC2821103546926859E76EAA259B2B9053FFDE225CDB87012E914164E68B389EDC2821103546926859E76EAA259"));
     }
+
+    @Test
+    public void lengthTest() {
+        assertThat(DsRdata.parse("31122 5 2 3e0314f7c268574c0ab082d77779c4dc3d2bcf6620fe934efb912ba4fcd7f360").toString(),
+                is("31122 5 2 3e0314f7c268574c0ab082d77779c4dc3d2bcf6620fe934efb912ba4fcd7f360"));
+        assertThat(DsRdata.parse("52314 5 1 93B5837D4E5C063A3728FAA72BA64068F89B39DF").toString(),
+                is("52314 5 1 93B5837D4E5C063A3728FAA72BA64068F89B39DF"));
+        assertThat(DsRdata.parse("17729 8 4 6FA0EF598C528C860630AE82676B9CCF66161284960E81622E68C2F69ADBAA6215970F92214CCED9357B20714A36BDF6").toString(),
+                is("17729 8 4 6FA0EF598C528C860630AE82676B9CCF66161284960E81622E68C2F69ADBAA6215970F92214CCED9357B20714A36BDF6"));
+        assertThat(DsRdata.parse("33841 5 3 B2B9053FFDE225CDB87012E914164E68B389EDC2821103546926859E76EAA259").toString(),
+                is("33841 5 3 B2B9053FFDE225CDB87012E914164E68B389EDC2821103546926859E76EAA259"));
+    }
+
+    @Test
+    public void failureTests() {
+      /*  thrown.expect(AttributeParseException.class);  IN PROGRESS!!
+
+        // too long for type
+        thrown.expectMessage("Digest format is invalid for digest type");
+        DsRdata.parse("31122 5 2 1233e0314f7c268574c0ab082d77779c4dc3d2bcf6620fe934efb912ba4fcd7f360");
+
+
+        thrown.expectMessage("Invalid keytag");
+        // keytag too large
+        DsRdata.parse("31122 5 2 3e0314f7c268574c0ab082d77779c4dc3d2bcf6620fe934efb912ba4fcd7f360");*/
+    }
+
+
+
+//    private void validate(final String input, final String output) {
+//        try {
+//            DsRdata.parse(input);
+//            fail();
+//        } catch (Exception e) {
+//            assertThat(e.getMessage(), containsString("jdijdid"))
+//        }
+//
+//
+//    }
+
+
 }
