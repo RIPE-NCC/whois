1ad48db14087d8ef044ded733c75db447b982b2f
Thiago da Cruz <tdacruzper@ripe.net>
2016-05-03 16:36:37 +0200

    Add test for 'modify non-lir attributes with lir password should be possible'

--- whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/update/LirOrganisationAttributeValidationSpec.groovy
+++ whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/update/LirOrganisationAttributeValidationSpec.groovy
@@ -9,7 +9,8 @@ class  LirOrganisationAttributeValidationSpec extends BaseQueryUpdateSpec {
 
     @Override
     Map<String, String> getTransients() {
-        ["LIR-ORG": """\
+        [
+        "LIR-ORG": """\
             organisation:   AUTO-1
             org-name:       Reseaux IP Europeens Network Coordination Centre (RIPE NCC)
             org-type:       LIR
@@ -24,10 +25,25 @@ class  LirOrganisationAttributeValidationSpec extends BaseQueryUpdateSpec {
             mnt-by:         RIPE-NCC-HM-MNT
             mnt-by:         LIR-MNT
             source:         TEST
+        """,
+        "NON-LIR-ORG": """\
+            organisation:   AUTO-1
+            org-name:       Reseaux IP Europeens Network Coordination Centre (RIPE NCC)
+            org-type:       RIR
+            descr:          RIPE NCC Operations
+            address:        P.O. Box 10096
+            phone:          +31205354444
+            fax-no:         +31205354445
+            e-mail:         ncc@ripe.net
+            admin-c:        TP1-TEST
+            abuse-c:        AH1-TEST
+            mnt-ref:        LIR-MNT
+            mnt-by:         LIR-MNT
+            source:         TEST
         """
+
         ]
     }
-
 
     def "modify lir attributes with lir password should not be possible"() {
         given:
@@ -69,6 +85,46 @@ class  LirOrganisationAttributeValidationSpec extends BaseQueryUpdateSpec {
 
         ack.errorMessagesFor("Modify", "[organisation] ORG-RIEN1-TEST") == [
                 "Organisation \"address:\" can only be changed by the RIPE NCC for this organisation. Please contact \"ncc@ripe.net\" to change it."]
+
+    }
+
+    def "modify non-lir attributes with lir password should be possible"() {
+        given:
+        def ack1 = syncUpdate(getTransient("NON-LIR-ORG") + "override: denis,override1")
+        ack1.toString()
+
+        expect:
+        queryObject("-GBr -T organisation ORG-RIEN1-TEST", "organisation", "ORG-RIEN1-TEST")
+
+        when:
+        def ack = syncUpdateWithResponse("""
+            organisation:   ORG-RIEN1-TEST
+            org-name:       Reseaux IP Europeens Network Coordination Centre (RIPE NCC)
+            org-type:       RIR
+            descr:          RIPE NCC Operations
+            address:        Different P.O. Box 10096
+            phone:          +31205354444
+            fax-no:         +31205354445
+            e-mail:         ncc@ripe.net
+            admin-c:        TP1-TEST
+            abuse-c:        AH1-TEST
+            mnt-ref:        LIR-MNT
+            mnt-by:         LIR-MNT
+            source:         TEST
+            password: lir
+        """.stripIndent()
+        )
+
+        then:
+
+        ack.success
+
+        ack.summary.nrFound == 1
+        ack.summary.assertSuccess(1, 0, 1, 0, 0)
+        ack.summary.assertErrors(0, 0, 0, 0)
+
+        ack.countErrorWarnInfo(0, 0, 0)
+        ack.successes.any { it.operation == "Modify" && it.key == "[organisation] ORG-RIEN1-TEST" }
 
     }
 }
