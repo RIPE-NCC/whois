307032138f53e7ec49a1901271b3e9316b1b61a0
Edward Shryane <eshryane@ripe.net>
2016-05-04 17:46:55 +0200

    (re-)added version number to whois-1.86 patch.

--- whois-commons/src/main/resources/patch/whois-1.86.sql
+++ whois-commons/src/main/resources/patch/whois-1.86.sql
@@ -8,3 +8,6 @@ CREATE TABLE `transfer_update_lock` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 INSERT INTO transfer_update_lock VALUES (0);
+
+TRUNCATE version;
+INSERT INTO version VALUES ('whois-1.86');
