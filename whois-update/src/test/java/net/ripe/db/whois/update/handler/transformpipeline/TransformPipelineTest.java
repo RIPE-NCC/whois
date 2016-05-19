b7fb6167f434505dc10ef2f67bad1a7529aca894
Harrie Hazewinkel <hhazewinkel@gmail.com>
2016-05-19 11:12:43 +0200

    Pipeline Transformer (#378)
        - latin1

--- whois-update/src/test/java/net/ripe/db/whois/update/handler/transformpipeline/TransformPipelineTest.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/handler/transformpipeline/TransformPipelineTest.java
@@ -1,11 +1,16 @@
 package net.ripe.db.whois.update.handler.transformpipeline;
 
+import net.ripe.db.whois.common.rpsl.AttributeType;
+import net.ripe.db.whois.common.rpsl.RpslAttribute;
 import net.ripe.db.whois.common.rpsl.RpslObject;
+import net.ripe.db.whois.common.rpsl.RpslObjectBuilder;
 import net.ripe.db.whois.update.domain.Action;
 import net.ripe.db.whois.update.domain.Operation;
 import net.ripe.db.whois.update.domain.Paragraph;
 import net.ripe.db.whois.update.domain.Update;
 import net.ripe.db.whois.update.domain.UpdateContext;
+import net.ripe.db.whois.update.domain.UpdateMessages;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -21,6 +26,47 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TransformPipelineTest {
+
+    LatinTransformer latinTransformer;
+    TransformPipeline subject;
+
+    @Before
+    public void setUp() throws Exception {
+        latinTransformer = new LatinTransformer();
+        subject = new TransformPipeline(new Transformer[]{latinTransformer});
+    }
+
+    @Test
+    public void should_convert_into_latin_plus_verify_and_assert() throws Exception {
+
+        UpdateContext updateContext = mock(UpdateContext.class);
+
+        Update update = mock(Update.class);
+        when(update.getParagraph()).thenReturn(new Paragraph(""));
+        when(update.getOperation()).thenReturn(Operation.UNSPECIFIED);
+        final RpslObject person = new RpslObjectBuilder()
+                .append(new RpslAttribute(AttributeType.PERSON, "Test Person"))
+                .append(new RpslAttribute(AttributeType.ADDRESS, "Тверская улица,москва"))
+                .append(new RpslAttribute(AttributeType.PHONE, "+30 123 411141"))
+                .append(new RpslAttribute(AttributeType.FAX_NO, "+30 123 411140"))
+                .append(new RpslAttribute(AttributeType.NIC_HDL, "TP1-TEST"))
+                .append(new RpslAttribute(AttributeType.MNT_BY, "UPD-MNT"))
+                .append(new RpslAttribute(AttributeType.SOURCE, "TEST"))
+                .get();
+        when(update.getSubmittedObject()).thenReturn(person);
+
+        RpslObject transformedPerson = subject.transform(person, update, updateContext, Action.NOOP);
+
+        // Verify_integration
+        verify(updateContext).addMessage(update, UpdateMessages.valueChangedDueToLatin1Conversion("address"));
+        verifyNoMoreInteractions(update);
+        verifyNoMoreInteractions(updateContext);
+
+        // Validate result
+        assertNotNull(transformedPerson);
+        assertThat(transformedPerson.getAttributes().size(), is(7));
+        assertThat(transformedPerson.getValueForAttribute(AttributeType.ADDRESS), is("???????? ?????,??????"));
+    }
 
     @Test
     public void should_validate_loop_only_and_verify_access() throws Exception {
