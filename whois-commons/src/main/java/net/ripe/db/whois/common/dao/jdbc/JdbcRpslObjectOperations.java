43bcef8c61c2f4cd5774878729afe35a82f1d2d7
Edward Shryane <eshryane@ripe.net>
2016-06-10 16:57:24 +0200

    create new table and delete old table, rather than truncating.

--- whois-commons/src/main/java/net/ripe/db/whois/common/dao/jdbc/JdbcRpslObjectOperations.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/dao/jdbc/JdbcRpslObjectOperations.java
@@ -353,7 +353,7 @@ public class JdbcRpslObjectOperations {
                         try (final ResultSet resultSet = tableStatement.getResultSet()) {
                             while (resultSet.next()) {
                                 final String tableName = resultSet.getString(1);
-                                truncateStatements.addBatch(String.format("CREATE TABLE %s_new LIKE %s", tableName));
+                                truncateStatements.addBatch(String.format("CREATE TABLE %s_new LIKE %s", tableName, tableName));
                                 truncateStatements.addBatch(String.format("RENAME TABLE %s TO %s_old, %s_new TO %s", tableName, tableName, tableName, tableName));
                                 truncateStatements.addBatch(String.format("DROP TABLE %s_old", tableName));
                             }
