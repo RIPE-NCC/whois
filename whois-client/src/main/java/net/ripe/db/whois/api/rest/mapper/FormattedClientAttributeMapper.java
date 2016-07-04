35a332608ca4c4f07da74b4fd2fcfcb880c1cc3c
Edward Shryane <eshryane@ripe.net>
2016-07-04 17:07:02 +0200

    If an mnt-route value contains ANY or {prefix} then treat as not referencing a maintainer. This fixes invalid links from mnt-route attribute to a mntner object in the REST API response. The mnt-route syntax must always start with a mntner name.

--- whois-client/src/main/java/net/ripe/db/whois/api/rest/mapper/FormattedClientAttributeMapper.java
+++ whois-client/src/main/java/net/ripe/db/whois/api/rest/mapper/FormattedClientAttributeMapper.java
@@ -1,12 +1,12 @@
 package net.ripe.db.whois.api.rest.mapper;
 
+import com.google.common.collect.Lists;
 import net.ripe.db.whois.api.rest.domain.Attribute;
 import net.ripe.db.whois.common.domain.CIString;
 import net.ripe.db.whois.common.rpsl.RpslAttribute;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -21,13 +21,14 @@ public class FormattedClientAttributeMapper implements AttributeMapper {
 
     @Override
     public Collection<Attribute> map(final RpslAttribute rpslAttribute, final String source) {
-        List<Attribute> result = new ArrayList(4);
+        List<Attribute> result = Lists.newArrayList();
         for (CIString value : rpslAttribute.getCleanValues()) {
             result.add(new Attribute(rpslAttribute.getKey(), value.toString(), rpslAttribute.getCleanComment(), null, null));
         }
         return result;
     }
 
+    // TODO: duplicate method
     private static String getAttributeValue(final Attribute attribute) {
         if (StringUtils.isBlank(attribute.getComment())) {
             return attribute.getValue();
