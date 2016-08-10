5fab213cc3ca817087fc431857925f927af8f817
Ed Shryane <eshryane@ripe.net>
2016-08-10 16:26:59 +0200

    Catch database errors when calling dnscheck, and return an error response. (#394)

--- whois-update/src/main/java/net/ripe/db/whois/update/dns/DnsGatewayImpl.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/dns/DnsGatewayImpl.java
@@ -2,6 +2,7 @@ package net.ripe.db.whois.update.dns;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.Uninterruptibles;
 import net.ripe.db.whois.common.Message;
 import net.ripe.db.whois.common.Messages;
 import net.ripe.db.whois.common.profiles.DeployedProfile;
@@ -15,10 +16,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Nullable;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @DeployedProfile
 @Primary
@@ -54,15 +56,25 @@ class DnsGatewayImpl implements DnsGateway {
 
     @Override
     public Map<DnsCheckRequest, DnsCheckResponse> performDnsChecks(final Set<DnsCheckRequest> dnsCheckRequests) {
-        Map<DnsCheckRequest, String> dnsRequestsPerProcessId = Maps.newHashMap();
+        final Map<DnsCheckRequest, DnsCheckResponse> dnsResults = Maps.newHashMap();
+
+        final Map<DnsCheckRequest, String> dnsRequestsPerProcessId = Maps.newHashMap();
         for (DnsCheckRequest dnsCheckRequest : dnsCheckRequests) {
-            dnsRequestsPerProcessId.put(dnsCheckRequest, queueDnsCheck(dnsCheckRequest));
+            try {
+                dnsRequestsPerProcessId.put(dnsCheckRequest, queueDnsCheck(dnsCheckRequest));
+            } catch (IllegalStateException e) {
+                dnsResults.put(dnsCheckRequest, new DnsCheckResponse(UpdateMessages.dnsCheckMessageParsingError()));
+            }
         }
 
-        Map<DnsCheckRequest, DnsCheckResponse> dnsResults = Maps.newHashMap();
         for (Map.Entry<DnsCheckRequest, String> dnsRequestPerProcessId : dnsRequestsPerProcessId.entrySet()) {
-            final DnsResult dnsResult = pollResult(dnsRequestPerProcessId.getValue());
-            dnsResults.put(dnsRequestPerProcessId.getKey(), getDnsResponseForPollResult(dnsResult));
+            final DnsCheckRequest dnsCheckRequest = dnsRequestPerProcessId.getKey();
+            try {
+                final DnsResult dnsResult = pollResult(dnsRequestPerProcessId.getValue());
+                dnsResults.put(dnsCheckRequest, getDnsResponseForPollResult(dnsResult));
+            } catch (IllegalStateException e) {
+                dnsResults.put(dnsCheckRequest, new DnsCheckResponse(UpdateMessages.dnsCheckMessageParsingError()));
+            }
         }
 
         return dnsResults;
@@ -74,7 +86,7 @@ class DnsGatewayImpl implements DnsGateway {
         } else if (dnsResult.nrErrors > 0) {
             try {
                 return new DnsCheckResponse(getMessages(dnsResult));
-            } catch (IllegalStateException | DataAccessException e) {
+            } catch (IllegalStateException e) {
                 LOGGER.error(String.format("Error in retrieving messages during DNS check for test_id (dnsResult.id): %s", dnsResult.id), e);
                 return new DnsCheckResponse(UpdateMessages.dnsCheckMessageParsingError());
             }
@@ -83,89 +95,98 @@ class DnsGatewayImpl implements DnsGateway {
     }
 
     private List<Message> getMessages(final DnsResult dnsResult) {
-        return jdbcTemplate.query("" +
-                "SELECT message,LEVEL,formatstring,description,arg0,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9 " +
-                "FROM (" +
-                "  SELECT * " +
-                "  FROM results " +
-                "  WHERE test_id=? " +
-                "  AND LEVEL IN ('ERROR', 'CRITICAL', 'WARNING') " +
-                ") AS tmp " +
-                "LEFT JOIN messages ON tmp.message = messages.tag " +
-                "ORDER BY tmp.id ASC",
-                new RowMapper<Message>() {
-                    @Override
-                    public Message mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+        try {
+            return jdbcTemplate.query(
+                    "SELECT message,LEVEL,formatstring,description,arg0,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9 " +
+                    "FROM (" +
+                    "  SELECT * " +
+                    "  FROM results " +
+                    "  WHERE test_id = ? " +
+                    "  AND LEVEL IN ('ERROR', 'CRITICAL', 'WARNING') " +
+                    ") AS tmp " +
+                    "LEFT JOIN messages ON tmp.message = messages.tag " +
+                    "ORDER BY tmp.id ASC",
+                    new RowMapper<Message>() {
+                        @Override
+                        public Message mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+                            final StringBuilder messageBuilder = new StringBuilder();
 
-                        final StringBuilder messageBuilder = new StringBuilder("");
+                            final String formatstring = rs.getString("formatstring");
+                            if (StringUtils.isNotBlank(formatstring)) {
+                                messageBuilder.append(String.format(
+                                        formatstring,
+                                        rs.getObject("arg0"),
+                                        rs.getObject("arg1"),
+                                        rs.getObject("arg2"),
+                                        rs.getObject("arg3"),
+                                        rs.getObject("arg4"),
+                                        rs.getObject("arg5"),
+                                        rs.getObject("arg6"),
+                                        rs.getObject("arg7"),
+                                        rs.getObject("arg8"),
+                                        rs.getObject("arg9")));
+                            } else {
+                                throw new IllegalStateException(String.format("Failed to parse message tag: '%s' for test_id: %s", rs.getString("message"), dnsResult.id));
+                            }
 
-                        final String formatstring = rs.getString("formatstring");
+                            final String description = rs.getString("description");
+                            if (StringUtils.isNotEmpty(description)) {
+                                messageBuilder.append("\n\n").append(description);
+                            }
 
-                        if (StringUtils.isNotBlank(formatstring)) {
-                            messageBuilder.append(String.format(
-                                    formatstring,
-                                    rs.getObject("arg0"),
-                                    rs.getObject("arg1"),
-                                    rs.getObject("arg2"),
-                                    rs.getObject("arg3"),
-                                    rs.getObject("arg4"),
-                                    rs.getObject("arg5"),
-                                    rs.getObject("arg6"),
-                                    rs.getObject("arg7"),
-                                    rs.getObject("arg8"),
-                                    rs.getObject("arg9")));
-                        } else {
-                            throw new IllegalStateException(String.format("Failed to parse message tag: '%s' for test_id: %s", rs.getString("message"), dnsResult.id));
+                            return new Message(ERRORLEVEL_CONVERSION.get(rs.getString("level")), messageBuilder.toString());
                         }
-
-                        final String description = rs.getString("description");
-                        if (StringUtils.isNotEmpty(description)) {
-                            messageBuilder.append("\n\n").append(description);
-                        }
-
-                        return new Message(ERRORLEVEL_CONVERSION.get(rs.getString("level")), messageBuilder.toString());
-                    }
-                }, dnsResult.id);
+                    },
+                    dnsResult.id);
+        } catch (DataAccessException e) {
+            throw new IllegalStateException("dnscheck get messages failed", e);
+        }
     }
 
     private String queueDnsCheck(final DnsCheckRequest dnsCheckRequest) {
         final String processId = "RDP-" + UUID.randomUUID().toString();
-
-        final int rows = jdbcTemplate.update("" +
-                "INSERT INTO queue (DOMAIN, priority, source_id, source_data, fake_parent_glue) " +
-                "VALUES (?, 1, 2, ?, ?) ",
-                dnsCheckRequest.getDomain(),
-                processId,
-                dnsCheckRequest.getGlue());
-        Validate.isTrue(rows == 1);
-        return processId;
+        try {
+            final int rows = jdbcTemplate.update(
+                    "INSERT INTO queue (DOMAIN, priority, source_id, source_data, fake_parent_glue) " +
+                    "VALUES (?, 1, 2, ?, ?) ",
+                    dnsCheckRequest.getDomain(),
+                    processId,
+                    dnsCheckRequest.getGlue());
+            Validate.isTrue(rows == 1);
+            return processId;
+        } catch (DataAccessException e) {
+            LOGGER.error("dnscheck queue failed", e);
+            throw new IllegalStateException("dnscheck queue failed", e);
+        }
     }
 
+    @Nullable
     private DnsResult pollResult(final String processId) {
         final long maxTime = System.currentTimeMillis() + timeout;
 
         while (System.currentTimeMillis() < maxTime) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException("Interrupted", e);
-            }
+            Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
 
-            final DnsResult dnsResult = jdbcTemplate.query("" +
+
+            final DnsResult dnsResult;
+            try {
+                dnsResult = jdbcTemplate.query(
                     "SELECT id, END, count_critical, count_error " +
                     "FROM tests " +
-                    "WHERE source_id=2 AND source_data=?",
-                    new ResultSetExtractor<DnsResult>() {
-                        @Override
-                        public DnsResult extractData(final ResultSet rs) throws SQLException, DataAccessException {
-                            if (rs.next() && rs.getTimestamp("end") != null) {
-                                return new DnsResult(rs.getInt("id"), rs.getInt("count_critical") + rs.getInt("count_error"));
-                            }
-
+                    "WHERE source_id = 2 " +
+                    "AND source_data = ?",
+                    rs -> {
+                        if (rs.next() && rs.getTimestamp("end") != null) {
+                            return new DnsResult(rs.getInt("id"), rs.getInt("count_critical") + rs.getInt("count_error"));
+                        } else {
                             return null;
                         }
                     },
                     processId);
+            } catch (DataAccessException e) {
+                LOGGER.error("dnscheck poll result failed", e);
+                throw new IllegalStateException("dnscheck poll result failed", e);
+            }
 
             if (dnsResult != null) {
                 return dnsResult;
