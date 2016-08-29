f7e9853270939b325503eead4c437b219987a949
Ed Shryane <eshryane@ripe.net>
2016-08-29 14:29:15 +0200

    Mnt routes link (#401)
    * If an mnt-route value contains ANY or {prefix} then treat as not referencing a maintainer.
    This fixes invalid links from mnt-route attribute to a mntner object in the REST API response.
    The mnt-route syntax must always start with a mntner name.
    
    * Parse mnt-routes value, always return mntner type if found as it's a valid reference (and not null if ANY | prefix).
    
    * Fixed mnt-routes link (should link to the mntner)
    
    * fall through to return statement
    
    * Added tests
    
    * Always use reference values when creating indexes (e.g. maintainer in mnt-routes attribute).

--- whois-api/src/main/java/net/ripe/db/whois/api/rest/mapper/FormattedServerAttributeMapper.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rest/mapper/FormattedServerAttributeMapper.java
@@ -1,22 +1,29 @@
 package net.ripe.db.whois.api.rest.mapper;
 
+import com.google.common.collect.Lists;
 import net.ripe.db.whois.api.rest.ReferencedTypeResolver;
 import net.ripe.db.whois.api.rest.domain.Attribute;
 import net.ripe.db.whois.api.rest.domain.Link;
 import net.ripe.db.whois.common.domain.CIString;
+import net.ripe.db.whois.common.rpsl.AttributeParser;
+import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.RpslAttribute;
+import net.ripe.db.whois.common.rpsl.attrs.AttributeParseException;
+import net.ripe.db.whois.common.rpsl.attrs.MntRoutes;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 @Component
 public class FormattedServerAttributeMapper implements AttributeMapper {
+
+    private static final AttributeParser.MntRoutesParser MNT_ROUTES_PARSER = new AttributeParser.MntRoutesParser();
+
     private final ReferencedTypeResolver referencedTypeResolver;
     private final String baseUrl;
 
@@ -34,11 +41,11 @@ public class FormattedServerAttributeMapper implements AttributeMapper {
 
     @Override
     public Collection<Attribute> map(final RpslAttribute rpslAttribute, final String source) {
-        final List<Attribute> result = new ArrayList(4);
+        final List<Attribute> result = Lists.newArrayList();
         for (CIString value : rpslAttribute.getCleanValues()) {
             // TODO: [AH] for each person or role reference returned, we make an sql lookup - baaad
             final String referencedType = (rpslAttribute.getType() != null) ? referencedTypeResolver.getReferencedType(rpslAttribute.getType(), value) : null;
-            final Link link = (referencedType != null) ? Link.create(baseUrl, source, referencedType, value.toString()) : null;
+            final Link link = (referencedType != null) ? Link.create(baseUrl, source, referencedType, getLinkValue(rpslAttribute.getType(), value)) : null;
             result.add(new Attribute(rpslAttribute.getKey(), value.toString(), rpslAttribute.getCleanComment(), referencedType, link));
         }
         return result;
@@ -54,6 +61,20 @@ public class FormattedServerAttributeMapper implements AttributeMapper {
             } else {
                 return String.format("%s # %s", attribute.getValue(), attribute.getComment());
             }
+        }
+    }
+
+    private static String getLinkValue(final AttributeType attributeType, final CIString value) {
+        switch (attributeType) {
+            case MNT_ROUTES:
+                try {
+                    final MntRoutes mntRoutes = MNT_ROUTES_PARSER.parse(value.toString());
+                    return mntRoutes.getMaintainer().toString();
+                } catch (AttributeParseException e) {
+                    return value.toString();
+                }
+            default:
+                return value.toString();
         }
     }
 }
