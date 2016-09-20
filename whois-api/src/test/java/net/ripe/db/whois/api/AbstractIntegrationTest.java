de14f1ff8b631e1f4a656ed0561451b720c2246a
tom <tom.kalmijn@dopay.com>
2016-09-20 14:37:56 +0200

    removed YieldToTestServer class (redundant)

--- whois-api/src/test/java/net/ripe/db/whois/api/AbstractIntegrationTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/AbstractIntegrationTest.java
@@ -1,7 +1,6 @@
 package net.ripe.db.whois.api;
 
 import net.ripe.db.whois.api.httpserver.JettyBootstrap;
-import net.ripe.db.whois.api.rest.YieldToTestServer;
 import net.ripe.db.whois.common.ApplicationService;
 import net.ripe.db.whois.common.support.AbstractDaoTest;
 import org.junit.After;
@@ -9,6 +8,8 @@ import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 @ContextConfiguration(locations = {"classpath:applicationContext-api-test.xml"})
@@ -35,7 +36,22 @@ public abstract class AbstractIntegrationTest extends AbstractDaoTest {
         return jettyBootstrap.getPort();
     }
 
-    protected synchronized void stopTestHereButKeepLocalServerRunning() {
-        YieldToTestServer.yield(this);
+    /**
+     * This method can be called anywhere in a derived test class to
+     * be able to debug the server on a local development machine.
+     */
+    protected synchronized void stopExecutionHereButKeepTheServerRunning() {
+        Instant start = Instant.now();
+
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Instant end = Instant.now();
+                Duration timeElapsed = Duration.between(start, end);
+                System.out.println(String.format("Server listening for %d minutes on port %d", timeElapsed.toMinutes(), getPort()));
+                wait(60000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
