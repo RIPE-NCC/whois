3adc6a8b145821d77a892f16533c1bb24574afd5
Ivana Svonja <isvonja@ripe.net>
2017-01-16 23:27:17 +0100

    added getter and setter to Error class

--- whois-update/src/main/java/net/ripe/db/whois/update/dns/zonemaster/domain/StartDomainTestResponse.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/dns/zonemaster/domain/StartDomainTestResponse.java
@@ -39,6 +39,22 @@ public class StartDomainTestResponse extends ZonemasterResponse {
         private String message;
         private int code;
 
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
         @Override
         public String toString() {
             return MoreObjects.toStringHelper(this)
