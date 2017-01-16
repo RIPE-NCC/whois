5f01e1ea157e6f3d9b6dcfa254253d99665ecc4f
Edward Shryane <eshryane@ripe.net>
2017-01-16 14:28:56 +0100

    Added mapping for error element.

--- whois-update/src/main/java/net/ripe/db/whois/update/dns/zonemaster/StartDomainTestResponse.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/dns/zonemaster/StartDomainTestResponse.java
@@ -1,11 +1,14 @@
 package net.ripe.db.whois.update.dns.zonemaster;
 
 
+import javax.annotation.Nullable;
+
 public class StartDomainTestResponse {
 
     private String jsonrpc;
     private String id;
     private String result;
+    private Error error;
 
     public String getJsonrpc() {
         return jsonrpc;
@@ -29,5 +32,15 @@ public class StartDomainTestResponse {
 
     public void setResult(final String result) {
         this.result = result;
+    }
+
+    @Nullable
+    public Error getError() {
+        return error;
+    }
+
+    static class Error {
+        private String message;
+        private int code;
     }
 }
