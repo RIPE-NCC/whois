dac3667cc7a2bb28f76a91b76da9527a998ebbcb
Edward Shryane <eshryane@ripe.net>
2017-01-17 12:04:11 +0100

    Added test class

---
+++ whois-update/src/test/java/net/ripe/db/whois/update/dns/zonemaster/ZonemasterDnsGatewayTest.java
@@ -0,0 +1,28 @@
+package net.ripe.db.whois.update.dns.zonemaster;
+
+import net.ripe.db.whois.update.domain.Update;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ZonemasterDnsGatewayTest {
+
+
+    @Mock
+    private ZonemasterRestClient restClient;
+    @Mock
+    private Update update;
+    @InjectMocks
+    private ZonemasterDnsGateway subject;
+
+
+    @Test
+    public void single_record() {
+        // when(restClient.sendRequest()) ...
+
+        // subject.performDnsChecks(Collections.singleton(new DnsCheckRequest(update, "ns.ripe.net", null)));
+    }
+}
