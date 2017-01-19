6000b9feaa2d11ff6ad77113a47e1e6840c18534
Edward Shryane <eshryane@ripe.net>
2017-01-19 14:29:15 +0100

    Refactored ZonemasterDnsGateway to use parallelStream() rather than fork/join (simplified code).

--- whois-update/src/main/java/net/ripe/db/whois/update/dns/zonemaster/ZonemasterDnsGateway.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/dns/zonemaster/ZonemasterDnsGateway.java
@@ -2,9 +2,6 @@ package net.ripe.db.whois.update.dns.zonemaster;
 
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.math.IntMath;
 import com.google.common.util.concurrent.Uninterruptibles;
 import net.ripe.db.whois.common.Message;
 import net.ripe.db.whois.common.profiles.DeployedProfile;
@@ -21,13 +18,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.math.RoundingMode;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static net.ripe.db.whois.common.Messages.Type.ERROR;
@@ -44,7 +39,6 @@ public class ZonemasterDnsGateway implements DnsGateway {
     private static final int TEST_PROGRESS_MAXIMUM_RETRIES = 60 * 5;
 
     private static final String PERCENTAGE_COMPLETE = "100";
-    private static final int FORK_THRESHOLD = 4;
 
     private static final ImmutableList<String> ERROR_LEVELS = ImmutableList.of("CRITICAL", "ERROR");
 
@@ -56,47 +50,21 @@ public class ZonemasterDnsGateway implements DnsGateway {
 
     @Override
     public Map<DnsCheckRequest, DnsCheckResponse> performDnsChecks(final Set<DnsCheckRequest> dnsCheckRequests) {
-        final Map<DnsCheckRequest, DnsCheckResponse> dnsResults = Maps.newConcurrentMap();
-        new ForkJoinPool().invoke(new DomainCheckAction(Lists.newArrayList(dnsCheckRequests), dnsResults));
-        return dnsResults;
+        return dnsCheckRequests
+            .parallelStream()
+            .collect(Collectors.toMap(
+                dnsCheckRequest -> dnsCheckRequest,
+                new ZonemasterFunction()));
     }
 
-    private class DomainCheckAction extends RecursiveAction {
-        private final List<DnsCheckRequest> dnsCheckRequests;
-        private final Map<DnsCheckRequest, DnsCheckResponse> responseMap;
-
-        public DomainCheckAction(
-                final List<DnsCheckRequest> dnsCheckRequests,
-                final Map<DnsCheckRequest, DnsCheckResponse> responseMap) {
-            this.dnsCheckRequests = dnsCheckRequests;
-            this.responseMap = responseMap;
-        }
+    private class ZonemasterFunction implements Function<DnsCheckRequest, DnsCheckResponse> {
 
         @Override
-        protected void compute() {
-            if (dnsCheckRequests.size() < FORK_THRESHOLD) {
-                computeDirectly();
-            } else {
-                final List<List<DnsCheckRequest>> split = split(dnsCheckRequests);
-                invokeAll(
-                        new DomainCheckAction(split.get(0), responseMap),
-                        new DomainCheckAction(split.get(1), responseMap));
-            }
-        }
-
-        private List<List<DnsCheckRequest>> split(final List<DnsCheckRequest> dnsCheckRequests) {
-            final int size = IntMath.divide(dnsCheckRequests.size(), 2, RoundingMode.UP);
-            return Lists.partition(dnsCheckRequests, size);
-        }
-
-        private void computeDirectly() {
-            for (final DnsCheckRequest dnsCheckRequest : dnsCheckRequests) {
-                final String id = makeRequest(dnsCheckRequest);
-                testProgressUntilComplete(id);
-                final GetTestResultsResponse testResults = getResults(id);
-                final DnsCheckResponse dnsCheckResponse = new DnsCheckResponse(getErrorsFromResults(testResults));
-                responseMap.put(dnsCheckRequest, dnsCheckResponse);
-            }
+        public DnsCheckResponse apply(final DnsCheckRequest dnsCheckRequest) {
+            final String id = makeRequest(dnsCheckRequest);
+            testProgressUntilComplete(id);
+            final GetTestResultsResponse testResults = getResults(id);
+            return new DnsCheckResponse(getErrorsFromResults(testResults));
         }
 
         /**
@@ -170,5 +138,4 @@ public class ZonemasterDnsGateway implements DnsGateway {
                 .collect(Collectors.toList());
         }
     }
-
 }
