0d1d67133bf9be8954f3a6ce3fe70866d84f6b03
Edward Shryane <eshryane@ripe.net>
2017-01-23 23:19:04 +0100

    Fixed tests

--- whois-client/src/test/java/net/ripe/db/whois/common/sso/CrowdClientTest.java
+++ whois-client/src/test/java/net/ripe/db/whois/common/sso/CrowdClientTest.java
@@ -15,6 +15,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.NoSuchElementException;
 
@@ -45,6 +46,7 @@ public class CrowdClientTest {
         when(client.target(anyString())).thenReturn(webTarget);
         when(webTarget.path(anyString())).thenReturn(webTarget);
         when(webTarget.queryParam(anyString(), anyVararg())).thenReturn(webTarget);
+        when(webTarget.request(any(MediaType.class))).thenReturn(builder);
         when(webTarget.request()).thenReturn(builder);
 
         subject = new CrowdClient("http://testurl", "crowduser", "crowdpassword");
