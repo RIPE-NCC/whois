d702d8f37f82d06470d60dcd9e8c7d64ff198e01
Ivana Svonja <isvonja@ripe.net>
2017-02-13 21:32:30 +0100

    SONAR - try-catch moved out of if loop

--- whois-api/src/main/java/net/ripe/db/whois/api/autocomplete/AutocompleteService.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/autocomplete/AutocompleteService.java
@@ -70,41 +70,31 @@ public class AutocompleteService {
             @QueryParam("from") final Set<String> from,
             @QueryParam("where") final Set<String> where,
             @QueryParam("like") final String like) {
+        try {
+            if (!Strings.isNullOrEmpty(query) && !Strings.isNullOrEmpty(field)) {
 
-        if (!Strings.isNullOrEmpty(query) && !Strings.isNullOrEmpty(field)) {
+                // query by field name
 
-            // query by field name
+                if (query.length() < MINIMUM_PREFIX_LENGTH) {
+                    return badRequest("query parameter is required, and must be at least " + MINIMUM_PREFIX_LENGTH + " characters long");
+                }
 
-            if (query.length() < MINIMUM_PREFIX_LENGTH) {
-                return badRequest("query parameter is required, and must be at least " + MINIMUM_PREFIX_LENGTH + " characters long");
-            }
-
-            if (AttributeType.getByNameOrNull(field) == null) {
-                return badRequest("invalid name for field");
-            }
-
-            try {
+                if (AttributeType.getByNameOrNull(field) == null) {
+                    return badRequest("invalid name for field");
+                }
                 return okRequest(autocompleteSearch.search(query, getLookupAttributes(field), getAttributeTypes(attributes), Collections.emptySet()));
-            } catch (IOException e) {
-                return badRequest("Query failed.");
-            } catch (IllegalArgumentException e) {
-                return badRequest(e.getMessage());
-            }
+            } else if (!select.isEmpty() && !where.isEmpty() && !Strings.isNullOrEmpty(like)) {
 
-        } else if (!select.isEmpty() && !where.isEmpty() && !Strings.isNullOrEmpty(like)) {
+                // query by attribute(s)
 
-            // query by attribute(s)
-
-            try {
                 return okRequest(autocompleteSearch.search(like, getAttributeTypes(where), getAttributeTypes(select), getObjectTypes(from)));
-            } catch (IOException e) {
-                return badRequest("Query failed.");
-            } catch (IllegalArgumentException e) {
-                return badRequest(e.getMessage());
+            } else {
+                return badRequest("invalid arguments");
             }
-
-        } else {
-            return badRequest("invalid arguments");
+        } catch (IOException e) {
+            return badRequest("Query failed.");
+        } catch (IllegalArgumentException e) {
+            return badRequest(e.getMessage());
         }
     }
 
