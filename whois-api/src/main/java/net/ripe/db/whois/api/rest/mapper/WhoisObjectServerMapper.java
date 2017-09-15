ac0d4a67c41753b220cf779bb01a80e49bfa2818
Paul Etchells <petchells@ripe.net>
2017-09-15 16:05:00 +0200

    sends comaintained flag back with objects

--- whois-api/src/main/java/net/ripe/db/whois/api/rest/mapper/WhoisObjectServerMapper.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rest/mapper/WhoisObjectServerMapper.java
@@ -85,9 +85,14 @@ public class WhoisObjectServerMapper {
         }
     }
 
-    public void mapManagedAttributes(final WhoisObject whoisObject, final Parameters parameters, final RpslObject rpslObject) {
-        if ((Boolean.TRUE == parameters.getManagedAttributes()) &&
-                    (managedAttributeSearch.isCoMaintained(rpslObject))) {
+    public void mapManagedAttributes(final WhoisObject whoisObject,
+                                     final Parameters parameters,
+                                     final RpslObject rpslObject) {
+
+        if (Boolean.TRUE == parameters.getManagedAttributes()) {
+            whoisObject.setComaintained(managedAttributeSearch.isCoMaintained(rpslObject));
+        }
+        if (whoisObject.isComaintained()) {
             final Iterator<Attribute> attributeIterator = whoisObject.getAttributes().iterator();
             final Iterator<RpslAttribute> rpslAttributeIterator = rpslObject.getAttributes().iterator();
             while (attributeIterator.hasNext() && rpslAttributeIterator.hasNext()) {
