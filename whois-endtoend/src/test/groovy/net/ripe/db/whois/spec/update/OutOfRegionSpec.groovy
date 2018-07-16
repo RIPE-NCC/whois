ebc01712f70ae2becfc85246710c5f599abd0b4b
Sander Buskens <sbuskens@ripe.net>
2018-07-16 10:31:01 +0200

    Do not allow objects other than autnum, route, route6 to be created with nonauth source.

--- whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/update/OutOfRegionSpec.groovy
+++ whois-endtoend/src/test/groovy/net/ripe/db/whois/spec/update/OutOfRegionSpec.groovy
@@ -85,13 +85,15 @@ class OutOfRegionSpec extends BaseQueryUpdateSpec {
         ack.summary.assertSuccess(0, 0, 0, 0, 0)
         ack.summary.assertErrors(1, 1, 0, 0)
 
-        ack.countErrorWarnInfo(2, 0, 0)
+        ack.countErrorWarnInfo(2, 1, 0)
 
       ack.errors.any { it.operation == "Create" && it.key == "[aut-num] AS252" }
       ack.errorMessagesFor("Create", "[aut-num] AS252") == [
               "Authorisation for [as-block] AS222 - AS333 failed using \"mnt-lower:\" not authenticated by: RIPE-NCC-HM-MNT",
               "Cannot create out of region objects"
       ]
+      ack.warningMessagesFor("Create", "[aut-num] AS252") ==
+              ["Out of region object has wrong source"]
 
       queryObjectNotFound("-rBG -T aut-num AS252", "aut-num", "AS252")
     }
@@ -208,6 +210,40 @@ class OutOfRegionSpec extends BaseQueryUpdateSpec {
                 ["Out of region object has wrong source"]
 
         queryObject("-rGBT aut-num AS252", "aut-num", "AS252")
+    }
+
+    def "not create inetnum with nonauth source"() {
+        whoisFixture.dumpSchema();
+        when:
+        def ack = syncUpdateWithResponse("""
+                inetnum:     10.1.0.0 - 10.1.255.255
+                netname:     invalid-net
+                country:     NL
+                admin-c:     TP1-TEST
+                tech-c:      TP1-TEST
+                status:      ASSIGNED PA
+                mnt-by:      OWNER-MNT
+                source:      TEST-NONAUTH
+
+                password:   owner
+                password:   owner2
+                
+                """.stripIndent()
+        )
+
+        then:
+        ack.summary.nrFound == 1
+        ack.summary.assertSuccess(0, 0, 0, 0, 0)
+        ack.summary.assertErrors(1, 1, 0, 0)
+
+        ack.countErrorWarnInfo(1, 0, 0)
+
+        ack.errors.any { it.operation == "Create" && it.key == "[inetnum] 10.1.0.0 - 10.1.255.255" }
+        ack.errorMessagesFor("Create", "[inetnum] 10.1.0.0 - 10.1.255.255") == [
+                "Source TEST-NONAUTH is not allowed for inetnum objects"
+        ]
+
+        queryObjectNotFound("-r -T inetnum 10.1.0.0 - 10.1.255.255", "inetnum", "10.1.0.0 - 10.1.255.255")
     }
 
 }
