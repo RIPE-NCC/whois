0b6df77df71d360bdec76c3b0496720b8f8a573f
sbusk <sbuskens@ripe.net>
2018-07-24 14:31:18 +0200

    Out of region validations (#487)
    * Added -NONAUTH source and validations for out of region objects.
    
    * Do not allow objects other than autnum, route, route6 to be created with nonauth source.
    
    * Added tests for out of region autnum creation.
    Implemented (incorrect) source attribute warning/replace for in/out of region objects.
    
    * Added tests for out of region autnum modification.
    
    * Added support and tests for out of region routes.
    
    * Use correct attribute to get route prefix.
    
    * Added tests for modifying out of region routes.
    
    * Added tests for creating out of region route6 objects.
    
    * Added tests for modifying out of region route6 objects.
    
    * Test cleanup.
    
    * Fixed tests.
    
    * Authoritative resource test data can now be specified per testcase.
    
    * Fixed integration tests, removed tests related to pending authentication.
    
    * Fixed integration tests.
    
    * Process AttributeGenerators in a predictable order.
    
    * Fixed tests.
    
    * Moved out of region object detection for route(6) into AuthoritativeResource.

--- whois-api/src/test/java/net/ripe/db/whois/api/rest/WhoisRestServiceTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/rest/WhoisRestServiceTestIntegration.java
@@ -539,7 +539,7 @@ public class WhoisRestServiceTestIntegration extends AbstractIntegrationTest {
         assertThat(whoisObject.getAttributes(), contains(
                 new Attribute("route", "193.254.30.0/24"),
                 new Attribute("descr", "Test route"),
-                new Attribute("origin", "AS12726", null, "aut-num", Link.create("http://rest-test.db.ripe.net/test/aut-num/AS12726"), null),
+                new Attribute("origin", "AS12726"),
                 new Attribute("mnt-by", "OWNER-MNT", null, "mntner", Link.create("http://rest-test.db.ripe.net/test/mntner/OWNER-MNT"), null),
                 new Attribute("mnt-routes", "OWNER-MNT {192.168.0.0/16}", null, "mntner", Link.create("http://rest-test.db.ripe.net/test/mntner/OWNER-MNT"), null),
                 new Attribute("source", "TEST")
@@ -571,7 +571,7 @@ public class WhoisRestServiceTestIntegration extends AbstractIntegrationTest {
         assertThat(whoisObject.getAttributes(), contains(
                 new Attribute("route6", "2001::/32"),
                 new Attribute("descr", "Test route"),
-                new Attribute("origin", "AS12726", null, "aut-num", Link.create("http://rest-test.db.ripe.net/test/aut-num/AS12726"), null),
+                new Attribute("origin", "AS12726"),
                 new Attribute("mnt-by", "OWNER-MNT", null, "mntner", Link.create("http://rest-test.db.ripe.net/test/mntner/OWNER-MNT"), null),
                 new Attribute("source", "TEST")
         ));
@@ -2042,9 +2042,8 @@ public class WhoisRestServiceTestIntegration extends AbstractIntegrationTest {
             fail();
         } catch (BadRequestException e) {
             final WhoisResources whoisResources = RestTest.mapClientException(e);
-            RestTest.assertErrorCount(whoisResources, 2);
-            RestTest.assertErrorMessage(whoisResources, 0, "Error", "Unrecognized source: %s", "RIPE");
-            RestTest.assertErrorMessage(whoisResources, 1, "Error", "\"%s\" is not valid for this object type", "admin-c");
+            RestTest.assertErrorCount(whoisResources, 1);
+            RestTest.assertErrorMessage(whoisResources, 0, "Error", "\"%s\" is not valid for this object type", "admin-c");
         }
     }
 
