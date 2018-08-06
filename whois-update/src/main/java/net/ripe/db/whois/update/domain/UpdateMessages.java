9fdf139d501facce08345d97c8ec478a69004a94
Sander Buskens <sbuskens@ripe.net>
2018-08-06 16:07:17 +0200

    Overriding source attribute value is no longer possible, value is now always determined by whois.

--- whois-update/src/main/java/net/ripe/db/whois/update/domain/UpdateMessages.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/domain/UpdateMessages.java
@@ -497,10 +497,6 @@ public final class UpdateMessages {
         return new Message(Type.ERROR, "Source %s is not allowed for %s objects", source, objectType.getName());
     }
 
-    public static Message wrongOutOfRegionSource(final CharSequence expectedSource) {
-        return new Message(Type.WARNING, "Object has wrong source, should be %s", expectedSource);
-    }
-
     public static Message cannotUseReservedAsNumber(final Long asNumber) {
         return new Message(Type.ERROR, "Cannot use reserved AS number %d", asNumber);
     }
