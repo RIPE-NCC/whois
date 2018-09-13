60e3a63eb92ba0d68de958c4d32ce5df62012903
Sander Buskens <sbuskens@ripe.net>
2018-09-13 14:58:23 +0200

    Timestamps should be.. timestamps.

--- whois-commons/src/main/resources/internals_schema.sql
+++ whois-commons/src/main/resources/internals_schema.sql
@@ -85,7 +85,7 @@ DROP TABLE IF EXISTS `abuse_email`;
 CREATE TABLE `abuse_email` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `address` varchar(256) NOT NULL,
-  `checked_at` int(10),
+  `checked_at` timestamp,
   `comment` varchar(256),
   `status` varchar(256),
   PRIMARY KEY (`id`)
@@ -93,9 +93,9 @@ CREATE TABLE `abuse_email` (
 
 DROP TABLE IF EXISTS `abuse_org_email`;
 CREATE TABLE `abuse_org_email` (
-  `org_id` int(10) UNSIGNED NOT NULL,
+  `org_id` varchar(256) NOT NULL,
   `email_id` int(10) UNSIGNED NOT NULL,
-  `deleted_at` int(10),
+  `deleted_at` timestamp,
   PRIMARY KEY (`org_id`, `email_id`),
   CONSTRAINT FOREIGN KEY (`email_id`) REFERENCES `abuse_email` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
