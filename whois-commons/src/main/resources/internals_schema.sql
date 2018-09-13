2020335e6388fe6e53d61aa68b17c0462e88a916
Sander Buskens <sbuskens@ripe.net>
2018-09-13 16:08:14 +0200

    Don't want current timestamp as default.

--- whois-commons/src/main/resources/internals_schema.sql
+++ whois-commons/src/main/resources/internals_schema.sql
@@ -85,7 +85,7 @@ DROP TABLE IF EXISTS `abuse_email`;
 CREATE TABLE `abuse_email` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `address` varchar(256) NOT NULL,
-  `checked_at` timestamp,
+  `checked_at` timestamp null default null,
   `comment` varchar(256),
   `status` varchar(256),
   PRIMARY KEY (`id`)
@@ -95,7 +95,7 @@ DROP TABLE IF EXISTS `abuse_org_email`;
 CREATE TABLE `abuse_org_email` (
   `org_id` varchar(256) NOT NULL,
   `email_id` int(10) UNSIGNED NOT NULL,
-  `deleted_at` timestamp,
+  `deleted_at` timestamp null default null,
   PRIMARY KEY (`org_id`, `email_id`),
   CONSTRAINT FOREIGN KEY (`email_id`) REFERENCES `abuse_email` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
