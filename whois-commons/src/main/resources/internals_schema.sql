bf261a39057e00e76bfeaa39e03f436fc08f2d34
Sander Buskens <sbuskens@ripe.net>
2018-09-18 15:03:12 +0200

    Added created_at to abuse_email and abuse_org_email tables.

--- whois-commons/src/main/resources/internals_schema.sql
+++ whois-commons/src/main/resources/internals_schema.sql
@@ -87,6 +87,7 @@ CREATE TABLE `abuse_email` (
   `address` varchar(256) NOT NULL,
   `checked_at` timestamp null default null,
   `comment` varchar(256),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` varchar(256),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -96,8 +97,9 @@ CREATE UNIQUE INDEX abuse_email_address_i on `abuse_email`(`address`);
 DROP TABLE IF EXISTS `abuse_org_email`;
 CREATE TABLE `abuse_org_email` (
   `org_id` varchar(256) NOT NULL,
-  `email_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp null default null,
+  `email_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`org_id`, `email_id`),
   CONSTRAINT FOREIGN KEY (`email_id`) REFERENCES `abuse_email` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- whois-commons/src/main/resources/patch/internals-1.92.sql
+++ whois-commons/src/main/resources/patch/internals-1.92.sql
@@ -1,20 +1,20 @@
-DROP TABLE IF EXISTS `abuse_email`;
 CREATE TABLE `abuse_email` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `address` varchar(256) NOT NULL,
   `checked_at` timestamp null default null,
   `comment` varchar(256),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` varchar(256),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE UNIQUE INDEX abuse_email_address_i on `abuse_email`(`address`);
 
-DROP TABLE IF EXISTS `abuse_org_email`;
 CREATE TABLE `abuse_org_email` (
   `org_id` varchar(256) NOT NULL,
-  `email_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp null default null,
+  `email_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`org_id`, `email_id`),
   CONSTRAINT FOREIGN KEY (`email_id`) REFERENCES `abuse_email` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
