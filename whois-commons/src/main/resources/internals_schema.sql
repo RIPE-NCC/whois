fe729740488f0e6ced27fcf516b3418bf5663fc2
Ed Shryane <eshryane@ripe.net>
2018-09-28 15:36:51 +0200

    Abuse validation (#512)
    * Database schema changes for abuse validation.
    
    * Timestamps should be.. timestamps.
    
    * Don't want current timestamp as default.
    
    * Added unique index on abuse_email address.
    Added patch file for schema changes.
    
    * Added created_at to abuse_email and abuse_org_email tables.
    
    * Abuse email status is no longer nullable.
    
    * Add ticket related tables for abuse contact validation.
    
    * Fix SQL script.
    
    * Merged abuse_ticket and abuse_org_ticket together into one abuse_ticket table.
    
    * Added created_at and last_modified to abuse_ticket.
    
    * Added indexes on abuse_ticket.
    
    * Reverted pom version

--- whois-commons/src/main/resources/internals_schema.sql
+++ whois-commons/src/main/resources/internals_schema.sql
@@ -69,3 +69,41 @@ CREATE TABLE `default_maintainer_history` (
   `in_progress` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `abuse_email`;
+CREATE TABLE `abuse_email` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `address` varchar(256) NOT NULL,
+  `checked_at` timestamp null default null,
+  `comment` varchar(256),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(7) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE UNIQUE INDEX abuse_email_address_i on `abuse_email`(`address`);
+
+DROP TABLE IF EXISTS `abuse_org_email`;
+CREATE TABLE `abuse_org_email` (
+  `org_id` varchar(256) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp null default null,
+  `email_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`org_id`, `email_id`),
+  CONSTRAINT FOREIGN KEY (`email_id`) REFERENCES `abuse_email` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `abuse_ticket`;
+CREATE TABLE `abuse_ticket` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_checked` timestamp null default null,
+  `last_modified` timestamp null default null,
+  `org_id` varchar(256) NOT NULL,
+  `status` varchar(256),
+  `ticket_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE INDEX abuse_ticket_org_id_i ON abuse_ticket(org_id);
+CREATE UNIQUE INDEX abuse_ticket_ticket_id_i on abuse_ticket(ticket_id);
