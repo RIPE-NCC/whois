063bb0069b0880ca84d55b3a34c05dd7bff1eaa0
Edward Shryane <eshryane@ripe.net>
2018-10-05 14:31:27 +0200

    Fixed compilation errors

--- whois-scheduler/src/test/java/net/ripe/db/whois/scheduler/task/acl/AutomaticPermanentBlocksCleanupTest.java
+++ whois-scheduler/src/test/java/net/ripe/db/whois/scheduler/task/acl/AutomaticPermanentBlocksCleanupTest.java
@@ -2,8 +2,6 @@ package net.ripe.db.whois.scheduler.task.acl;
 
 import net.ripe.db.whois.common.DateTimeProvider;
 import net.ripe.db.whois.query.dao.AccessControlListDao;
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
 import org.joda.time.LocalDate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,29 +28,10 @@ public class AutomaticPermanentBlocksCleanupTest {
 
         subject.run();
 
-        verify(accessControlListDao, times(1)).removePermanentBlocksBefore(argThat(new BaseMatcher<LocalDate>() {
-            @Override
-            public boolean matches(Object item) {
-                return item.equals(now.minusYears(1));
-            }
+        // Should only delete bans older than 1 year
+        verify(accessControlListDao, times(1)).removePermanentBlocksBefore(argThat(item -> item.equals(now.minusYears(1))));
 
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Should only delete bans older than 1 year");
-            }
-        }));
-
-        verify(accessControlListDao, times(1)).removeBlockEventsBefore(argThat(new BaseMatcher<LocalDate>() {
-            @Override
-            public boolean matches(Object item) {
-                return item.equals(now.minusMonths(3));
-            }
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Should only delete events older than 3 months");
-            }
-        }));
-
+        // Should only delete events older than 3 months
+        verify(accessControlListDao, times(1)).removeBlockEventsBefore(argThat(item -> item.equals(now.minusMonths(3))));
     }
 }
