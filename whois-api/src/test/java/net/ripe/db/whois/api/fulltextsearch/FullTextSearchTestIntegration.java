b43e2f2b5c09a6706ba5ef5695c637ecd898e5f3
sbusk <sbuskens@ripe.net>
2019-05-21 15:03:12 +0200

    Hitting personal object accounting limits from Fulltext search should… (#540)
    * Hitting personal object accounting limits from Fulltext search should result in a too many requests (429) error response.
    
    * Fixed tests.

--- whois-api/src/test/java/net/ripe/db/whois/api/fulltextsearch/FullTextSearchTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/fulltextsearch/FullTextSearchTestIntegration.java
@@ -4,18 +4,28 @@ import com.google.common.collect.Lists;
 import net.ripe.db.whois.api.AbstractIntegrationTest;
 import net.ripe.db.whois.api.RestTest;
 import net.ripe.db.whois.common.IntegrationTest;
+import net.ripe.db.whois.common.ip.IpInterval;
 import net.ripe.db.whois.common.rpsl.RpslObject;
+import net.ripe.db.whois.query.acl.IpResourceConfiguration;
+import net.ripe.db.whois.query.dao.jdbc.JdbcAccessControlListDao;
+import net.ripe.db.whois.query.support.TestPersonalObjectAccounting;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
+import org.joda.time.LocalDate;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.ClientErrorException;
+import java.net.Inet4Address;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -33,7 +43,13 @@ import static org.junit.Assert.fail;
 
 @Category(IntegrationTest.class)
 public class FullTextSearchTestIntegration extends AbstractIntegrationTest {
+
     @Autowired FullTextIndex fullTextIndex;
+    @Autowired TestPersonalObjectAccounting testPersonalObjectAccounting;
+    @Autowired JdbcAccessControlListDao jdbcAccessControlListDao;
+    @Autowired IpResourceConfiguration ipResourceConfiguration;
+
+    private JdbcTemplate aclJdbcTemplate;
 
     @BeforeClass
     public static void setProperty() {
@@ -49,6 +65,12 @@ public class FullTextSearchTestIntegration extends AbstractIntegrationTest {
     @Before
     public void setUp() {
         fullTextIndex.rebuild();
+        testPersonalObjectAccounting.resetAccounting();
+    }
+
+    @Autowired
+    public void setAclDataSource(@Qualifier("aclDataSource") DataSource dataSource) {
+        this.aclJdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Test
@@ -676,6 +698,47 @@ public class FullTextSearchTestIntegration extends AbstractIntegrationTest {
 
         assertThat(queryResponse.getStatus(), is(0));
         assertThat(queryResponse.getResults().getNumFound(), is(1L));
+    }
+
+    @Test
+    public void temporary_block() {
+        testPersonalObjectAccounting.accountPersonalObject(Inet4Address.getLoopbackAddress(), 5001);
+
+        databaseHelper.addObject(RpslObject.parse(
+                "person: John McDonald\n" +
+                        "nic-hdl: AA1-RIPE\n" +
+                        "source: RIPE"));
+        fullTextIndex.rebuild();
+
+        try {
+            query("q=john%20mcdonald");
+            fail("request should have been blocked");
+        } catch (ClientErrorException cee) {
+            assertThat(cee.getResponse().getStatus(), is(429));
+        }
+    }
+
+    @Test
+    public void permanent_block() {
+        final IpInterval localhost = IpInterval.parse(Inet4Address.getLoopbackAddress().getHostAddress());
+        jdbcAccessControlListDao.savePermanentBlock(localhost, LocalDate.now(), 1, "test");
+        ipResourceConfiguration.reload();
+
+        databaseHelper.addObject(RpslObject.parse(
+                "person: John McDonald\n" +
+                        "nic-hdl: AA1-RIPE\n" +
+                        "source: RIPE"));
+        fullTextIndex.rebuild();
+
+        try {
+            query("q=john%20mcdonald");
+            fail("request should have been blocked");
+        } catch (ClientErrorException cee) {
+            assertThat(cee.getResponse().getStatus(), is(429));
+        } finally {
+            assertThat(aclJdbcTemplate.update("DELETE FROM acl_denied WHERE prefix = ?", localhost.toString()), is(1));
+            ipResourceConfiguration.reload();
+        }
     }
 
     @Test
