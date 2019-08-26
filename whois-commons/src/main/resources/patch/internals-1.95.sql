b40a3146c7574d27417fd8e5ed76c765f702380f
Ed Shryane <eshryane@ripe.net>
2019-08-26 12:21:05 +0200

    Updated whois 1.95 SQL patch (schema change for new internal scheduler table).

--- whois-commons/src/main/resources/patch/internals-1.95.sql
+++ whois-commons/src/main/resources/patch/internals-1.95.sql
@@ -9,5 +9,14 @@ CREATE TABLE `default_maintainer_sync_history` (
     `is_synchronised` tinyint(1) DEFAULT 0,PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `shedlock`;
+CREATE TABLE `shedlock` (
+    `name` VARCHAR(64),
+    `lock_until` TIMESTAMP(3) NULL,
+    `locked_at` TIMESTAMP(3) NULL,
+    `locked_by`  VARCHAR(255),
+    PRIMARY KEY (`name`)
+)  ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 TRUNCATE version;
 INSERT INTO version VALUES ('internals-1.95');
