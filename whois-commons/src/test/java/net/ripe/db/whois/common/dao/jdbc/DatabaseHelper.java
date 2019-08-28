e12ebc51d830ea15ef3f6e6813b45bb35e9560ac
Ed Shryane <eshryane@ripe.net>
2019-08-28 16:25:55 +0200

    Test database name has millisecond precision

--- whois-commons/src/test/java/net/ripe/db/whois/common/dao/jdbc/DatabaseHelper.java
+++ whois-commons/src/test/java/net/ripe/db/whois/common/dao/jdbc/DatabaseHelper.java
@@ -5,12 +5,12 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import net.ripe.db.whois.common.DateTimeProvider;
 import net.ripe.db.whois.common.dao.RpslObjectDao;
 import net.ripe.db.whois.common.dao.RpslObjectInfo;
 import net.ripe.db.whois.common.dao.RpslObjectUpdateDao;
 import net.ripe.db.whois.common.dao.RpslObjectUpdateInfo;
 import net.ripe.db.whois.common.domain.BlockEvent;
+import net.ripe.db.whois.common.domain.Timestamp;
 import net.ripe.db.whois.common.domain.User;
 import net.ripe.db.whois.common.grs.AuthoritativeResourceData;
 import net.ripe.db.whois.common.jdbc.driver.LoggingDriver;
@@ -184,8 +184,7 @@ public class DatabaseHelper implements EmbeddedValueResolverAware {
             final Matcher dbMatcher = dbPattern.matcher(db);
             if (dbMatcher.matches()) {
                 final String creationTimeString = dbMatcher.group(1);
-
-                final LocalDateTime creationTime = DateTimeProvider.fromEpochMilli(Long.parseLong(creationTimeString));
+                final LocalDateTime creationTime = new Timestamp(Long.parseLong(creationTimeString) / 1000L).toLocalDateTime();
                 if (creationTime.isBefore(LocalDateTime.now().minusHours(1))) {
                     jdbcTemplate.execute("DROP DATABASE IF EXISTS " + db);
                 }
