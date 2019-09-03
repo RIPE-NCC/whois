a7c0646c0d87496a46ac99dd33491062a231c9be
Ed Shryane <eshryane@ripe.net>
2019-09-03 11:26:14 +0200

    Added comments

--- whois-commons/src/main/java/net/ripe/db/whois/common/DateTimeProvider.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/DateTimeProvider.java
@@ -6,8 +6,14 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 public interface DateTimeProvider {
+
+    /** Return the current date */
     LocalDate getCurrentDate();
+
+    /** Return the current date time (in current system timezone) */
     LocalDateTime getCurrentDateTime();
+
+    /** Return the current date time in UTC */
     ZonedDateTime getCurrentDateTimeUtc();
 
     /** returns elapsed time (with nanosecond precision). N.B. not related to system time */
