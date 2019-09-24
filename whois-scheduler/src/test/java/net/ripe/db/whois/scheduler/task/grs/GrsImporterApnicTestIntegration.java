0e500a88b63596b79c0e25304fd57385ddc412d5
Sander Buskens <sbuskens@ripe.net>
2019-09-24 13:07:55 +0200

    Refactored method names for refreshing main and grs sources. Refresh main source on init.

--- whois-scheduler/src/test/java/net/ripe/db/whois/scheduler/task/grs/GrsImporterApnicTestIntegration.java
+++ whois-scheduler/src/test/java/net/ripe/db/whois/scheduler/task/grs/GrsImporterApnicTestIntegration.java
@@ -115,7 +115,7 @@ public class GrsImporterApnicTestIntegration extends AbstractSchedulerIntegratio
     public void setUp() throws Exception {
         // initialize authoritativeresource
         authoritativeResourceImportTask.run();
-        authoritativeResourceData.refreshAllSources();
+        authoritativeResourceData.refreshGrsSources();
 
         grsImporter.setGrsImportEnabled(true);
         queryServer.start();
