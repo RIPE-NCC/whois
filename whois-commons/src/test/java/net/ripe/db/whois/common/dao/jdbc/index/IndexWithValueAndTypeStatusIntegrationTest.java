71ac45d0ddccd558bb7d4b4e4bb83ee7e43aecb4
mahesh aggarwal <maggarwal@ripe.net>
2019-10-30 16:40:04 +0100

    test cases

--- whois-commons/src/test/java/net/ripe/db/whois/common/dao/jdbc/index/IndexWithValueAndTypeStatusIntegrationTest.java
+++ whois-commons/src/test/java/net/ripe/db/whois/common/dao/jdbc/index/IndexWithValueAndTypeStatusIntegrationTest.java
@@ -4,75 +4,142 @@ import net.ripe.db.whois.common.IntegrationTest;
 import net.ripe.db.whois.common.dao.RpslObjectInfo;
 import net.ripe.db.whois.common.rpsl.AttributeType;
 import net.ripe.db.whois.common.rpsl.ObjectType;
+import net.ripe.db.whois.common.rpsl.RpslObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
 @Category(IntegrationTest.class)
 public class IndexWithValueAndTypeStatusIntegrationTest extends IndexIntegrationTestBase {
     private IndexStrategy subject;
 
+    private static final RpslObject OWNER_MNT = RpslObject.parse("" +
+            "mntner:      OWNER-MNT\n" +
+            "descr:       Owner Maintainer\n" +
+            "admin-c:     TP1-TEST\n" +
+            "upd-to:      noreply@ripe.net\n" +
+            "auth:        MD5-PW $1$d9fKeTr2$Si7YudNf4rUGmR71n/cqk/ #test\n" +
+            "auth:        SSO person@net.net\n" +
+            "mnt-by:      OWNER-MNT\n" +
+            "source:      TEST\n");
+
+    private static final RpslObject TEST_PERSON = RpslObject.parse("" +
+            "person:    Test Person\n" +
+            "address:   Singel 258\n" +
+            "phone:     +31 6 12345678\n" +
+            "nic-hdl:   TP1-TEST\n" +
+            "mnt-by:    OWNER-MNT\n" +
+            "source:    TEST\n");
+
+    private static final RpslObject TEST_ROLE = RpslObject.parse("" +
+            "role:      Test Role\n" +
+            "address:   Singel 258\n" +
+            "phone:     +31 6 12345678\n" +
+            "nic-hdl:   TR1-TEST\n" +
+            "admin-c:   TR1-TEST\n" +
+            "abuse-mailbox: abuse@test.net\n" +
+            "mnt-by:    OWNER-MNT\n" +
+            "source:    TEST\n");
+
+    private static final RpslObject TEST_OTHER_ORGANISATION = RpslObject.parse("" +
+            "organisation:   ORG-TO1-TEST\n" +
+            "org-name:       Test Organisation\n" +
+            "status:         OTHER\n" +
+            "mnt-by:         OWNER-MNT\n" +
+            "source:         TEST\n");
+
     @Before
     public void setUp() throws Exception {
-        subject = IndexStrategies.get(AttributeType.ROLE);
+        databaseHelper.addObject("person: Test Person\nnic-hdl: TP1-TEST");
+        databaseHelper.addObject("role: Test Role\nnic-hdl: TR1-TEST");
+        databaseHelper.addObject(OWNER_MNT);
+        databaseHelper.updateObject(TEST_PERSON);
+        databaseHelper.updateObject(TEST_ROLE);
+        databaseHelper.addObject(TEST_OTHER_ORGANISATION);
+
+        subject = IndexStrategies.get(AttributeType.STATUS);
     }
 
     @Test
-    public void findSingleRoleByNameOneWord() {
-        databaseHelper.addObject("role:RIPE DBM\nnic-hdl:RD2964-RIPE");
+    public void findAutnumBYStatusIndex() {
+        databaseHelper.addObject("" +
+                "aut-num:        AS102\n" +
+                "org:            ORG-TO1-TEST\n" +
+                "status:         ASSIGNED\n" +
+                "as-name:        End-User-2\n" +
+                "descr:          description\n" +
+                "admin-c:        TP1-TEST\n" +
+                "tech-c:         TP1-TEST\n" +
+                "mnt-by:         OWNER-MNT\n" +
+                "source:         TEST\n");
 
-        List<RpslObjectInfo> result = subject.findInIndex(whoisTemplate, "RIPE");
+        List<RpslObjectInfo> result = subject.findInIndex(whoisTemplate, "ASSIGNED");
 
         assertThat(result, hasSize(1));
 
         RpslObjectInfo rpslObject = result.get(0);
-        assertThat(rpslObject.getObjectType(), is(ObjectType.ROLE));
-        assertThat(rpslObject.getKey(), is("RD2964-RIPE"));
+        assertThat(rpslObject.getObjectType(), is(ObjectType.AUT_NUM));
+        assertThat(rpslObject.getKey(), is("AS102"));
     }
 
     @Test
-    public void findSingleRoleByNameTwoWords() {
+    public void findInetnumByStatusIndex() {
 
-        databaseHelper.addObject("role:RIPE DBM\nnic-hdl:RD2964-RIPE");
-        databaseHelper.addObject("role:RIPE BA\nnic-hdl:BA-RIPE");
+        databaseHelper.addObject(
+                "inet6num:       2001:2002:2003::/48\n" +
+                        "netname:        RIPE-NCC\n" +
+                        "descr:          Private Network\n" +
+                        "country:        NL\n" +
+                        "tech-c:         TP1-TEST\n" +
+                        "status:         ASSIGNED PA\n" +
+                        "mnt-by:         OWNER-MNT\n" +
+                        "mnt-lower:      OWNER-MNT\n" +
+                        "source:         TEST"
+        );
 
-        List<RpslObjectInfo> result = subject.findInIndex(whoisTemplate, "RIPE BA");
+        databaseHelper.addObject(
+                "inetnum:   10.0.0.0 - 10.255.255.255\n" +
+                        "netname:   TEST-NET\n" +
+                        "descr:     description\n" +
+                        "country:   NL\n" +
+                        "admin-c:   TP1-TEST\n" +
+                        "tech-c:    TP1-TEST\n" +
+                        "status:    ALLOCATED PA\n" +
+                        "mnt-by:    OWNER-MNT\n" +
+                        "source:    TEST\n");
+
+        databaseHelper.addObject(
+                "inetnum:   10.0.0.0 - 10.0.0.255\n" +
+                        "org:       ORG-TO1-TEST\n" +
+                        "netname:   TEST-NET\n" +
+                        "descr:     description\n" +
+                        "country:   NL\n" +
+                        "admin-c:   TP1-TEST\n" +
+                        "tech-c:    TP1-TEST\n" +
+                        "status:    ALLOCATED PA\n" +
+                        "mnt-by:    OWNER-MNT\n" +
+                        "source:    TEST\n");
+
+        List<RpslObjectInfo> result = subject.findInIndex(whoisTemplate, "ASSIGNED PA");
 
         assertThat(result, hasSize(1));
         RpslObjectInfo rpslObject = result.get(0);
 
-        assertThat(rpslObject.getObjectType(), is(ObjectType.ROLE));
-        assertThat(rpslObject.getKey(), is("BA-RIPE"));
-    }
+        assertThat(rpslObject.getObjectType(), is(ObjectType.INET6NUM));
+        assertThat(rpslObject.getKey(), is("2001:2002:2003::/48"));
 
-    @Test
-    public void findTwoRolesByName() {
-        databaseHelper.addObject("role:RIPE DBM\nnic-hdl:RD2964-RIPE");
-        databaseHelper.addObject("role:RIPE BA\nnic-hdl:BA-RIPE");
+        List<RpslObjectInfo> result2 = subject.findInIndex(whoisTemplate, "ALLOCATED PA");
+        assertThat(result2, hasSize(2));
 
-        List<RpslObjectInfo> result = subject.findInIndex(whoisTemplate, "RIPE");
-
-        assertThat(result, hasSize(2));
-        assertThat(result.get(0).getObjectType(), is(ObjectType.ROLE));
-        assertThat(result.get(1).getObjectType(), is(ObjectType.ROLE));
-        assertThat(result.get(0), is(not(result.get(1))));
-    }
-
-    @Test
-    public void findSingleRoleManyNames() {
-        databaseHelper.addObject("role: the quick brown fox jumped over lazy dog\nnic-hdl:RD1-RIPE");
-
-        List<RpslObjectInfo> result = subject.findInIndex(whoisTemplate, "the quick brown fox jumped over lazy dog");
-
-        assertThat(result, hasSize(1));
-        assertThat(result.get(0).getKey(), is("RD1-RIPE"));
+        assertThat(result2.stream().map( inetnum -> inetnum.getKey()).collect(Collectors.toSet()), containsInAnyOrder("10.0.0.0 - 10.0.0.255", "10.0.0.0 - 10.255.255.255"));
     }
 
     @Test
