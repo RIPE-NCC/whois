580823753355e091a9b8ef205188c2789a02928d
mahesh aggarwal <maggarwal@ripe.net>
2020-01-06 16:27:08 +0100

    nrtm service to be public not memebr specific

--- whois-nrtm/src/test/java/net/ripe/db/whois/nrtm/integration/AbstractNrtmIntegrationBase.java
+++ whois-nrtm/src/test/java/net/ripe/db/whois/nrtm/integration/AbstractNrtmIntegrationBase.java
@@ -6,7 +6,6 @@ import net.ripe.db.whois.common.dao.jdbc.AbstractDatabaseHelperIntegrationTest;
 import net.ripe.db.whois.common.rpsl.ObjectType;
 import net.ripe.db.whois.common.source.Source;
 import net.ripe.db.whois.common.source.SourceContext;
-import net.ripe.db.whois.nrtm.AccessControlList;
 import net.ripe.db.whois.nrtm.NrtmServer;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +20,12 @@ import static org.hamcrest.Matchers.is;
 @ContextConfiguration(locations = {"classpath:applicationContext-nrtm-test.xml"})
 public abstract class AbstractNrtmIntegrationBase extends AbstractDatabaseHelperIntegrationTest {
     @Autowired protected NrtmServer nrtmServer;
-    @Autowired protected AccessControlList accessControlList;
     @Autowired protected SourceContext sourceContext;
 
     @Before
     public void beforeAbstractNrtmIntegrationBase() {
         databaseHelper.insertAclMirror("127.0.0.1/32");
         databaseHelper.insertAclMirror("0:0:0:0:0:0:0:1");
-        accessControlList.reload();
     }
 
     protected void objectExists(final ObjectType type, final String key, final boolean exists) {
