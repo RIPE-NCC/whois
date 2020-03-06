83c1d951b7352aaadc18373aa038c95534ff7360
Ed Shryane <eshryane@ripe.net>
2020-03-06 12:48:16 +0100

    Include build timestamp and commit id in the Whois lookup and search responses. (#598)
    * Include build timestamp and commit id in the Whois search response.
    
    * Fixed propOrder.
    
    * Moved new element to end of the response.
    
    * Renamed element commitId to commit-id for consistency.
    
    * The git property is required to exist, or it isn't populated into the properties file by the plugin .
    
    * move the git plugin to the root pom.
    
    * The git property must exist, so set a default value so integration tests will run.
    
    * Move version properties into a separate component. Remove unused properties.
    
    * Return application version in fulltext search response
    
    * Set default git commit-id for e2e tests also.
    
    * Added version to fulltextsearch response in Solr-compatible format.
    
    * Fixed tests
    
    * Fixed tests
    
    * Fixed tests

--- whois-commons/src/main/java/net/ripe/db/whois/common/jdbc/DatabaseVersionCheck.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/jdbc/DatabaseVersionCheck.java
@@ -10,7 +10,6 @@ import net.ripe.db.whois.common.source.SourceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -32,8 +31,6 @@ public class DatabaseVersionCheck {
     static final Splitter VERSION_SPLITTER = Splitter.on(CharMatcher.anyOf(".-")).omitEmptyStrings();
 
     private final ApplicationContext applicationContext;
-    @Value("${application.version}")
-    private String applicationVersion;
 
     @Autowired
     public DatabaseVersionCheck(ApplicationContext applicationContext) {
