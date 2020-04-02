9bf2dfce80aac6447e56d8d1efbd5f6a54ae0cbe
Sander Buskens <sbuskens@ripe.net>
2020-04-02 09:26:50 +0200

    Fix expected json error message output.

--- whois-api/src/test/java/net/ripe/db/whois/api/rest/DomainObjectServiceTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/rest/DomainObjectServiceTestIntegration.java
@@ -228,7 +228,7 @@ public class DomainObjectServiceTestIntegration extends AbstractIntegrationTest 
             final WhoisResources response = e.getResponse().readEntity(WhoisResources.class);
             RestTest.assertErrorCount(response, 1);
             RestTest.assertErrorMessage(response, 0, "Error", "JSON processing exception: %s (line: %s, column: %s)",
-                    "Unrecognized token 'bad': was expecting", "1", "28");
+                    "Unrecognized token 'bad': was expecting", "1", "32");
         }
     }
 
