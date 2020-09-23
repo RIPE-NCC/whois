00f3f0517d219323f5314750698ab29e1b8a3118
Ed Shryane <eshryane@ripe.net>
2020-09-23 16:28:17 +0200

    Removed unused logger and method arguments

--- whois-commons/src/main/java/net/ripe/db/whois/common/rpsl/DummifierRC.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/rpsl/DummifierRC.java
@@ -1,16 +1,12 @@
 package net.ripe.db.whois.common.rpsl;
 
 import com.google.common.collect.Lists;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class DummifierRC extends DummifierCurrent {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DummifierRC.class);
 
     @Override
     public RpslObject dummify(final int version, final RpslObject rpslObject) {
@@ -20,11 +16,10 @@ public class DummifierRC extends DummifierCurrent {
             RpslAttribute replacement = attributes.get(i);
 
             replacement = dummifyOrgName(rpslObject, replacement);
-            replacement = dummifyDescr(rpslObject, replacement);
-            replacement = dummifyRemarks(rpslObject, replacement);
-            replacement = dummifyAsName(rpslObject, replacement);
-            replacement = dummifyOwner(rpslObject, replacement);
-
+            replacement = dummifyDescr(replacement);
+            replacement = dummifyRemarks(replacement);
+            replacement = dummifyAsName(replacement);
+            replacement = dummifyOwner(replacement);
 
             attributes.set(i, replacement);
         }
@@ -41,7 +36,7 @@ public class DummifierRC extends DummifierCurrent {
         return new RpslAttribute(AttributeType.ORG_NAME, String.format("Dummy org-name for %s", rpslObject.getKey().toUpperCase()));
     }
 
-    private RpslAttribute dummifyDescr(final RpslObject rpslObject, final RpslAttribute rpslAttribute) {
+    private RpslAttribute dummifyDescr(final RpslAttribute rpslAttribute) {
         if (rpslAttribute.getType() != AttributeType.DESCR) {
             return rpslAttribute;
         }
@@ -49,7 +44,7 @@ public class DummifierRC extends DummifierCurrent {
         return new RpslAttribute(AttributeType.DESCR, "***");
     }
 
-    private RpslAttribute dummifyRemarks(final RpslObject rpslObject, final RpslAttribute rpslAttribute) {
+    private RpslAttribute dummifyRemarks(final RpslAttribute rpslAttribute) {
         if (rpslAttribute.getType() != AttributeType.REMARKS) {
             return rpslAttribute;
         }
@@ -57,7 +52,7 @@ public class DummifierRC extends DummifierCurrent {
         return new RpslAttribute(AttributeType.REMARKS, "***");
     }
 
-    private RpslAttribute dummifyAsName(final RpslObject rpslObject, final RpslAttribute rpslAttribute) {
+    private RpslAttribute dummifyAsName(final RpslAttribute rpslAttribute) {
         if (rpslAttribute.getType() != AttributeType.AS_NAME) {
             return rpslAttribute;
         }
@@ -65,7 +60,7 @@ public class DummifierRC extends DummifierCurrent {
         return new RpslAttribute(AttributeType.AS_NAME, "***");
     }
 
-    private RpslAttribute dummifyOwner(final RpslObject rpslObject, final RpslAttribute rpslAttribute) {
+    private RpslAttribute dummifyOwner(final RpslAttribute rpslAttribute) {
         if (rpslAttribute.getType() != AttributeType.OWNER) {
             return rpslAttribute;
         }
