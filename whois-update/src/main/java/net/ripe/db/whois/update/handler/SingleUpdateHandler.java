5b8b8e19f8180a6458f00a7bb3ae0ead40e66db4
mahesh aggarwal <maggarwal@ripe.net>
2020-12-16 13:52:40 +0100

    delete with cidr  supported

--- whois-update/src/main/java/net/ripe/db/whois/update/handler/SingleUpdateHandler.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/handler/SingleUpdateHandler.java
@@ -102,7 +102,7 @@ public class SingleUpdateHandler {
         }
 
         if (action == Action.DELETE && originalObject == null) {
-            updateContext.addMessage(update, UpdateMessages.objectNotFound(updatedObject.getFormattedKey()));
+            updateContext.addMessage(update, UpdateMessages.objectNotFound(update.getSubmittedObject().getFormattedKey()));
         }
 
         // up to this point, updatedObject could have structural+syntax errors (unknown attributes, etc...), bail out if so
