7ce7d7d8ee26c375b0d2f37ee1f5dda631cde574
sbusk <sbuskens@ripe.net>
2021-01-07 17:31:54 +0100

    Aws (#721)
    * Create whois docker image.
    
    * Hardcoded config locations for now.
    
    * Push docker image to Gitlab container registry.
    
    * Use build image with docker client.
    
    * Use Gitlab ECS deployment template.
    
    * Deploy to AWS.
    
    * Deploy to AWS.
    
    * Use AWS CLI from whois-build image (as Gitlab's aws-ecs image does not provide docker).
    
    * Use AWS v2 CLI.
    
    * Need more debugging info.
    
    * Need more debugging info.
    
    * Don't need this.
    
    * Fiddling with ecs task definition format..
    
    * Remove script debugging.
    
    * Set deployment to manual.
    
    * Deploy aws (#654)
    
    * Use build image with docker client.
    
    * Use Gitlab ECS deployment template.
    
    * Deploy to AWS.
    
    * Deploy to AWS.
    
    * Use AWS CLI from whois-build image (as Gitlab's aws-ecs image does not provide docker).
    
    * Use AWS v2 CLI.
    
    * Need more debugging info.
    
    * Need more debugging info.
    
    * Don't need this.
    
    * Fiddling with ecs task definition format..
    
    * Remove script debugging.
    
    * Set deployment to manual.
    
    * Renamed whois-ecr docker registry to whois.
    
    * Aws ripe profile (#653)
    
    * use aws profile and ripe profile
    
    * add todo
    
    * remove propertysource for aws profile
    
    * basic refactoring
    
    * remove hard coded spring profile
    
    * use env variable for aws region
    
    * automate properties
    
    * use command line arguments
    
    * Pickup instance hostname to seperate instance filesystems.
    
    * Create any missing output directories.
    
    * Output dirs should already be created automatically.
    
    * Rewrites (#656)
    
    * Handle rewrites in AWS environment.
    
    * Use feature toggle for rewrites rather than tying it into AWS profile (easier to test).
    Corrected handler chaining.
    
    * Added integration tests
    
    * Fix syncupdates rewrite.
    
    Co-authored-by: Ed Shryane <eshryane@ripe.net>
    
    * Added UseContainerSupport.
    
    * added ability to deploy to aws for prepdev (#682)
    
    * Adding deploy_aws_prepdev job. Still needs configuration
    
    * logging  .gitlab-ci.yml
    
    * more logging in .gitlab-ci.yml
    
    * more logging in .gitlab-ci.yml
    
    * removed $
    
    * log
    
    * trying out setting the needed variable
    
    * trying out setting the needed variable
    
    * removed echo linne
    
    * trying out having the expot also in before_script
    
    * trying out staged scope variable
    
    * switching back to using export
    
    Co-authored-by: Dadepo Aderemi <daderemi@ripe.net>
    
    * aws ecs cluster swe -> whois.
    
    * Use latest whois-build image.
    
    * Run Java11.
    
    * genrify deploy template (#709)
    
    * genrify deploy template
    
    * use common template
    
    * syntax errort
    
    * Use latest deploy template.
    
    Co-authored-by: Sander Buskens <sbuskens@ripe.net>
    
    * Differentiate artifact dir.
    
    * Only build container in release phase.
    
    Co-authored-by: maggarwal13 <50230916+maggarwal13@users.noreply.github.com>
    Co-authored-by: mahesh aggarwal <maggarwal@ripe.net>
    Co-authored-by: Ed Shryane <eshryane@ripe.net>
    Co-authored-by: dadepo <dadepo@gmail.com>
    Co-authored-by: Dadepo Aderemi <daderemi@ripe.net>

---
+++ whois-api/src/test/java/net/ripe/db/whois/api/httpserver/RewriteEngineTestIntegration.java
@@ -0,0 +1,90 @@
+package net.ripe.db.whois.api.httpserver;
+
+import net.ripe.db.whois.api.AbstractIntegrationTest;
+import net.ripe.db.whois.api.RestTest;
+import net.ripe.db.whois.api.rdap.domain.Entity;
+import net.ripe.db.whois.api.rest.domain.WhoisResources;
+import net.ripe.db.whois.common.IntegrationTest;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@Category(IntegrationTest.class)
+public class RewriteEngineTestIntegration extends AbstractIntegrationTest {
+
+    @BeforeClass
+    public static void enableRewriteEngine() {
+        System.setProperty("rewrite.engine.enabled", "true");
+    }
+
+    @AfterClass
+    public static void disableRewriteEngine() {
+        System.clearProperty("rewrite.engine.enabled");
+    }
+
+    @Value("${api.rest.baseurl}")
+    private String restApiBaseUrl;
+
+    @Before
+    public void setup() {
+        databaseHelper.addObject(
+            "person: Test Person\n" +
+             "nic-hdl: TP1-TEST\n" +
+             "source: TEST");
+    }
+
+    @Test
+    public void rest_lookup_with_rewrite() {
+        final WhoisResources whoisResources = RestTest.target(getPort(), "test/person/TP1-TEST")
+                .request()
+                .header(HttpHeaders.HOST, getHost(restApiBaseUrl))
+                .get(WhoisResources.class);
+
+        assertThat(whoisResources.getErrorMessages(), is(empty()));
+        assertThat(whoisResources.getWhoisObjects(), hasSize(1));
+    }
+
+    @Test
+    public void rdap_lookup_with_rewrite() {
+        final Entity person = RestTest.target(getPort(), "entity/TP1-TEST")
+                .request()
+                .header(HttpHeaders.HOST, getHost(restApiBaseUrl).replace("rest", "rdap"))
+                .get(Entity.class);
+
+        assertThat(person.getHandle(), is("TP1-TEST"));
+    }
+
+    @Test
+    public void syncupdates_with_rewrite() {
+        final Response response = RestTest.target(getPort(), "?HELP=yes")
+                .request()
+                .header(HttpHeaders.HOST, getHost(restApiBaseUrl).replace("rest", "syncupdates"))
+                .get(Response.class);
+
+        final String responseBody = response.readEntity(String.class);
+        assertThat(responseBody, containsString("You have requested Help information from the RIPE NCC Database"));
+
+    }
+
+    // helper methods
+
+    private String getHost(final String url) {
+        final URI uri = URI.create(url);
+        return uri.getHost();
+    }
+
+
+}
