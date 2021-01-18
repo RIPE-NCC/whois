8c2ba3404ad3f3921234ca5ba0066fd7e0bb5fb8
mahesh aggarwal <maggarwal@ripe.net>
2021-01-18 14:36:39 +0100

    add event listner

---
+++ whois-query/src/main/java/net/ripe/db/whois/query/acl/HazelcastMemberShipListner.java
@@ -0,0 +1,23 @@
+package net.ripe.db.whois.query.acl;
+
+import com.hazelcast.core.MembershipEvent;
+import com.hazelcast.core.MembershipListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HazelcastMemberShipListner  implements MembershipListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HazelcastPersonalObjectAccounting.class);
+
+    @Override
+    public void memberAdded(MembershipEvent membershipEvent) {
+       LOGGER.info("Member added: " + membershipEvent.getMember());
+       LOGGER.info("List of members now :" + membershipEvent.getMembers());
+    }
+
+    @Override
+    public void memberRemoved(MembershipEvent membershipEvent) {
+        LOGGER.info("Member removed: " + membershipEvent.getMember());
+        LOGGER.info("list of members now :" + membershipEvent.getMembers());
+    }
+}
