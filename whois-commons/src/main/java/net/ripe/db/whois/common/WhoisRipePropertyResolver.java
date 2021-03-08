70f22ff01bf29e1b9cfa4920d50f025b2b3b13a1
mahesh aggarwal <maggarwal@ripe.net>
2021-03-08 16:42:30 +0100

    static block  issue

--- whois-commons/src/main/java/net/ripe/db/whois/common/WhoisRipePropertyResolver.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/WhoisRipePropertyResolver.java
@@ -1,7 +1,6 @@
 package net.ripe.db.whois.common;
 
 import net.ripe.db.whois.common.profiles.WhoisProfile;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +9,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @Configuration
 @PropertySources({
@@ -24,7 +20,7 @@ public class WhoisRipePropertyResolver {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WhoisRipePropertyResolver.class);
 
-    static {
+    /*static {
         String hostName = System.getenv("HOSTNAME");
         if (StringUtils.isBlank(hostName)) {
             try {
@@ -42,7 +38,7 @@ public class WhoisRipePropertyResolver {
         final String instanceName = StringUtils.substringBefore(hostName, ".").toUpperCase();
         System.setProperty("instance.name", instanceName);
         LOGGER.info("Instance name is {}", instanceName);
-    }
+    }*/
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties(){
