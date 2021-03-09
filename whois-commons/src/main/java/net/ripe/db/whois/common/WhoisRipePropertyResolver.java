232b2a3f42d41a8c59f647a36710339e996e87f0
mahesh aggarwal <maggarwal@ripe.net>
2021-03-09 12:21:10 +0100

    failsafe report issue

--- whois-commons/src/main/java/net/ripe/db/whois/common/WhoisRipePropertyResolver.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/WhoisRipePropertyResolver.java
@@ -1,6 +1,7 @@
 package net.ripe.db.whois.common;
 
 import net.ripe.db.whois.common.profiles.WhoisProfile;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Configuration
 @PropertySources({
@@ -20,7 +24,7 @@ public class WhoisRipePropertyResolver {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WhoisRipePropertyResolver.class);
 
-    /*static {
+    static {
         String hostName = System.getenv("HOSTNAME");
         if (StringUtils.isBlank(hostName)) {
             try {
@@ -38,7 +42,7 @@ public class WhoisRipePropertyResolver {
         final String instanceName = StringUtils.substringBefore(hostName, ".").toUpperCase();
         System.setProperty("instance.name", instanceName);
         LOGGER.info("Instance name is {}", instanceName);
-    }*/
+    }
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties(){
--- whois-rpsl/src/main/java/net/ripe/db/whois/common/domain/Hosts.java
+++ whois-rpsl/src/main/java/net/ripe/db/whois/common/domain/Hosts.java
@@ -3,16 +3,12 @@ package net.ripe.db.whois.common.domain;
 import org.apache.commons.lang.StringUtils;
 
 public class Hosts {
-    private static final String instanceName;
 
-    static {
-        instanceName = System.getProperty("instance.name");
+    public static String getInstanceName() {
+        final String instanceName = System.getProperty("instance.name");
         if (StringUtils.isBlank(instanceName)) {
             throw new IllegalStateException("Instance name is not defined");
         }
-    }
-
-    public static String getInstanceName() {
         return instanceName;
     }
 }
