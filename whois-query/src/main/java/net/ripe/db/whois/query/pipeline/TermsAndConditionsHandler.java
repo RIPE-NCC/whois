4c1a48a7d70a3ef73f99a41bdbae04ea72a6711b
Dadepo Aderemi <dadepo@gmail.com>
2021-03-23 16:47:49 +0100

    attempting to log results of write operation

--- whois-query/src/main/java/net/ripe/db/whois/query/pipeline/TermsAndConditionsHandler.java
+++ whois-query/src/main/java/net/ripe/db/whois/query/pipeline/TermsAndConditionsHandler.java
@@ -1,21 +1,32 @@
 package net.ripe.db.whois.query.pipeline;
 
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.group.ChannelGroupFutureListener;
 import net.ripe.db.whois.common.Message;
 import net.ripe.db.whois.query.QueryMessages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @ChannelHandler.Sharable
 public class TermsAndConditionsHandler extends ChannelInboundHandlerAdapter {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(TermsAndConditionsHandler.class);
     private static final Message TERMS_AND_CONDITIONS = QueryMessages.termsAndConditions();
 
     @Override
-    public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(TERMS_AND_CONDITIONS);
+    public void channelActive(ChannelHandlerContext ctx) throws InterruptedException {
+        ChannelFuture channelFuture = ctx.writeAndFlush(TERMS_AND_CONDITIONS);
+        channelFuture.addListener(future -> {
+            if (!future.isSuccess()) {
+                LOGGER.info("writing failed: " + future.cause());
+            } else {
+                LOGGER.info("writing succeeded");
+            }
+        });
         ctx.pipeline().remove(this);
         ctx.fireChannelActive();
     }
