e7945a6745e161e0ff886b6e4461fe86268a7996
Dadepo Aderemi <dadepo@gmail.com>
2021-03-26 20:08:53 +0100

    fixing the reference counting issue

--- whois-query/src/main/java/net/ripe/db/whois/query/pipeline/HAProxyMessageChannelHandler.java
+++ whois-query/src/main/java/net/ripe/db/whois/query/pipeline/HAProxyMessageChannelHandler.java
@@ -13,20 +13,24 @@ public class HAProxyMessageChannelHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (msg instanceof HAProxyMessage) {
             HAProxyMessage hapm = (HAProxyMessage) msg;
-            String sourceAddress = hapm.sourceAddress();
-            if (sourceAddress != null) {
-                switch (hapm.proxiedProtocol()) {
-                    case TCP4:
-                    case TCP6:
-                        ctx.channel().attr(ChannelUtil.CLIENT_IP).set(InetAddress.getByName(hapm.sourceAddress()));
-                        break;
-                    default:
-                        throw new UnsupportedOperationException("unsupported proxy protocol " + hapm.proxiedProtocol());
+            try {
+                String sourceAddress = hapm.sourceAddress();
+                if (sourceAddress != null) {
+                    switch (hapm.proxiedProtocol()) {
+                        case TCP4:
+                        case TCP6:
+                            ctx.channel().attr(ChannelUtil.CLIENT_IP).set(InetAddress.getByName(hapm.sourceAddress()));
+                            break;
+                        default:
+                            throw new UnsupportedOperationException("unsupported proxy protocol " + hapm.proxiedProtocol());
+                    }
                 }
-            }
 
-            ctx.pipeline().remove(this);
-            ctx.fireChannelActive(); // trigger channel active again now client ip has been set
+                ctx.pipeline().remove(this);
+                ctx.fireChannelActive(); // trigger channel active again now client ip has been set
+            } finally {
+                hapm.release();
+            }
         }
     }
 }
