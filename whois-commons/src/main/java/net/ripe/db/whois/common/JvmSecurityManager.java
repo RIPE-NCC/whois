9d2d052bdb604f8b77c0e7d4029e6d74bbfb4007
mahesh aggarwal <maggarwal@ripe.net>
2021-04-16 10:32:26 +0200

    jvm ttl properties

---
+++ whois-commons/src/main/java/net/ripe/db/whois/common/JvmSecurityManager.java
@@ -0,0 +1,21 @@
+package net.ripe.db.whois.common;
+
+import net.ripe.db.whois.common.profiles.DeployedProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@DeployedProfile
+public class JvmSecurityManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JvmSecurityManager.class);
+
+    @Autowired
+    public JvmSecurityManager() {
+
+        LOGGER.info("Setting jvm ttl properties");
+        java.security.Security.setProperty("networkaddress.cache.ttl", "30");
+        java.security.Security.setProperty("networkaddress.cache.negative.ttl", "10");
+    }
+}
