dbde7207435921f9f41c9a6ab53d6f2d9ff07062
mahesh aggarwal <maggarwal@ripe.net>
2021-04-19 10:50:35 +0200

    print security properties

--- whois-db/src/main/java/net/ripe/db/whois/db/WhoisServer.java
+++ whois-db/src/main/java/net/ripe/db/whois/db/WhoisServer.java
@@ -62,8 +62,10 @@ public class WhoisServer {
                 .distinct()
                 .forEach(prop -> LOGGER.info("{}: {}", prop, (prop.contains("credentials") || prop.contains("password")) ? "*****" :  applicationContext.getEnvironment().getProperty(prop)));
 
+        LOGGER.info("security file ", java.security.Security.getProperty("security.overridePropertiesFile"));
         LOGGER.info("networkaddress.cache.ttl:", java.security.Security.getProperty("networkaddress.cache.ttl"));
-        LOGGER.info("networkaddress.cache.negative.ttl:", java.security.Security.getProperty("networkaddress.cache.negative.ttl"));
+        LOGGER.info("networkaddress.cache.ttl:", java.security.Security.getProperty("networkaddress.cache.ttl"));
+
 
         LOGGER.info("Whois server started in {}", stopwatch.stop());
     }
