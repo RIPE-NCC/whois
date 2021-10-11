b0a44ca57ba2f7b01b23676aba0d4a06ff126982
Sander Buskens <sbuskens@ripe.net>
2021-10-11 14:05:31 +0200

    Support bracketed notation in Ipv6Resource

--- whois-api/src/main/java/net/ripe/db/whois/api/httpserver/RemoteAddressFilter.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/httpserver/RemoteAddressFilter.java
@@ -1,8 +1,5 @@
 package net.ripe.db.whois.api.httpserver;
 
-import com.google.common.base.Splitter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.Filter;
@@ -17,10 +14,6 @@ import java.io.IOException;
 
 @Component
 public class RemoteAddressFilter implements Filter {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RemoteAddressFilter.class);
-
-    private static final Splitter COMMA_SPLITTER = Splitter.on(',').omitEmptyStrings().trimResults();
 
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
@@ -54,10 +47,8 @@ public class RemoteAddressFilter implements Filter {
             return remoteAddress;
         }
 
-        //TODO: Ipv6 address will have brackets by default. Make changes to ipv6 parser instead and check all usages of getRemoteAddress
         private static String getRemoteAddress(final HttpServletRequest request) {
-            final String address = request.getRemoteAddr();
-            return (address.startsWith("[") && address.endsWith("]")) ? address.substring(1, address.length() - 1) : address;
+            return request.getRemoteAddr();
         }
     }
 }
