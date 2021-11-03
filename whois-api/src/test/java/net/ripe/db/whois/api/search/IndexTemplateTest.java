c27734159b11cb0e2837371498e4bca06482919b
sbusk <sbuskens@ripe.net>
2021-11-03 15:39:17 +0100

    Lucene 8 10 (#934)
    * Update Lucene to 8.10.1.
    
    * Fix test (expectations).
    
    * Added test for FulltextAnalyzer (to capture/describe/explain existing behaviour).
    Change test expectations in highlighting.
    
    * Use Lucene 8.9.0. (version needs to be identical to the one used by the ES version we intend on using).
    
    * Added comment clarifying that Lucene version needs to the same as the ElasticSarch Lucene version.
    
    Co-authored-by: Ed Shryane <eshryane@ripe.net>

--- whois-api/src/test/java/net/ripe/db/whois/api/search/IndexTemplateTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/search/IndexTemplateTest.java
@@ -1,19 +1,17 @@
 package net.ripe.db.whois.api.search;
 
 import net.ripe.db.whois.api.fulltextsearch.IndexTemplate;
+import net.ripe.db.whois.api.fulltextsearch.IndexTemplate.SearchCallback;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.facet.taxonomy.TaxonomyReader;
 import org.apache.lucene.facet.taxonomy.TaxonomyWriter;
-import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopScoreDocCollector;
@@ -30,8 +28,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.fail;
 
 public class IndexTemplateTest {
@@ -56,31 +54,25 @@ public class IndexTemplateTest {
 
     @Test
     public void index_and_search() throws IOException, ParseException {
-        subject.write(new IndexTemplate.WriteCallback() {
-            @Override
-            public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                addDoc(indexWriter, "Lucene in Action", "193398817");
-                addDoc(indexWriter, "Lucene for Dummies", "55320055Z");
-                addDoc(indexWriter, "Managing Gigabytes", "55063554A");
-                addDoc(indexWriter, "The Art of Computer Science", "9900333X");
+        subject.write((indexWriter, taxonomyWriter) -> {
+            addDoc(indexWriter, "Lucene in Action", "193398817");
+            addDoc(indexWriter, "Lucene for Dummies", "55320055Z");
+            addDoc(indexWriter, "Managing Gigabytes", "55063554A");
+            addDoc(indexWriter, "The Art of Computer Science", "9900333X");
 
-                assertThat(indexWriter.numDocs(), is(4));
-            }
+            assertThat(indexWriter.getPendingNumDocs(), is(4L));
         });
 
         final Query query = new QueryParser("title", analyzer).parse("Lucene");
-        subject.search(new IndexTemplate.SearchCallback<Void>() {
-            @Override
-            public Void search(final IndexReader indexReader, final TaxonomyReader taxonomyReader, final IndexSearcher indexSearcher) throws IOException {
-                final TopScoreDocCollector collector = TopScoreDocCollector.create(10);
-                indexSearcher.search(query, collector);
-                final ScoreDoc[] hits = collector.topDocs().scoreDocs;
+        subject.search((SearchCallback<Void>) (indexReader, taxonomyReader, indexSearcher) -> {
+            final TopScoreDocCollector collector = TopScoreDocCollector.create(10, Integer.MAX_VALUE);
+            indexSearcher.search(query, collector);
+            final ScoreDoc[] hits = collector.topDocs().scoreDocs;
 
-                assertThat(hits.length, is(2));
-                assertThat(indexReader.document(hits[0].doc).get("isbn"), is("193398817"));
-                assertThat(indexReader.document(hits[1].doc).get("isbn"), is("55320055Z"));
-                return null;
-            }
+            assertThat(hits.length, is(2));
+            assertThat(indexReader.document(hits[0].doc).get("isbn"), is("193398817"));
+            assertThat(indexReader.document(hits[1].doc).get("isbn"), is("55320055Z"));
+            return null;
         });
     }
 
@@ -118,43 +110,34 @@ public class IndexTemplateTest {
 
     @Test
     public void search_concurrent() throws Exception {
-        final int nrDocs = 1000;
+        final long nrDocs = 1000;
         final int nrThreads = 100;
 
-        subject.write(new IndexTemplate.WriteCallback() {
-            @Override
-            public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                for (int i = 0; i < nrDocs; i++) {
-                    addDoc(indexWriter, "title: " + i, "isbn: " + i);
-                }
-
-                assertThat(indexWriter.numDocs(), is(nrDocs));
+        subject.write((indexWriter, taxonomyWriter) -> {
+            for (int i = 0; i < nrDocs; i++) {
+                addDoc(indexWriter, "title: " + i, "isbn: " + i);
             }
+
+            assertThat(indexWriter.getPendingNumDocs(), is(nrDocs));
         });
 
         final CountDownLatch countDownLatch = new CountDownLatch(nrThreads);
         final ExecutorService executorService = Executors.newFixedThreadPool(nrThreads);
         final Query query = new QueryParser("title", analyzer).parse("title");
         for (int i = 0; i < nrThreads; i++) {
-            executorService.submit(new Callable<Void>() {
-                @Override
-                public Void call() throws Exception {
-                    try {
-                        subject.search(new IndexTemplate.SearchCallback<Void>() {
-                            @Override
-                            public Void search(final IndexReader indexReader, final TaxonomyReader taxonomyReader, final IndexSearcher indexSearcher) throws IOException {
-                                final TopScoreDocCollector collector = TopScoreDocCollector.create(10);
-                                indexSearcher.search(query, collector);
-                                assertThat(collector.topDocs().scoreDocs.length, is(nrDocs));
-                                return null;
-                            }
-                        });
-                    } finally {
-                        countDownLatch.countDown();
-                    }
-
-                    return null;
+            executorService.submit((Callable<Void>) () -> {
+                try {
+                    subject.search((SearchCallback<Void>) (indexReader, taxonomyReader, indexSearcher) -> {
+                        final TopScoreDocCollector collector = TopScoreDocCollector.create(10, Integer.MAX_VALUE);
+                        indexSearcher.search(query, collector);
+                        assertThat(collector.topDocs().scoreDocs.length, is(nrDocs));
+                        return null;
+                    });
+                } finally {
+                    countDownLatch.countDown();
                 }
+
+                return null;
             });
         }
 
@@ -164,12 +147,9 @@ public class IndexTemplateTest {
     @Test
     public void out_of_memory() throws IOException {
         try {
-            subject.write(new IndexTemplate.WriteCallback() {
-                @Override
-                public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                    addDoc(indexWriter, "title", "isbn");
-                    throw new OutOfMemoryError();
-                }
+            subject.write((indexWriter, taxonomyWriter) -> {
+                addDoc(indexWriter, "title", "isbn");
+                throw new OutOfMemoryError();
             });
 
             fail("Expected exception");
@@ -179,12 +159,7 @@ public class IndexTemplateTest {
         // no rollback on out of memory, document was written properly
         assertThat(numDocs(), is(1));
 
-        subject.write(new IndexTemplate.WriteCallback() {
-            @Override
-            public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                addDoc(indexWriter, "title", "isbn");
-            }
-        });
+        subject.write((indexWriter, taxonomyWriter) -> addDoc(indexWriter, "title", "isbn"));
 
         assertThat(numDocs(), is(2));
     }
@@ -192,12 +167,9 @@ public class IndexTemplateTest {
     @Test
     public void runtime_exception() throws IOException {
         try {
-            subject.write(new IndexTemplate.WriteCallback() {
-                @Override
-                public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                    addDoc(indexWriter, "title", "isbn");
-                    throw new IllegalStateException();
-                }
+            subject.write((indexWriter, taxonomyWriter) -> {
+                addDoc(indexWriter, "title", "isbn");
+                throw new IllegalStateException();
             });
 
             fail("Expected exception");
@@ -206,12 +178,7 @@ public class IndexTemplateTest {
 
         assertThat(numDocs(), is(0));
 
-        subject.write(new IndexTemplate.WriteCallback() {
-            @Override
-            public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                addDoc(indexWriter, "title", "isbn");
-            }
-        });
+        subject.write((indexWriter, taxonomyWriter) -> addDoc(indexWriter, "title", "isbn"));
 
         assertThat(numDocs(), is(1));
     }
@@ -219,12 +186,9 @@ public class IndexTemplateTest {
     @Test
     public void io_exception() throws IOException {
         try {
-            subject.write(new IndexTemplate.WriteCallback() {
-                @Override
-                public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                    addDoc(indexWriter, "title", "isbn");
-                    throw new IOException();
-                }
+            subject.write((indexWriter, taxonomyWriter) -> {
+                addDoc(indexWriter, "title", "isbn");
+                throw new IOException();
             });
 
             fail("Expected exception");
@@ -233,12 +197,7 @@ public class IndexTemplateTest {
 
         assertThat(numDocs(), is(0));
 
-        subject.write(new IndexTemplate.WriteCallback() {
-            @Override
-            public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                addDoc(indexWriter, "title", "isbn");
-            }
-        });
+        subject.write((indexWriter, taxonomyWriter) -> addDoc(indexWriter, "title", "isbn"));
 
         assertThat(numDocs(), is(1));
     }
@@ -248,24 +207,14 @@ public class IndexTemplateTest {
         assertThat(numDocs(), is(0));
 
         for (int i = 1; i <= 10; i++) {
-            subject.write(new IndexTemplate.WriteCallback() {
-                @Override
-                public void write(final IndexWriter indexWriter, final TaxonomyWriter taxonomyWriter) throws IOException {
-                    addDoc(indexWriter, "title", String.valueOf("isbn"));
-                }
-            });
+            subject.write((indexWriter, taxonomyWriter) -> addDoc(indexWriter, "title", "isbn"));
 
             assertThat(numDocs(), is(i));
         }
     }
 
     int numDocs() throws IOException {
-        return subject.read(new IndexTemplate.ReadCallback<Integer>() {
-            @Override
-            public Integer read(final IndexReader indexReader, final TaxonomyReader taxonomyReader) throws IOException {
-                return indexReader.numDocs();
-            }
-        });
+        return subject.read((indexReader, taxonomyReader) -> indexReader.numDocs());
     }
 
     void addDoc(final IndexWriter indexWriter, final String title, final String isbn) throws IOException {
