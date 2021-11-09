52860566a1c2ce76ada4c1b97b3f184078f3a189
Jacopo Bartolini <jbartolini@ripe.net>
2021-11-09 09:06:53 +0100

    Updated junit 4 to 5

--- whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/inetnum/GeofeedValidatorTest.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/inetnum/GeofeedValidatorTest.java
@@ -4,17 +4,17 @@ import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.update.domain.PreparedUpdate;
 import net.ripe.db.whois.update.domain.UpdateContext;
 import net.ripe.db.whois.update.domain.UpdateMessages;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class GeofeedValidatorTest {
 
     @Mock
