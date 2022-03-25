4990c91d3b56dffb4e2d31ac7041d841aecaea3d
mahesh aggarwal <maggarwal@ripe.net>
2022-03-25 12:37:03 +0100

    refactor conditional bean

--- whois-api/src/main/java/net/ripe/db/whois/api/autocomplete/LuceneSearchCondition.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/autocomplete/LuceneSearchCondition.java
@@ -1,16 +1,12 @@
 package net.ripe.db.whois.api.autocomplete;
 
-import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
-public class LuceneSearchCondition implements Condition {
+public class LuceneSearchCondition extends ElasticSearchCondition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        if( !context.getEnvironment().containsProperty("elasticsearch.enabled")) {
-            return true;
-        }
-        return !Boolean.parseBoolean(context.getEnvironment().getProperty("elasticsearch.enabled"));
+        return !super.matches(context, metadata);
     }
 }
