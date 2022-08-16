fc0f8a7e5f18deb243986100243db129defdfe40
mherran <mherran@ripe.net>
2022-08-16 15:37:42 +0200

    feat: require a transaction

--- whois-update/src/test/java/net/ripe/db/whois/update/dao/AbstractUpdateDaoIntegrationTest.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/dao/AbstractUpdateDaoIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.transaction.IllegalTransactionStateException;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,29 +13,21 @@ import java.util.Collections;
 import java.util.List;
 
 @ContextConfiguration(locations = {"classpath:applicationContext-update-test.xml"})
-@Transactional(propagation = Propagation.NEVER)
+@Transactional(propagation = Propagation.REQUIRED)
 public abstract class AbstractUpdateDaoIntegrationTest extends AbstractDaoIntegrationTest {
     @Autowired(required = false) protected List<ApplicationService> applicationServices = Collections.emptyList();
 
     @BeforeEach
     public void startServer() {
         for (final ApplicationService applicationService : applicationServices) {
-            try {
-                applicationService.start();
-            } catch (IllegalTransactionStateException illegalTransactionStateException){
-                System.out.println("Service already working");
-            }
+            applicationService.start();
         }
     }
 
     @AfterEach
     public void stopServer() {
         for (final ApplicationService applicationService : applicationServices) {
-            try {
-                applicationService.stop(true);
-            } catch (IllegalTransactionStateException illegalTransactionStateException){
-                System.out.println("Service already stopped");
-            }
+            applicationService.stop(true);
         }
     }
 }
