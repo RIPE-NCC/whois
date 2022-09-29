3b4d4edf73d2c8376bf6120cff9343a1c3643cec
Ed Shryane <eshryane@ripe.net>
2022-09-29 14:57:47 +0200

    Split healthchecks into separate components

---
+++ whois-api/src/main/java/net/ripe/db/whois/api/healthcheck/HealthCheck.java
@@ -0,0 +1,7 @@
+package net.ripe.db.whois.api.healthcheck;
+
+public interface HealthCheck {
+
+    boolean check();
+
+}
---
+++ whois-api/src/main/java/net/ripe/db/whois/api/healthcheck/HealthCheckService.java
@@ -0,0 +1,39 @@
+package net.ripe.db.whois.api.healthcheck;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.util.List;
+
+@Component
+@Path("/healthcheck")
+public class HealthCheckService {
+
+    private final List<HealthCheck> healthChecks;
+
+    @Autowired
+    public HealthCheckService(final List<HealthCheck> healthChecks) {
+        this.healthChecks = healthChecks;
+    }
+
+    @GET
+    public Response check() {
+        boolean healthy = true;
+        for (HealthCheck healthCheck : healthChecks) {
+            if (!healthCheck.check()) {
+                healthy = false;
+                break;
+            }
+        }
+
+        return healthy ?
+                Response.ok().entity("OK").build() :
+                Response.status(Status.SERVICE_UNAVAILABLE).entity("DISABLED").build();
+    }
+
+
+}
