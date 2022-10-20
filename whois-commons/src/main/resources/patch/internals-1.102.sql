b8cc6c0d6e94209da973cf045bcaea089ec9a964
Mahesh Aggarwal <maggarwal@ripe.net>
2022-10-20 11:37:44 +0200

    prepopulate tables default maintainer

---
+++ whois-commons/src/main/resources/patch/internals-1.102.sql
@@ -0,0 +1,28 @@
+
+DROP TABLE IF EXISTS `default_maintainer`;
+CREATE TABLE `default_maintainer` (
+    `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `org` varchar(256) NOT NULL,
+    `mntner` varchar(256) NOT NULL,
+    `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `uuid` varchar(256) NOT NULL,
+    `email` varchar(256),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO  `default_maintainer` (`org`, `mntner`, `timestamp`, `uuid`,  `email`) select `org`, `mntner`, `timestamp`, `uuid`,  `email` from `default_maintainer_history` where in_progress = 1;
+
+DROP TABLE IF EXISTS `default_maintainer_sync`;
+CREATE TABLE `default_maintainer_sync` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `org` varchar(256) NOT NULL,
+    `mntner` varchar(256) NOT NULL,
+    `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `email` varchar(256) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO  `default_maintainer_sync` (`org`, `mntner`, `timestamp`, `email`) select `org`, `mntner`, `timestamp`,  `email` from `default_maintainer_sync_history`  where is_synchronised = 1;
+
+TRUNCATE version;
+INSERT INTO version VALUES ('internals-1.101');
