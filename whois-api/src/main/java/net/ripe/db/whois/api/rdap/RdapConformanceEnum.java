05dba3414b0bf4a2c7f2e6a0e3c9eb642250e610
mherran <mherran@ripe.net>
2022-11-22 16:27:20 +0100

    feat: refactor enum

--- whois-api/src/main/java/net/ripe/db/whois/api/rdap/RdapConformanceEnum.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rdap/RdapConformanceEnum.java
@@ -3,8 +3,5 @@ package net.ripe.db.whois.api.rdap;
 public enum RdapConformanceEnum {
     CIRD_0("cidr0"), LEVEL_0("rdap_level_0"), FLAT_MODEL("nro_rdap_profile_asn_flat_0");
 
-    private String value;
-    RdapConformanceEnum(String value){
-        this.value = value;
-    }
+    RdapConformanceEnum(final String value){}
 }
