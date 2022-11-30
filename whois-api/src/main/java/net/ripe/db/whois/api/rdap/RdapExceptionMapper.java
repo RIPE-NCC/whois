b303a9801f97a18e9c7f168fe4c1adb1070e6f1f
mherran <mherran@ripe.net>
2022-11-30 10:17:28 +0100

    feat: use if instead of if else

--- whois-api/src/main/java/net/ripe/db/whois/api/rdap/RdapExceptionMapper.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rdap/RdapExceptionMapper.java
@@ -33,19 +33,22 @@ public class RdapExceptionMapper implements ExceptionMapper<Exception> {
     public Response toResponse(final Exception exception) {
         if (exception instanceof IllegalArgumentException || exception instanceof JsonProcessingException){
             return createErrorResponse(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
-        } else if (exception instanceof ParamException){
+        }
+        if (exception instanceof ParamException){
             final String parameterName = ((ParamException) exception).getParameterName();
             return createErrorResponse(HttpServletResponse.SC_BAD_REQUEST, "400 Bad Request",
                     "unknown " + parameterName);
-        } else if (exception instanceof RdapException){
+        }
+        if (exception instanceof RdapException){
             final RdapException rdapException = (RdapException) exception;
             return createErrorResponse(rdapException.getErrorCode(), rdapException.getErrorTitle(),
                     rdapException.getErrorDescription() == null? "Unknown error cause" :
                             rdapException.getErrorDescription());
-        } else {
-            LOGGER.error("Unexpected", exception);
-            return createErrorResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, exception.getMessage());
         }
+
+        LOGGER.error("Unexpected", exception);
+        return createErrorResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, exception.getMessage());
+
     }
 
     private Response createErrorResponse(final int status, final String errorTitle, final String ... errorMessage) {
