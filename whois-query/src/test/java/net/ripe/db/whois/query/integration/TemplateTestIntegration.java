2a5c6f788cc7ceff1976c87a72244a4011eda8d0
mherran <mherran@ripe.net>
2022-12-06 14:21:28 +0100

    feat: typo fix and use AS uppercase

--- whois-query/src/test/java/net/ripe/db/whois/query/integration/TemplateTestIntegration.java
+++ whois-query/src/test/java/net/ripe/db/whois/query/integration/TemplateTestIntegration.java
@@ -168,7 +168,7 @@ public class TemplateTestIntegration extends AbstractQueryIntegrationTest {
                 "\n" +
                 "descr\n" +
                 "\n" +
-                "   A short decription related to the object.\n" +
+                "   A short description related to the object.\n" +
                 "\n" +
                 "     A sequence of ASCII characters.\n" +
                 "\n" +
@@ -292,7 +292,7 @@ public class TemplateTestIntegration extends AbstractQueryIntegrationTest {
                 "     letter or a digit.  The following words are reserved by\n" +
                 "     RPSL, and they can not be used as names:\n" +
                 "     \n" +
-                "      any as-any rs-any peeras and or not atomic from to at\n" +
+                "      any as-any rs-any peerAS and or not atomic from to at\n" +
                 "      action accept announce except refine networks into inbound\n" +
                 "      outbound\n" +
                 "     \n" +
@@ -320,7 +320,7 @@ public class TemplateTestIntegration extends AbstractQueryIntegrationTest {
                 "     letter or a digit.  The following words are reserved by\n" +
                 "     RPSL, and they can not be used as names:\n" +
                 "     \n" +
-                "      any as-any rs-any peeras and or not atomic from to at\n" +
+                "      any as-any rs-any peerAS and or not atomic from to at\n" +
                 "      action accept announce except refine networks into inbound\n" +
                 "      outbound\n" +
                 "     \n" +
@@ -346,7 +346,7 @@ public class TemplateTestIntegration extends AbstractQueryIntegrationTest {
                 "     letter or a digit.  The following words are reserved by\n" +
                 "     RPSL, and they can not be used as names:\n" +
                 "     \n" +
-                "      any as-any rs-any peeras and or not atomic from to at\n" +
+                "      any as-any rs-any peerAS and or not atomic from to at\n" +
                 "      action accept announce except refine networks into inbound\n" +
                 "      outbound\n" +
                 "     \n" +
@@ -464,7 +464,7 @@ public class TemplateTestIntegration extends AbstractQueryIntegrationTest {
                 "     letter or a digit.  The following words are reserved by\n" +
                 "     RPSL, and they can not be used as names:\n" +
                 "     \n" +
-                "      any as-any rs-any peeras and or not atomic from to at\n" +
+                "      any as-any rs-any peerAS and or not atomic from to at\n" +
                 "      action accept announce except refine networks into inbound\n" +
                 "      outbound\n" +
                 "     \n" +
@@ -479,7 +479,7 @@ public class TemplateTestIntegration extends AbstractQueryIntegrationTest {
                 "\n" +
                 "descr\n" +
                 "\n" +
-                "   A short decription related to the object.\n" +
+                "   A short description related to the object.\n" +
                 "\n" +
                 "     A sequence of ASCII characters.\n" +
                 "\n" +
@@ -685,7 +685,7 @@ public class TemplateTestIntegration extends AbstractQueryIntegrationTest {
                 "     letter or a digit.  The following words are reserved by\n" +
                 "     RPSL, and they can not be used as names:\n" +
                 "     \n" +
-                "      any as-any rs-any peeras and or not atomic from to at\n" +
+                "      any as-any rs-any peerAS and or not atomic from to at\n" +
                 "      action accept announce except refine networks into inbound\n" +
                 "      outbound\n" +
                 "     \n" +
