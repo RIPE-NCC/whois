88ce47c662be5d95e0cb461bdd1ff6ea545c2755
mherran <mherran@ripe.net>
2023-01-09 11:00:41 +0100

    feat: remove unused file and rename nrtm for nrtm4

--- whois-nrtm4/src/main/java/net/ripe/db/nrtm4/Nrtm4Log.java
+++ whois-nrtm4/src/main/java/net/ripe/db/nrtm4/Nrtm4Log.java
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Component;
 import java.net.InetAddress;
 
 @Component
-public class NrtmLog {
+public class Nrtm4Log {
 
     private final Logger logger;
 
-    public NrtmLog() {
-        this(LoggerFactory.getLogger(NrtmLog.class));
+    public Nrtm4Log() {
+        this(LoggerFactory.getLogger(Nrtm4Log.class));
     }
 
-    public NrtmLog(Logger logger) {
+    public Nrtm4Log(Logger logger) {
         this.logger = logger;
     }
 
