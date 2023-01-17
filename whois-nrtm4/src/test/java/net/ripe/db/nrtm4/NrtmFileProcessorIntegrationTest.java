818df5ae34f449704ab5e7eccfa8e107be2e69c7
Paul Etchells <petchells@ripe.net>
2023-01-17 14:13:00 +0100

    integration test for JMX enable/disable

--- whois-nrtm4/src/test/java/net/ripe/db/nrtm4/NrtmFileProcessorIntegrationTest.java
+++ whois-nrtm4/src/test/java/net/ripe/db/nrtm4/NrtmFileProcessorIntegrationTest.java
@@ -1,5 +1,8 @@
 package net.ripe.db.nrtm4;
 
+import net.ripe.db.nrtm4.dao.NrtmSourceHolder;
+import net.ripe.db.nrtm4.dao.NrtmVersionInfoRepository;
+import net.ripe.db.nrtm4.jmx.NrtmProcessControlJmx;
 import net.ripe.db.whois.common.dao.jdbc.AbstractDatabaseHelperIntegrationTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -7,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
 import static net.ripe.db.whois.common.dao.jdbc.JdbcRpslObjectOperations.loadScripts;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 
 @Tag("IntegrationTest")
@@ -15,11 +20,28 @@ public class NrtmFileProcessorIntegrationTest extends AbstractDatabaseHelperInte
 
     @Autowired
     private NrtmFileProcessor nrtmFileProcessor;
+    @Autowired
+    private NrtmVersionInfoRepository nrtmVersionInfoRepository;
+    @Autowired
+    private NrtmSourceHolder nrtmSourceHolder;
+    @Autowired
+    private NrtmProcessControlJmx nrtmProcessControlJmx;
 
     @Test
-    void run_nrtm_write_job() {
+    void nrtm_write_job_is_disabled_by_jmx() {
         loadScripts(whoisTemplate, "nrtm_sample_sm.sql");
         nrtmFileProcessor.updateNrtmFilesAndPublishNotification();
+        final var source = nrtmVersionInfoRepository.findLastVersion(nrtmSourceHolder.getSource());
+        assertThat(source.isPresent(), is(false));
+    }
+
+    @Test
+    void nrtm_write_job_is_enabled_by_jmx() {
+        loadScripts(whoisTemplate, "nrtm_sample_sm.sql");
+        nrtmProcessControlJmx.enableInitialSnapshotGeneration();
+        nrtmFileProcessor.updateNrtmFilesAndPublishNotification();
+        final var source = nrtmVersionInfoRepository.findLastVersion(nrtmSourceHolder.getSource());
+        assertThat(source.isPresent(), is(true));
     }
 
 }
