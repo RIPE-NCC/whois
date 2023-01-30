676619afae812dff9c62b2df121217cf4345e500
mherran <mherran@ripe.net>
2023-01-30 11:44:04 +0100

    feat: move logic to sourceGenerate class

--- whois-update/src/main/java/net/ripe/db/whois/update/generator/SourceGenerator.java
+++ whois-update/src/main/java/net/ripe/db/whois/update/generator/SourceGenerator.java
@@ -1,12 +1,18 @@
 package net.ripe.db.whois.update.generator;
 
 import com.google.common.collect.ImmutableSet;
+import net.ripe.db.whois.common.dao.RpslObjectDao;
 import net.ripe.db.whois.common.domain.CIString;
 import net.ripe.db.whois.common.grs.AuthoritativeResourceData;
+import net.ripe.db.whois.common.rpsl.AttributeType;
+import net.ripe.db.whois.common.rpsl.ObjectType;
+import net.ripe.db.whois.common.rpsl.RpslAttribute;
 import net.ripe.db.whois.common.rpsl.RpslObject;
+import net.ripe.db.whois.common.rpsl.RpslObjectBuilder;
 import net.ripe.db.whois.update.domain.Operation;
 import net.ripe.db.whois.update.domain.Update;
 import net.ripe.db.whois.update.domain.UpdateContext;
+import net.ripe.db.whois.update.domain.UpdateMessages;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -21,30 +27,60 @@ public class SourceGenerator extends AttributeGenerator {
 
     private final AuthoritativeResourceData authoritativeResourceData;
 
+    private final RpslObjectDao rpslObjectDao;
     private final CIString source;
     private final CIString nonAuthSource;
 
     @Autowired
     public SourceGenerator(final AuthoritativeResourceData authoritativeResourceData,
                            @Value("${whois.source}") final String source,
-                           @Value("${whois.nonauth.source}") final String nonAuthSource) {
+                           @Value("${whois.nonauth.source}") final String nonAuthSource,
+                           final RpslObjectDao rpslObjectDao) {
         this.authoritativeResourceData = authoritativeResourceData;
         this.source = ciString(source);
         this.nonAuthSource = ciString(nonAuthSource);
+        this.rpslObjectDao = rpslObjectDao;
     }
 
     @Override
     public RpslObject generateAttributes(RpslObject originalObject, RpslObject updatedObject, Update update, UpdateContext updateContext) {
-        switch (updatedObject.getType()) {
-            case AUT_NUM:
-            case ROUTE:
-            case ROUTE6:
-                return generateSource(updatedObject, update, updateContext);
-            default:
-                return updatedObject;
-        }
+        return switch (updatedObject.getType()) {
+            case AS_SET -> generateAsSetSource(updatedObject, update, updateContext);
+            case AUT_NUM, ROUTE, ROUTE6 -> generateSource(updatedObject, update, updateContext);
+            default -> updatedObject;
+        };
     }
 
+    private RpslObject generateAsSetSource(final RpslObject updatedObject, final Update update, final UpdateContext updateContext) {
+        if (update.getOperation() == Operation.DELETE) {
+            return updatedObject;
+        }
+        final String asSetKey = updatedObject.getKey().toString();
+
+        final boolean flatAsSet = !asSetKey.contains(":");
+        final CIString asSetSource = updatedObject.getValueForAttribute(AttributeType.SOURCE);
+
+        if (flatAsSet) {
+            return updatedObject;
+        }
+
+        final String autnumKey = asSetKey.substring(0, asSetKey.indexOf(":"));
+        final RpslObject autnumObject = rpslObjectDao.getByKeyOrNull(ObjectType.AUT_NUM, autnumKey);
+
+        if (autnumObject == null){
+            return updatedObject;
+        }
+
+        final CIString autnumSource = autnumObject.getValueForAttribute(AttributeType.SOURCE);
+        if (asSetSource.equals(autnumSource)){
+            return updatedObject;
+        }
+
+        updateContext.addMessage(update, UpdateMessages.sourceChanged(asSetSource, autnumSource, autnumKey));
+
+        return new RpslObjectBuilder(updatedObject).replaceAttribute(updatedObject.findAttribute(AttributeType.SOURCE),
+                new RpslAttribute(AttributeType.SOURCE, autnumSource)).get();
+    }
     private RpslObject generateSource(final RpslObject updatedObject, final Update update, final UpdateContext updateContext) {
         if (update.getOperation() == Operation.DELETE) {
             return updatedObject;
