e53c98ef8d656bba90750ad195541b0c45e5e07a
mherran <mherran@ripe.net>
2023-02-10 14:19:10 +0100

    feat: correct version

--- whois-commons/src/main/resources/patch/whois-1.106.sql
+++ whois-commons/src/main/resources/patch/whois-1.106.sql
@@ -4,4 +4,4 @@ DROP TABLE IF EXISTS `default_maintainer_history`;
 DROP TABLE IF EXISTS `default_maintainer_sync_history`;
 
 TRUNCATE version;
-INSERT INTO version VALUES ('whois-1.104');
+INSERT INTO version VALUES ('whois-1.106');
