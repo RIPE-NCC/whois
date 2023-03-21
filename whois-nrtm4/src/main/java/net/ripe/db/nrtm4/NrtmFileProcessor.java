21ef5b890165d909ad0c27fe55d8a829cb7f4d62
petchells <petchells@ripe.net>
2023-03-21 15:50:57 +0100

    put stopwatch time in log

--- whois-nrtm4/src/main/java/net/ripe/db/nrtm4/NrtmFileProcessor.java
+++ whois-nrtm4/src/main/java/net/ripe/db/nrtm4/NrtmFileProcessor.java
@@ -1,5 +1,6 @@
 package net.ripe.db.nrtm4;
 
+import com.google.common.base.Stopwatch;
 import net.ripe.db.nrtm4.dao.SourceRepository;
 import net.ripe.db.nrtm4.dao.WhoisObjectRepository;
 import net.ripe.db.nrtm4.domain.NrtmSourceModel;
@@ -39,13 +40,15 @@ public class NrtmFileProcessor {
 
     public void updateNrtmFilesAndPublishNotification() {
         LOGGER.info("updateNrtmFilesAndPublishNotification() called");
+        final Stopwatch stopwatch = Stopwatch.createStarted();
         final SnapshotState state = whoisObjectRepository.getSnapshotState();
+        LOGGER.info("Found {} objects in {}", state.whoisObjectData().size(), stopwatch);
         final List<NrtmSourceModel> sourceList = sourceRepository.getSources();
         if (sourceList.isEmpty()) {
             LOGGER.info("Initializing...");
             final List<NrtmVersionInfo> versions = snapshotFileGenerator.createInitialSnapshots(state);
             versions.forEach(notificationFileGenerator::createInitialNotification);
-            LOGGER.info("Initialization complete");
+            LOGGER.info("Initialization complete. Total time: {}", stopwatch);
         } else {
             // Must do deltas first since snapshot creation is skipped if there aren't any
             deltaFileGenerator.createDeltas(state.serialId());
