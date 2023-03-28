6072a566bf6393c7a4b883f3a00579e679279114
petchells <petchells@ripe.net>
2023-03-28 17:32:54 +0200

    typo in sql

--- whois-commons/src/main/resources/nrtm_schema.sql
+++ whois-commons/src/main/resources/nrtm_schema.sql
@@ -41,7 +41,7 @@ CREATE TABLE `version_info`
     `type`           varchar(128)    NOT NULL,
     `last_serial_id` int             NOT NULL,
     `created`        bigint unsigned NOT NULL,
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 --    UNIQUE KEY `version_info__session__source__version__type__uk` (`session_id`, `source_id`, `version`, `type`),
 --    UNIQUE KEY `version_info__type__source__last_serial_id__uk` (`type`, `source_id`, `last_serial_id`)
 ) ENGINE = InnoDB
