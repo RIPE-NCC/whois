e2095774020e42c463fc7a776e048049891cc587
mherran <mherran@ripe.net>
2023-08-21 12:45:12 +0200

    feat: remove unused imports

--- whois-api/src/test/java/net/ripe/db/whois/api/elasticsearch/AbstractElasticSearchIntegrationTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/elasticsearch/AbstractElasticSearchIntegrationTest.java
@@ -18,7 +18,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 
