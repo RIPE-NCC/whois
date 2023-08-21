deb136acaa6f9337a118de1c028f9d736b88ee38
Mahesh Aggarwal <maggarwal@ripe.net>
2023-08-21 13:47:46 +0200

    fix test

--- whois-api/src/test/java/net/ripe/db/whois/api/fulltextsearch/ElasticFullTextSearchTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/fulltextsearch/ElasticFullTextSearchTestIntegration.java
@@ -1030,8 +1030,8 @@ public class ElasticFullTextSearchTestIntegration extends AbstractElasticSearchI
         assertThat(searchResponse.getLsts().get(1).getLsts().get(0).getArrs().get(0).getName(), is("mntner"));
         assertThat(searchResponse.getLsts().get(1).getLsts().get(0).getArrs().get(0).getStr().getValue(), is("<b>DEV</b>-MNT"));
 
-        assertThat(searchResponse.getLsts().get(1).getLsts().get(0).getArrs().get(0).getName(), is("remarks"));
-        assertThat(searchResponse.getLsts().get(1).getLsts().get(0).getArrs().get(0).getStr().getValue(), is("<b>DEV</b> mntner"));
+        assertThat(searchResponse.getLsts().get(1).getLsts().get(0).getArrs().get(1).getName(), is("remarks"));
+        assertThat(searchResponse.getLsts().get(1).getLsts().get(0).getArrs().get(1).getStr().getValue(), is("<b>DEV</b> mntner"));
         assertThat(searchResponse.getLsts().get(2).getName(), is("version"));
     }
 
