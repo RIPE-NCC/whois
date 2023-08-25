568c09e8140eb35a44b93b1c0ba2c3325c8c07ec
mherran <mherran@ripe.net>
2023-08-25 14:07:13 +0200

    feat: fix tests

--- whois-scheduler/src/main/java/net/ripe/db/whois/scheduler/task/export/DecorationStrategy.java
+++ whois-scheduler/src/main/java/net/ripe/db/whois/scheduler/task/export/DecorationStrategy.java
@@ -34,9 +34,7 @@ public interface DecorationStrategy {
 
             final ObjectType objectType = object.getType();
             if (writtenPlaceHolders.add(objectType)) {
-                if (objectType.equals(ObjectType.PERSON)) {
-                    return DummifierNrtm.getPlaceholderPersonObject();
-                }
+                return DummifierNrtm.getPlaceholderPersonObject();
             }
 
             return null;
