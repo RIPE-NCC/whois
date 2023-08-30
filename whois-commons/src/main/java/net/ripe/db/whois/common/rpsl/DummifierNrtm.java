88aaa8f9f51527832be842b342a0e24d80b1aa3a
Miguel Herrán <106821523+MiguelAHM@users.noreply.github.com>
2023-08-30 12:25:40 +0200

    Remove role dummified object (#1300)
    * feat: remove role dummified object
    
    * feat: fix tests
    
    * feat: remove placeholder role from some tests

--- whois-commons/src/main/java/net/ripe/db/whois/common/rpsl/DummifierNrtm.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/rpsl/DummifierNrtm.java
@@ -51,7 +51,7 @@ public class DummifierNrtm implements Dummifier {
 
         // [EB]: Shortcircuit for objects we'd normally skip for old protocols.
         if (version <= 2 && usePlaceHolder(rpslObject)) {
-            return objectType.equals(ObjectType.ROLE) ? getPlaceholderRoleObject() : getPlaceholderPersonObject();
+            return getPlaceholderPersonObject();
         }
 
         final List<RpslAttribute> attributes = Lists.newArrayList(rpslObject.getAttributes());
@@ -223,31 +223,6 @@ public class DummifierNrtm implements Dummifier {
                         "address:        The Netherlands\n" +
                         "phone:          +31 20 535 4444\n" +
                         "nic-hdl:        DUMY-RIPE\n" +
-                        "mnt-by:         RIPE-DBM-MNT\n" +
-                        "remarks:        **********************************************************\n" +
-                        "remarks:        * This is a placeholder object to protect personal data.\n" +
-                        "remarks:        * To view the original object, please query the RIPE\n" +
-                        "remarks:        * Database at:\n" +
-                        "remarks:        * http://www.ripe.net/whois\n" +
-                        "remarks:        **********************************************************\n" +
-                        "created:        2009-07-24T17:00:00Z\n" +
-                        "last-modified:  2009-07-24T17:00:00Z\n" +
-                        "source:         RIPE"
-        );
-    }
-
-    public static RpslObject getPlaceholderRoleObject() {
-        return RpslObject.parse("" +
-                        "role:           Placeholder Role Object\n" +
-                        "address:        RIPE Network Coordination Centre\n" +
-                        "address:        P.O. Box 10096\n" +
-                        "address:        1001 EB Amsterdam\n" +
-                        "address:        The Netherlands\n" +
-                        "phone:          +31 20 535 4444\n" +
-                        "e-mail:         ripe-dbm@ripe.net\n" +
-                        "admin-c:        DUMY-RIPE\n" +
-                        "tech-c:         DUMY-RIPE\n" +
-                        "nic-hdl:        ROLE-RIPE\n" +
                         "mnt-by:         RIPE-DBM-MNT\n" +
                         "remarks:        **********************************************************\n" +
                         "remarks:        * This is a placeholder object to protect personal data.\n" +
