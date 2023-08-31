2d482d900c7bc761c4c3e5581734d4157a09fda6
Ed Shryane <eshryane@ripe.net>
2023-08-31 10:58:17 +0200

    Switch from ehcache 2.x to 3.x

--- whois-commons/src/main/java/net/ripe/db/whois/common/CacheManagerJmx.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/CacheManagerJmx.java
@@ -1,13 +1,13 @@
 package net.ripe.db.whois.common;
 
 import net.ripe.db.whois.common.jmx.JmxBase;
-import net.sf.ehcache.CacheManager;
+import net.ripe.db.whois.common.sso.domain.ValidateTokenResponse;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jmx.export.annotation.ManagedOperation;
-import org.springframework.jmx.export.annotation.ManagedOperationParameter;
-import org.springframework.jmx.export.annotation.ManagedOperationParameters;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
@@ -25,48 +25,20 @@ public class CacheManagerJmx extends JmxBase {
         this.cacheManager = cacheManager;
     }
 
-    @ManagedOperation(description = "Get cache status")
-    @ManagedOperationParameters({
-        @ManagedOperationParameter(name = "name", description = "cache name"),
-    })
-    public String status(final String name) {
-        backgroundOperation("get cache status", name, () -> {
-
-            if (!cacheManager.cacheExists(name)) {
-                LOGGER.error("Cache {} doesn't exist?", name);
-                return null;
-            }
-
-            LOGGER.info(
-                "Cache {} is {}: Keys {} Size {} Evicted {} Expired {} Hits {} Hit Ratio {} Misses {} Puts {} Size {}",
-                    name,
-                    cacheManager.getCache(name).getStatus(),
-                    cacheManager.getCache(name).getKeys().size(),
-                    cacheManager.getCache(name).getSize(),
-                    cacheManager.getCache(name).getStatistics().cacheEvictedCount(),
-                    cacheManager.getCache(name).getStatistics().cacheExpiredCount(),
-                    cacheManager.getCache(name).getStatistics().cacheHitCount(),
-                    cacheManager.getCache(name).getStatistics().cacheHitRatio(),
-                    cacheManager.getCache(name).getStatistics().cacheMissCount(),
-                    cacheManager.getCache(name).getStatistics().cachePutCount(),
-                    cacheManager.getCache(name).getStatistics().getSize());
-
-            return null;
-        });
-        return "Get cache status " + name;
+    @ManagedOperation(description = "Get status of SSO Validate Token cache")
+    public void ssoValidateTokenStatus() {
+        final Cache<String, ValidateTokenResponse> cache = cacheManager.getCache("ssoValidateToken", String.class, ValidateTokenResponse.class);
+        if (cache == null) {
+            LOGGER.info("cache not found");
+        } else {
+            LOGGER.info("cache: {}", cache.getClass().getName());
+        }
     }
 
-    @ManagedOperation(description = "Clear contents of cache")
-    @ManagedOperationParameters({
-        @ManagedOperationParameter(name = "prefix", description = "clear contents of cache(s) starting with prefix"),
-    })
-    public String clearAll(final String prefix) {
-        backgroundOperation("clear contents of cache(s) starting with prefix", prefix, () -> {
-            cacheManager.clearAllStartingWith(prefix);
-            LOGGER.info("cacheManager.clearAllStartingWith {} completed.", prefix);
-            return null;
-        });
-        return "Clear contents of cache(s) starting with prefix " + prefix;
+    @ManagedOperation(description = "Clear contents of SSO Validate Token cache")
+    public void clearSsoValidateToken() {
+        final Cache<String, ValidateTokenResponse> cache = cacheManager.getCache("ssoValidateToken", String.class, ValidateTokenResponse.class);
+        cache.clear();
     }
 
 
