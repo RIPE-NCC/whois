d8361dc28c0cbb682668f0b4a4c428543528f36c
Ed Shryane <eshryane@ripe.net>
2023-08-31 23:13:51 +0200

    Allow Jackson to use JAXB @XmlElement annotations when deserialising JSON (in addition to @JsonProperty).

--- whois-api/src/test/java/net/ripe/db/whois/api/RestTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/RestTest.java
@@ -1,7 +1,13 @@
 package net.ripe.db.whois.api;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.jakarta.rs.json.JacksonJsonProvider;
+import com.fasterxml.jackson.module.jakarta.xmlbind.JakartaXmlBindAnnotationIntrospector;
 import jakarta.ws.rs.ClientErrorException;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
@@ -20,9 +26,16 @@ public class RestTest {
     private static final Client client;
 
     static {
+        final ObjectMapper objectMapper = JsonMapper.builder().build();
+        objectMapper.setAnnotationIntrospector(
+                new AnnotationIntrospectorPair(
+                        new JacksonAnnotationIntrospector(),
+                        new JakartaXmlBindAnnotationIntrospector(TypeFactory.defaultInstance())));
+
         final JacksonJsonProvider jsonProvider = new JacksonJsonProvider()
                 .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, false)
                 .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+        jsonProvider.setMapper(objectMapper);
 
         client = ClientBuilder.newBuilder()
                 .register(MultiPartFeature.class)
