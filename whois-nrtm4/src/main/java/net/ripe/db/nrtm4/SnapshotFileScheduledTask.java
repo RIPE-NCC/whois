3dd16bae413a0e897529ec08582dcd03a85847cc
Mahesh Aggarwal <maggarwal@ripe.net>
2023-09-05 15:09:56 +0200

    change sechduler

--- whois-nrtm4/src/main/java/net/ripe/db/nrtm4/SnapshotFileScheduledTask.java
+++ whois-nrtm4/src/main/java/net/ripe/db/nrtm4/SnapshotFileScheduledTask.java
@@ -20,7 +20,8 @@ public class SnapshotFileScheduledTask implements DailyScheduledTask {
     }
 
     @Override
-    @Scheduled(cron = "0 0 0 * * *")
+    //@Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 0/30 * * * ?")
     @SchedulerLock(name = "NrtmSnapshotFileGenerationTask")
     public void run() {
         try {
