6c79d0a5fcb33bef99b1cbcc028851fcf1f443d8
Ed Shryane <eshryane@ripe.net>
2023-09-08 16:06:08 +0200

    CERT is a private static final field.

--- whois-api/src/test/java/net/ripe/db/whois/api/rest/ClientCertificateExtractorTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/rest/ClientCertificateExtractorTest.java
@@ -2,7 +2,7 @@ package net.ripe.db.whois.api.rest;
 
 import net.ripe.db.whois.common.ClockDateTimeProvider;
 import net.ripe.db.whois.common.TestDateTimeProvider;
-import net.ripe.db.whois.update.keycert.X509CertificateTestUtil;
+import net.ripe.db.whois.update.keycert.X509CertificateUtil;
 import org.junit.jupiter.api.Test;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,11 +18,11 @@ import static org.mockito.Mockito.when;
 
 public class ClientCertificateExtractorTest {
 
-    static String cert;
+    private static final String CERT;
 
     static {
         try {
-            cert = X509CertificateTestUtil.asPem(X509CertificateTestUtil.generate("test-cn", new ClockDateTimeProvider()));
+            CERT = X509CertificateUtil.getCertificateAsString(X509CertificateUtil.generateCertificate(new ClockDateTimeProvider()));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -31,7 +31,7 @@ public class ClientCertificateExtractorTest {
     @Test
     public void testValidCertificate() {
         final HttpServletRequest request = mock(HttpServletRequest.class);
-        when(request.getHeader(HEADER_SSL_CLIENT_CERT)).thenReturn(cert);
+        when(request.getHeader(HEADER_SSL_CLIENT_CERT)).thenReturn(CERT);
         when(request.getHeader(HEADER_SSL_CLIENT_VERIFY)).thenReturn("GENEROUS");
 
         assertThat(ClientCertificateExtractor.getClientCertificate(request, new ClockDateTimeProvider()).isPresent(), is(true));
@@ -40,7 +40,7 @@ public class ClientCertificateExtractorTest {
     @Test
     public void testInvalidVerifyHeader() {
         final HttpServletRequest request = mock(HttpServletRequest.class);
-        when(request.getHeader(HEADER_SSL_CLIENT_CERT)).thenReturn(cert);
+        when(request.getHeader(HEADER_SSL_CLIENT_CERT)).thenReturn(CERT);
         when(request.getHeader(HEADER_SSL_CLIENT_VERIFY)).thenReturn("NOT-ACCEPTED");
 
         assertThat(ClientCertificateExtractor.getClientCertificate(request, new ClockDateTimeProvider()).isPresent(), is(false));
@@ -57,7 +57,7 @@ public class ClientCertificateExtractorTest {
     @Test
     public void testCertificateExpired() {
         final HttpServletRequest request = mock(HttpServletRequest.class);
-        when(request.getHeader(HEADER_SSL_CLIENT_CERT)).thenReturn(cert);
+        when(request.getHeader(HEADER_SSL_CLIENT_CERT)).thenReturn(CERT);
         when(request.getHeader(HEADER_SSL_CLIENT_VERIFY)).thenReturn("GENEROUS");
 
         final TestDateTimeProvider testDateTimeProvider = new TestDateTimeProvider();
@@ -69,7 +69,7 @@ public class ClientCertificateExtractorTest {
     @Test
     public void testCertificateNotYetValid() {
         final HttpServletRequest request = mock(HttpServletRequest.class);
-        when(request.getHeader(HEADER_SSL_CLIENT_CERT)).thenReturn(cert);
+        when(request.getHeader(HEADER_SSL_CLIENT_CERT)).thenReturn(CERT);
         when(request.getHeader(HEADER_SSL_CLIENT_VERIFY)).thenReturn("GENEROUS");
 
         final TestDateTimeProvider testDateTimeProvider = new TestDateTimeProvider();
