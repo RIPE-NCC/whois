b5c92f28040dcd927e77e0063af835d247fb3386
mherran <mherran@ripe.net>
2023-09-21 15:36:38 +0200

    Revert "feat: add an error to produce the rollback"
    This reverts commit 74d3a93d0d085cdc56e3e35ddc5934f51ed646fd.

--- whois-nrtm4/src/main/java/net/ripe/db/nrtm4/dao/UpdateNrtmFileRepository.java
+++ whois-nrtm4/src/main/java/net/ripe/db/nrtm4/dao/UpdateNrtmFileRepository.java
@@ -123,7 +123,6 @@ public class UpdateNrtmFileRepository {
             VALUES (?, ?, ?, ?)
             """;
         jdbcTemplate.update(sql, versionId, name, hash, payload);
-        throw new IllegalStateException("Test NRTM");
     }
 
     public void saveSnapshot(final SnapshotFile snapshotFile, final byte[] payload) {
@@ -161,7 +160,7 @@ public class UpdateNrtmFileRepository {
         }
         deleteVersionInfos(versionIds);
     }
-
+    
     public void cleanupNrtmv4Database() {
         LOGGER.warn("Cleaning up NRTMv4 Database");
 
