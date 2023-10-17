c75259a539ff1546521ecfac0c706fb62db9a1b2
Mahesh Aggarwal <maggarwal@ripe.net>
2023-10-17 14:13:37 +0200

    Refactor redactinMapper

--- whois-api/src/main/java/net/ripe/db/whois/api/rdap/RedactionObjectMapper.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rdap/RedactionObjectMapper.java
@@ -1,40 +1,36 @@
 package net.ripe.db.whois.api.rdap;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import net.ripe.db.whois.api.rdap.domain.Redaction;
 import net.ripe.db.whois.common.rpsl.AttributeType;
+import net.ripe.db.whois.common.rpsl.RpslAttribute;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RedactionObjectMapper {
 
     public static String REDACTED_ENTITIES_SYNTAX = "$.entities[?(@.handle=='%s')]";
+    public static List<AttributeType> REDACTED_PERSONAL_ATTR = Lists.newArrayList(AttributeType.NOTIFY);
 
-    public static Set<Redaction> createMainEntityRedactions(final RpslObject rpslObject){
-        final Set<Redaction> redactions = Sets.newHashSet();
-
-        rpslObject.getAttributes().forEach( rpslAttribute -> {
-            addRedactionForVcard(redactions, rpslAttribute.getType(), "$");
-        });
-
-        return redactions;
+    public static Set<Redaction> createEntityRedactions(final RpslObject rpslObject){
+        return createPersonalRedaction(rpslObject.findAttributes(REDACTED_PERSONAL_ATTR), "$");
     }
 
     public static Set<Redaction> createContactEntityRedaction(final RpslObject rpslObject) {
-        final Set<Redaction> redactions = Sets.newHashSet();
-        rpslObject.getAttributes().forEach( rpslAttribute ->
-            addRedactionForVcard(redactions, rpslAttribute.getType(), String.format(REDACTED_ENTITIES_SYNTAX, rpslObject.getKey()))
-        );
-        return  redactions;
+        return createPersonalRedaction(rpslObject.findAttributes(REDACTED_PERSONAL_ATTR), String.format(REDACTED_ENTITIES_SYNTAX, rpslObject.getKey()));
     }
 
-    private static void addRedactionForVcard(final Set<Redaction> redactions, final AttributeType attributeType, final String prefix){
-        if (attributeType == AttributeType.NOTIFY) {
-            redactions.add(new Redaction("Updates notification e-mail information",
-                    String.format("%s.vcardArray[1][?(@[0]=='%s')]", prefix, attributeType.getName()),
-                    "Personal data")
-            );
-        }
+    private static Set<Redaction> createPersonalRedaction(final List<RpslAttribute> attributeTypes, final String prefix){
+       return attributeTypes.stream()
+               .map( rpslAttribute -> new Redaction("Updates notification e-mail information",
+                                                String.format("%s.vcardArray[1][?(@[0]=='%s')]", prefix, rpslAttribute.getType().getName()),
+                                                "Personal data")
+                )
+               .collect(Collectors.toSet());
+
     }
 }
