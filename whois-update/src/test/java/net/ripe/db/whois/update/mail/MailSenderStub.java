4bc689d64c0ea364781f9d9a4c4e42c2de965ee9
mherran <mherran@ripe.net>
2024-02-23 12:40:05 +0100

    feat: use javasender impl instead a custom one

--- whois-update/src/test/java/net/ripe/db/whois/update/mail/MailSenderStub.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/mail/MailSenderStub.java
@@ -13,6 +13,7 @@ import org.awaitility.Awaitility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +27,7 @@ import java.util.concurrent.TimeUnit;
 
 @Profile({WhoisProfile.TEST})
 @Component
-public class MailSenderStub extends MailSenderBase implements Stub {
+public class MailSenderStub extends JavaMailSenderImpl implements Stub {
     private static final Logger LOGGER = LoggerFactory.getLogger(MailSenderStub.class);
 
     private static final Session SESSION = Session.getInstance(new Properties());
