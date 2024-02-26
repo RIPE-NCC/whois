ffaa669c413ebddeadab76ce338a8a2f7c6cbecb
Ed Shryane <eshryane@ripe.net>
2024-02-26 14:36:30 +0100

    Tests

--- whois-api/src/test/java/net/ripe/db/whois/api/mail/bounce/MailBounceTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/mail/bounce/MailBounceTestIntegration.java
@@ -1,25 +1,29 @@
 package net.ripe.db.whois.api.mail.bounce;
 
-import com.google.common.base.Joiner;
-import com.google.common.util.concurrent.Uninterruptibles;
-import jakarta.mail.Address;
-import net.ripe.db.whois.common.dao.jdbc.DatabaseHelper;
-import net.ripe.db.whois.update.dao.AbstractUpdateDaoIntegrationTest;
+import jakarta.mail.internet.MimeMessage;
+import net.ripe.db.whois.api.AbstractIntegrationTest;
+import net.ripe.db.whois.api.MailUpdatesTestSupport;
 import net.ripe.db.whois.update.mail.MailSenderStub;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 
 @Tag("IntegrationTest")
-public class MailBounceTestIntegration extends AbstractUpdateDaoIntegrationTest {
+public class MailBounceTestIntegration extends AbstractIntegrationTest {
 
 
     @Autowired
-    private MailSenderStub mailSender;
+    private MailSenderStub mailSenderStub;
+    @Autowired
+    private MailUpdatesTestSupport mailUpdatesTestSupport;
 
 
     @BeforeEach
@@ -33,7 +37,6 @@ public class MailBounceTestIntegration extends AbstractUpdateDaoIntegrationTest 
                 "descr:         Owner Maintainer\n" +
                 "admin-c:       TP1-TEST\n" +
                 "upd-to:        upd-to@ripe.net\n" +
-                "mnt-nfy:       mnt-nfy@ripe.net\n" +
                 "notify:        notify@ripe.net\n" +
                 "auth:          MD5-PW $1$d9fKeTr2$Si7YudNf4rUGmR71n/cqk/ #test\n" +
                 "mnt-by:        OWNER-MNT\n" +
@@ -51,85 +54,73 @@ public class MailBounceTestIntegration extends AbstractUpdateDaoIntegrationTest 
     // TODO: generate bounce reply to mailupdate. That address should be marked as undeliverable
 
     @Test
-    public void bouncing_mail_causes_address_to_be_marked_as_undeliverable() {
-        // send mail update to Whois
-
-        // generate bounce reply to Whois for one of the mail addresses
-
-        // check that address gets marked as undeliverable
-    }
-
-
-    // TODO: mark address as undeliverable. Send mailupdate doesn't generate outgoing mail to that address
-
-    @Test
-    public void dont_send_mail_to_undeliverable_address() {
-        // mark address as undeliverable
-        insertUndeliverableAddress("nonexistant@ripe.net");
-
-        insertMailUpdate(
-            createPlainTextMailMessage(
+    public void bouncing_mail_causes_address_to_be_marked_as_undeliverable() throws Exception {
+        final String role =
                 "role:        dummy role\n" +
                 "address:       Singel 258\n" +
+                "e-mail:        dummyrole@ripe.net\n" +
                 "phone:         +31 6 12345678\n" +
+                "notify:        notify-dummy-role@ripe.net\n" +
                 "nic-hdl:       DR1-TEST\n" +
-                "mnt-by:        DR1-MNT\n" +
-                "source:        TEST"));
+                "mnt-by:        OWNER-MNT\n" +
+                "source:        TEST\n";
 
-        final Joiner commaJoiner = Joiner.on(",");
+        // send message and read reply
+        final String from = mailUpdatesTestSupport.insert("NEW", role + "\npassword: test\n");
+        final MimeMessage acknowledgement = mailSenderStub.getMessage(from);
+        assertThat(acknowledgement.getContent().toString(), containsString("Create SUCCEEDED: [role] DR1-TEST   dummy role"));
 
-        for (int i=0; i<10; i++) {
-            if (mailSender.anyMoreMessages()) {
-                final List<Address> allRecipients = mailSender.getAllRecipients();
-                System.out.println("*** FOUND " + commaJoiner.join(allRecipients) + " ***");
-            }
-            Uninterruptibles.sleepUninterruptibly(1L, TimeUnit.SECONDS);
-        }
+        // Mail to notify-dummy-role@ripe.net failed
+        final MimeMessage notify = mailSenderStub.getMessage("notify-dummy-role@ripe.net");
+        final String notifyMessageId = notify.getHeader("Message-ID", "\n");
+        assertThat(notifyMessageId, Matchers.is(not(nullValue())));
 
-        //
+        // TODO: generate bounce reply to Whois for one of the mail addresses
 
-        // make sure no mail is sent to undeliverabe address
+        // check that address gets marked as undeliverable
+        assertThat(isUndeliverableAddress("notify-dummy-role@ripe.net"), is(false));
+    }
 
-        DatabaseHelper.dumpSchema(whoisTemplate.getDataSource());
+    @Test
+    public void dont_send_mail_to_undeliverable_address() throws Exception {
+        final String role =
+                "role:        dummy role\n" +
+                "address:       Singel 258\n" +
+                "e-mail:        dummyrole@ripe.net\n" +
+                "phone:         +31 6 12345678\n" +
+                "notify:        nonexistant@ripe.net\n" +
+                "nic-hdl:       DR1-TEST\n" +
+                "mnt-by:        OWNER-MNT\n" +
+                "source:        TEST\n";
+
+        // mark address as undeliverable
+        setUndeliverableAddress("nonexistant@ripe.net");
+
+        // send message and read reply
+        final String from = mailUpdatesTestSupport.insert("NEW", role + "\npassword: test\n");
+        final MimeMessage acknowledgement = mailSenderStub.getMessage(from);
+        assertThat(acknowledgement.getContent().toString(), containsString("Create SUCCEEDED: [role] DR1-TEST   dummy role"));
+
+        // test that no mail is sent to nonexistant@ripe.net
+        assertThat(mailSenderStub.anyMoreMessages(), is(false));
+    }
+
+    @Test
+    public void delayed_delivery_is_not_permanent_undeliverable() {
+
+        // TODO: make sure that a delayed delivery message doesn't cause the address to be marked as undeliverable
+
     }
 
     // helper methods
 
-    private String createPlainTextMailMessage(final String rpslString) {
-        return String.format(
-                "From root@test.de Mon May 28 00:04:45 2012\n" +
-                "Return-path: <root@test.de>\n" +
-                "Envelope-to: dbase@whois-update.db.ripe.net\n" +
-                "Delivery-date: Mon, 28 May 2012 00:04:45 +0200\n" +
-                "Date: Mon, 28 May 2012 00:04:44 +0200\n" +
-                "From: \"foo@test.de\" <bitbucket@ripe.net>\n" +
-                "To: auto-dbm@XXXripe.net\n" +
-                "Message-ID: <20120527220444.GA6565@XXXsource.test.de>\n" +
-                "MIME-Version: 1.0\n" +
-                "Content-Type: text/plain; charset=us-ascii\n" +
-                "Content-Disposition: inline\n" +
-                "User-Agent: Mutt/1.5.17+20080114 (2008-01-14)\n" +
-                "\n" +
-                "%s" +
-                "\n", rpslString);
-    }
-
-
-    private void insertMailUpdate(final String mailMessage) {
-        databaseHelper.getMailupdatesTemplate().update("INSERT INTO mailupdates (message) VALUES (?)", mailMessage);
-    }
-
-    private void insertMessageId(final String messageId, final String emailAddress) {
-        databaseHelper.getInternalsTemplate().update(
-                "INSERT INTO in_progress_message (message_id, email, last_update) VALUES (?, ?, now())", messageId, emailAddress);
-    }
-
-    private void insertUndeliverableAddress(final String emailAddress) {
+    private void setUndeliverableAddress(final String emailAddress) {
         databaseHelper.getInternalsTemplate().update(
                 "INSERT INTO undeliverable_email (email, last_update) VALUES (?, now())", emailAddress);
     }
 
-
-
+    private boolean isUndeliverableAddress(final String emailAddress) {
+        return Boolean.TRUE.equals(databaseHelper.getInternalsTemplate().queryForObject("SELECT email FROM undeliverable_email WHERE email = ?", (rs, rowNum) -> rs.next(), emailAddress));
+    }
 
 }
