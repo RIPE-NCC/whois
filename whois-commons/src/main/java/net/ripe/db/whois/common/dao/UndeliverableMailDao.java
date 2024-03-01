31d95117ee123ff1817b288f097e8e9a5c00caf9
Ed Shryane <eshryane@ripe.net>
2024-03-01 10:26:28 +0100

    Generate UUID in MariaDB server

--- whois-commons/src/main/java/net/ripe/db/whois/common/dao/UndeliverableMailDao.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/dao/UndeliverableMailDao.java
@@ -5,13 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Nullable;
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 
 @Repository
@@ -29,6 +32,25 @@ public class UndeliverableMailDao {
 
     public void saveOutGoingMessageId(final String uuid, final String email){
         internalsMasterTemplate.update("INSERT INTO outgoing_message (message_id, email, last_update) VALUES (?, ?, ?)", uuid, email, LocalDateTime.now());
+    }
+
+    @Nullable
+    public String saveOutGoingMessageId(final String email) {
+        return internalsTemplate.execute(
+            connection -> {
+                final PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO outgoing_message (message_id, email) VALUES (uuid(), ?) RETURNING message_id", Statement.RETURN_GENERATED_KEYS);
+                preparedStatement.setString(1, email);
+                return preparedStatement;
+            },
+            (PreparedStatementCallback<String>) preparedStatement -> {
+                if (preparedStatement.execute()) {
+                    final ResultSet resultSet = preparedStatement.getResultSet();
+                    if (resultSet != null && resultSet.next()) {
+                          return resultSet.getString("message_id");
+                    }
+                }
+                return null;
+        });
     }
 
     public void createUndeliverableEmail(final String email){
