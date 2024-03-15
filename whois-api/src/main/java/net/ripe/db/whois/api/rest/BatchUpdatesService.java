c8eef4635881c93d584d9d090c4320ce4bb47a88
mherran <mherran@ripe.net>
2024-03-15 09:30:22 +0100

    feat: check required parameter

--- whois-api/src/main/java/net/ripe/db/whois/api/rest/BatchUpdatesService.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/rest/BatchUpdatesService.java
@@ -1,6 +1,18 @@
 package net.ripe.db.whois.api.rest;
 
 import com.google.common.collect.Lists;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.CookieParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.StreamingOutput;
 import net.ripe.db.whois.api.rest.domain.Action;
 import net.ripe.db.whois.api.rest.domain.ActionRequest;
 import net.ripe.db.whois.api.rest.domain.WhoisResources;
@@ -17,21 +29,10 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.CookieParam;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.StreamingOutput;
 import java.util.Collections;
 import java.util.List;
 
+import static jakarta.ws.rs.core.Response.Status.BAD_REQUEST;
 import static net.ripe.db.whois.api.rest.RestServiceHelper.isQueryParamSet;
 
 @Component
@@ -63,6 +64,10 @@ public class BatchUpdatesService {
                        @QueryParam("dry-run") final String dryRun,
                        @QueryParam("delete-reason") final String reason,
                        @CookieParam(AuthServiceClient.TOKEN_KEY)  final String crowdTokenKey) {
+
+        if (whoisResources == null || whoisResources.getWhoisObjects().size() == 0) {
+            return Response.status(BAD_REQUEST).entity("WhoisResources is mandatory").build();
+        }
 
         try {
             final Origin origin = updatePerformer.createOrigin(request);
