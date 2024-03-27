201eba5c8ad5209a3ac36dcfc0de4ca7daee1cc1
Ed Shryane <eshryane@ripe.net>
2024-03-27 13:45:18 +0100

    Corrected NRTM schema (put the drop table just before the create table; removed non-existant snapshot_object table; use 'utf8mb4' and not 'utf8'.

--- whois-commons/src/main/resources/nrtm_schema.sql
+++ whois-commons/src/main/resources/nrtm_schema.sql
@@ -1,7 +1,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
 /*!40103 SET TIME_ZONE = '+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
@@ -9,21 +9,7 @@
 /*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
 
-DROP TABLE IF EXISTS `version`;
-CREATE TABLE `version`
-(
-    `version` varchar(80)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
-DROP TABLE IF EXISTS `notification_file`;
-DROP TABLE IF EXISTS `delta_file`;
-DROP TABLE IF EXISTS `snapshot_file`;
-DROP TABLE IF EXISTS `snapshot_object`;
-DROP TABLE IF EXISTS `version_info`;
 DROP TABLE IF EXISTS `source`;
-DROP TABLE IF EXISTS `key_pair`;
-
 CREATE TABLE `source`
 (
     `id`   int unsigned NOT NULL AUTO_INCREMENT,
@@ -31,8 +17,9 @@ CREATE TABLE `source`
     PRIMARY KEY (`id`),
     UNIQUE KEY `source__name_uk` (`name`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
 
+DROP TABLE IF EXISTS `version_info`;
 CREATE TABLE `version_info`
 (
     `id`             int unsigned    NOT NULL AUTO_INCREMENT,
@@ -45,8 +32,9 @@ CREATE TABLE `version_info`
     PRIMARY KEY (`id`),
     UNIQUE KEY `version_info__session__source__version__type__uk` (`session_id`, `source_id`, `version`, `type`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
 
+DROP TABLE IF EXISTS `snapshot_file`;
 CREATE TABLE `snapshot_file`
 (
     `id`         int unsigned NOT NULL AUTO_INCREMENT,
@@ -59,8 +47,9 @@ CREATE TABLE `snapshot_file`
     UNIQUE KEY `snapshot_file__name__uk` (`name`),
     CONSTRAINT `snapshot_file__version_id__fk` FOREIGN KEY (`version_id`) REFERENCES `version_info` (`id`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
 
+DROP TABLE IF EXISTS `delta_file`;
 CREATE TABLE `delta_file`
 (
     `id`         int unsigned NOT NULL AUTO_INCREMENT,
@@ -73,8 +62,9 @@ CREATE TABLE `delta_file`
     UNIQUE KEY `delta_file__name__uk` (`name`),
     CONSTRAINT `delta_file__version_id__fk` FOREIGN KEY (`version_id`) REFERENCES `version_info` (`id`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
 
+DROP TABLE IF EXISTS `notification_file`;
 CREATE TABLE `notification_file`
 (
     `id`         int unsigned    NOT NULL AUTO_INCREMENT,
@@ -84,9 +74,9 @@ CREATE TABLE `notification_file`
     PRIMARY KEY (`id`),
     CONSTRAINT `notification_file__version_id__fk` FOREIGN KEY (`version_id`) REFERENCES `version_info` (`id`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
 
-
+DROP TABLE IF EXISTS `key_pair`;
 CREATE TABLE `key_pair`
 (
     `id`          int unsigned    NOT NULL AUTO_INCREMENT,
@@ -98,7 +88,14 @@ CREATE TABLE `key_pair`
     UNIQUE KEY `public_key_name_uk` (`public_key`),
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
+
+DROP TABLE IF EXISTS `version`;
+CREATE TABLE `version`
+(
+    `version` varchar(80)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 /*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
 
