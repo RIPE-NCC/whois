bc221044efdbd40e59a921a126219dfdbe749aea
Ed Shryane <eshryane@ripe.net>
2024-03-27 13:46:01 +0100

    Removed redundant character set and collation from Whois auth table VARCHAR column

--- whois-commons/src/main/resources/whois_schema.sql
+++ whois-commons/src/main/resources/whois_schema.sql
@@ -121,7 +121,7 @@ DROP TABLE IF EXISTS `auth`;
 /*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `auth` (
   `object_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `auth` varchar(90) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `auth` varchar(90) NOT NULL DEFAULT '',
   `object_type` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`auth`,`object_id`),
   KEY `object_id` (`object_id`)
