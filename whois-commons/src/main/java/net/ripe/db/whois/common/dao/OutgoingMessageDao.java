b63a970ba0c71dbab2fc13f7794ac77f3ee08a40
mherran <mherran@ripe.net>
2024-04-10 15:47:59 +0200

    feat: remove unused import

--- whois-commons/src/main/java/net/ripe/db/whois/common/dao/OutgoingMessageDao.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/dao/OutgoingMessageDao.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
 import java.time.LocalDateTime;
 import java.util.List;
 
