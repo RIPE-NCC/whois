09ee256bd54985f5113b46f9808aa736a3c22f69
Miguel Herrán <106821523+MiguelAHM@users.noreply.github.com>
2024-04-15 12:49:02 +0200

    Allow Inverse query for E-MAIL from TRUSTED (#1435)
    * feat: allow inverse query by sponsoring-org trusted and non trusted
    
    * feat: fix tests
    
    * feat: remove test case and add another assert for the object
    
    * feat: remove sponsoring-org indexing, we are already indexing it as inverse
    
    * feat: email as inverse query
    
    * feat: add ITs

--- whois-commons/src/main/java/net/ripe/db/whois/common/dao/jdbc/JdbcIndexDao.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/dao/jdbc/JdbcIndexDao.java
@@ -103,6 +103,7 @@ public class JdbcIndexDao implements IndexDao {
         rebuildIndexes(objectIds, Phase.OTHER);
     }
 
+    // TODO: [MH] Transaction doesn't work for private method, review this
     @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRES_NEW)
     private void rebuildIndexes(final Iterable<Integer> objectIds, final Phase phase) {
         updateLockDao.setUpdateLock();
@@ -193,6 +194,7 @@ public class JdbcIndexDao implements IndexDao {
         }
     }
 
+    // TODO: [MH] Transaction doesn't work for private method, review this
     @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRES_NEW)
     private void deleteIndexForMissingObjects(final AttributeType attributeType) {
         updateLockDao.setUpdateLock();
