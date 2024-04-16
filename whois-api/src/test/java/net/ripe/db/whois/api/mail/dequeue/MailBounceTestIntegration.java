c24947d48cdf7a17643137925ab2e9baae0fbd98
Ed Shryane <eshryane@ripe.net>
2024-04-16 21:53:04 +0200

    Added tests for Delivery Status Notifications (failed delivery) (#1447)
    * Add tests for multiple failed recipients that do not match the outgoing To: recipient (e.g. sent to a role account, and 2 individuals in that role don't exist)
    
    * Fixed test: delayed message *is* now deleted from the mailupdates table

--- whois-api/src/test/java/net/ripe/db/whois/api/mail/dequeue/MailBounceTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/mail/dequeue/MailBounceTestIntegration.java
@@ -163,6 +163,22 @@ public class MailBounceTestIntegration extends AbstractMailMessageIntegrationTes
     }
 
     @Test
+    public void permanent_delivery_failure_to_one_recipient_multiple_final_recipients() {
+        insertOutgoingMessageId("XXXXXXXX-5AE3-4C58-8E3F-860327BA955D@ripe.net", "noc@host.org");
+        final MimeMessage message = MimeMessageProvider.getUpdateMessage("permanentFailureMessageRfc822Noc.mail");
+        insertIncomingMessage(message);
+
+        // wait for incoming message to be processed
+        Awaitility.waitAtMost(10L, TimeUnit.SECONDS).until(() -> (! anyIncomingMessages()));
+
+        // delayed message has been processed but address is not set to undeliverable
+        assertThat(isUndeliverableAddress("noc@host.org"), is(false));
+        assertThat(isUndeliverableAddress("First.Person@host.org"), is(false));
+        assertThat(isUndeliverableAddress("Second.Person@host.org"), is(false));
+
+    }
+
+    @Test
     public void delayed_delivery_is_not_permanently_undeliverable() {
         // insert delayed response
         insertOutgoingMessageId("XXXXXXXX-734E-496B-AD3F-84D3425A7F27", "enduser@host.org");
@@ -175,7 +191,8 @@ public class MailBounceTestIntegration extends AbstractMailMessageIntegrationTes
         // delayed message has been processed but address is not set to undeliverable
         assertThat(isUndeliverableAddress("enduser@host.org"), is(false));
 
-        // TODO: [ES] delayed message not deleted from mailupdates table
+        // delayed message deleted from mailupdates table
+        assertThat(anyIncomingMessages(), is(false));
     }
 
     @Test
@@ -243,7 +260,6 @@ public class MailBounceTestIntegration extends AbstractMailMessageIntegrationTes
         // Make sure that failure response message was deleted
         assertThat(mailSenderStub.anyMoreMessages(), is(false));
     }
-
 
     @Test
     public void bouncing_headers_causes_address_to_be_marked_as_undeliverable() {
