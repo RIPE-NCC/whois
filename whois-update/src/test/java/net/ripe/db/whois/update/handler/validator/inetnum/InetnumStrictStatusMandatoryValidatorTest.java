5ebfd98bf2bec6e97bba6df82357615acb497fde
Mahesh Aggarwal <maggarwal@ripe.net>
2024-04-30 11:52:12 +0200

    remove unused imports

--- whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/inetnum/InetnumStrictStatusMandatoryValidatorTest.java
+++ whois-update/src/test/java/net/ripe/db/whois/update/handler/validator/inetnum/InetnumStrictStatusMandatoryValidatorTest.java
@@ -14,7 +14,6 @@ import net.ripe.db.whois.common.iptree.Ipv4Tree;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 import net.ripe.db.whois.update.authentication.Principal;
 import net.ripe.db.whois.update.authentication.Subject;
-import net.ripe.db.whois.update.domain.Action;
 import net.ripe.db.whois.update.domain.PreparedUpdate;
 import net.ripe.db.whois.update.domain.UpdateContext;
 import net.ripe.db.whois.update.domain.UpdateMessages;
