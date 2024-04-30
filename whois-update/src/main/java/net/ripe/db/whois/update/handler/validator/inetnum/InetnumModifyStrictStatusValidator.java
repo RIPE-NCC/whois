c3c09144838dc1c68920da02c69e0d0d5f616426
Mahesh Aggarwal <maggarwal@ripe.net>
2024-04-30 13:01:23 +0200

    use subclass for modify

---
+++ whois-update/src/main/java/net/ripe/db/whois/update/handler/validator/inetnum/InetnumModifyStrictStatusValidator.java
@@ -0,0 +1,84 @@
+package net.ripe.db.whois.update.handler.validator.inetnum;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import net.ripe.db.whois.common.Message;
+import net.ripe.db.whois.common.dao.RpslObjectDao;
+import net.ripe.db.whois.common.dao.StatusDao;
+import net.ripe.db.whois.common.domain.CIString;
+import net.ripe.db.whois.common.domain.Maintainers;
+import net.ripe.db.whois.common.ip.Ipv4Resource;
+import net.ripe.db.whois.common.iptree.Ipv4Entry;
+import net.ripe.db.whois.common.iptree.Ipv4Tree;
+import net.ripe.db.whois.common.rpsl.AttributeType;
+import net.ripe.db.whois.common.rpsl.ObjectType;
+import net.ripe.db.whois.common.rpsl.RpslObject;
+import net.ripe.db.whois.common.rpsl.attrs.InetnumStatus;
+import net.ripe.db.whois.update.authentication.Principal;
+import net.ripe.db.whois.update.authentication.Subject;
+import net.ripe.db.whois.update.domain.Action;
+import net.ripe.db.whois.update.domain.PreparedUpdate;
+import net.ripe.db.whois.update.domain.UpdateContext;
+import net.ripe.db.whois.update.domain.UpdateMessages;
+import net.ripe.db.whois.update.handler.validator.BusinessRuleValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static net.ripe.db.whois.common.rpsl.AttributeType.STATUS;
+import static net.ripe.db.whois.common.rpsl.attrs.InetnumStatus.LEGACY;
+import static net.ripe.db.whois.update.domain.Action.CREATE;
+import static net.ripe.db.whois.update.domain.Action.MODIFY;
+
+/**
+ * Apply stricter status validation when modifying an inetnum object.
+ */
+@Component
+public class InetnumModifyStrictStatusValidator extends InetnumStrictStatusValidator {
+
+    private static final ImmutableList<Action> ACTIONS = ImmutableList.of(MODIFY);
+    private static final ImmutableList<ObjectType> TYPES = ImmutableList.of(ObjectType.INETNUM);
+
+    @Autowired
+    public InetnumModifyStrictStatusValidator(
+            final RpslObjectDao objectDao,
+            final StatusDao statusDao,
+            final Ipv4Tree ipv4Tree,
+            final Maintainers maintainers) {
+        super(objectDao, statusDao, ipv4Tree, maintainers);
+
+    }
+
+    @Override
+    public List<Message> performValidation(final PreparedUpdate update, final UpdateContext updateContext) {
+        if(canSkipValidation(update)) {
+            return Collections.EMPTY_LIST;
+        }
+
+        return validateStatusAgainstResourcesInTree(update, updateContext);
+    }
+
+    protected static boolean canSkipValidation(final PreparedUpdate update) {
+        final InetnumStatus originalStatus = InetnumStatus.getStatusFor(update.getReferenceObject().getValueForAttribute(AttributeType.STATUS));
+        final InetnumStatus updateStatus = InetnumStatus.getStatusFor(update.getUpdatedObject().getValueForAttribute(AttributeType.STATUS));
+
+        return (originalStatus == updateStatus) || !InetnumStatusValidator.canChangeStatus(originalStatus, updateStatus);
+    }
+
+    @Override
+    public boolean isSkipForOverride() {
+        return true;
+    }
+    @Override
+    public ImmutableList<Action> getActions() {
+        return ACTIONS;
+    }
+
+    @Override
+    public ImmutableList<ObjectType> getTypes() {
+        return TYPES;
+    }
+}
