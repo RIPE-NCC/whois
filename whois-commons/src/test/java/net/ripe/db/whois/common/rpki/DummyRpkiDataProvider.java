7f040a271c9c9ee21010e42d11e0d3c7063ce14a
Ed Shryane <eshryane@ripe.net>
2024-05-08 21:55:28 +0200

    Copied changes from "Add Email Attachment(s) After Message Body" PR

--- whois-commons/src/test/java/net/ripe/db/whois/common/rpki/DummyRpkiDataProvider.java
+++ whois-commons/src/test/java/net/ripe/db/whois/common/rpki/DummyRpkiDataProvider.java
@@ -6,33 +6,35 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @Component
 @Profile({WhoisProfile.TEST})
 public class DummyRpkiDataProvider implements RpkiDataProvider {
 
-    private List<Roa> roas;
+    private List<Roa> loadedRoas;
 
     @Override
     public List<Roa> loadRoas() {
-        if (roas == null || roas.isEmpty()) {
-            try {
-                loadRoas(new ObjectMapper().readValue(getClass().getResourceAsStream("/rpki/roas.json"), Roas.class).getRoas());
-            } catch (IOException e){
-                throw new IllegalStateException(e);
-            }
+        if (loadedRoas!= null && !loadedRoas.isEmpty()){
+            return loadedRoas;
         }
 
-        return roas;
+        try {
+            return new ObjectMapper().readValue(getClass().getResourceAsStream("/rpki/roas.json"), Roas.class).getRoas();
+        } catch (IOException ex){
+            /* Do Nothing*/
+        }
+        return Collections.emptyList();
     }
 
-    public List<Roa> loadRoas(final List<Roa> roas) {
-        if (this.roas == null || this.roas.isEmpty()) {
-            this.roas = roas;
+    public void loadCustomRoas(final List<Roa> roas){
+        if (loadedRoas == null || loadedRoas.isEmpty()){
+            this.loadedRoas = roas;
+            return;
         }
-        this.roas.addAll(roas);
-        return this.roas;
+        loadedRoas.addAll(roas);
     }
 
 }
