8868c3529d1bfcc85cf74e8fdf213ff3694bbd5b
mherran <mherran@ripe.net>
2024-05-14 10:38:05 +0200

    feat: remove unused imports

--- whois-commons/src/test/java/net/ripe/db/whois/common/rpki/DummyRpkiDataProvider.java
+++ whois-commons/src/test/java/net/ripe/db/whois/common/rpki/DummyRpkiDataProvider.java
@@ -2,12 +2,10 @@ package net.ripe.db.whois.common.rpki;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.ripe.db.whois.common.profiles.WhoisProfile;
-import org.apache.commons.compress.utils.Lists;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 @Component
