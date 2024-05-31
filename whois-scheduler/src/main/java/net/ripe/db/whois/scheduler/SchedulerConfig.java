682ce7e632dcb86c64945e06339a2adf5f8933b1
Ed Shryane <eshryane@ripe.net>
2024-05-31 15:00:29 +0200

    Specify aspectj mode for scheduler config

--- whois-scheduler/src/main/java/net/ripe/db/whois/scheduler/SchedulerConfig.java
+++ whois-scheduler/src/main/java/net/ripe/db/whois/scheduler/SchedulerConfig.java
@@ -4,6 +4,7 @@ import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +21,7 @@ import javax.sql.DataSource;
 @Configuration
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor="23h")
+@EnableSchedulerLock(defaultLockAtMostFor="23h", mode = AdviceMode.ASPECTJ)
 @ImportResource(value = "classpath:applicationContext-api.xml")
 @ComponentScan(basePackages="net.ripe.db.whois.scheduler")
 public class SchedulerConfig {
