f9b4d572363ed3d6fee59f32364ff2a09e512454
mherran <mherran@ripe.net>
2024-05-31 16:51:27 +0200

    feat: rename class and add a comment

--- whois-commons/src/main/java/net/ripe/db/whois/common/rpki/WhoisRoaChecker.java
+++ whois-commons/src/main/java/net/ripe/db/whois/common/rpki/WhoisRoaChecker.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-import static net.ripe.db.whois.common.rpki.ValidationStatus.INVALID;
 import static net.ripe.db.whois.common.rpki.ValidationStatus.INVALID_ORIGIN;
 import static net.ripe.db.whois.common.rpki.ValidationStatus.INVALID_PREFIX_AND_ORIGIN;
 import static net.ripe.db.whois.common.rpki.ValidationStatus.INVALID_PREFIX_LENGTH;
@@ -22,12 +21,14 @@ import static net.ripe.db.whois.common.rpki.ValidationStatus.NOT_FOUND;
 import static net.ripe.db.whois.common.rpki.ValidationStatus.VALID;
 
 @Component
-public class WhoisRoaChecker extends RpkiRoaChecker {
+public class WhoisRoaChecker extends AbstractRpkiRoaChecker {
     public WhoisRoaChecker(final RpkiService rpkiService) {
         super(rpkiService);
     }
 
     public Map.Entry<Roa, ValidationStatus> validateAndGetInvalidRoa(final RpslObject route){
+        /* This method prioritize VALID roas over INVALID roas. So in case of overlap the VALID ROA will se used.
+         This is a common behaviour related to roas */
         final Map<Roa, ValidationStatus> roasStatus = validateRoas(route);
         final Optional<Map.Entry<Roa, ValidationStatus>> roaStatusMap = roasStatus
                 .entrySet()
