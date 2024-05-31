55562dfd8fb448b23f9d48ee2e093a1d803af65f
Ed Shryane <eshryane@ripe.net>
2024-05-31 12:17:07 +0200

    Rename duplicate classes

--- whois-commons/src/test/java/net/ripe/db/whois/common/aspects/RetryForAspectMethodTypeIntegrationTest.java
+++ whois-commons/src/test/java/net/ripe/db/whois/common/aspects/RetryForAspectMethodTypeIntegrationTest.java
@@ -1,37 +1,28 @@
-package net.ripe.db.whois.db;
+package net.ripe.db.whois.common.aspects;
 
 
-import net.ripe.db.whois.common.aspects.RetryFor;
 import net.ripe.db.whois.common.support.AbstractDaoIntegrationTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.fail;
 
-/**
- * Test to ensure that the Retry aspect is properly applied from whois-commons onto whois sub-modules.
- * <p/>
- * This test also fails running in your IDE if AspectJ is not configured correctly.
- */
-
 @Tag("IntegrationTest")
-@ContextConfiguration(locations = {"classpath:applicationContext-whois-test.xml"})
-public class RetryForAspectIntegrationTest extends AbstractDaoIntegrationTest {
+public class RetryForAspectMethodTypeIntegrationTest extends AbstractDaoIntegrationTest {
     static final int ATTEMPTS = 5;
 
     AtomicInteger attemptCounter;
 
-    @Autowired RetryForAspectOnClass retryForAspectOnClass;
+    @Autowired RetryForAspectMethod retryForAspectMethod;
+    @Autowired RetryForAspectType retryForAspectType;
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -55,7 +46,7 @@ public class RetryForAspectIntegrationTest extends AbstractDaoIntegrationTest {
 
     private void retryForAnnotatedMethod(final Exception e, final int expectedAttempts) throws Exception {
         try {
-            retryForAspectOnClass.incrementAndThrowException(attemptCounter, e);
+            retryForAspectMethod.incrementAndThrowException(attemptCounter, e);
             fail("Expected exception");
         } catch (Exception exc) {
             assertThat(e, is(exc));
@@ -81,18 +72,12 @@ public class RetryForAspectIntegrationTest extends AbstractDaoIntegrationTest {
 
     private void retryForAnnotatedType(final Exception e, final int expectedAttempts) throws Exception {
         try {
-            incrementAndThrowException(attemptCounter, e);
+            retryForAspectType.incrementAndThrowException(attemptCounter, e);
             fail("Expected exception");
         } catch (Exception exc) {
             assertThat(e, is(exc));
         }
 
         assertThat(attemptCounter.get(), is(expectedAttempts));
-    }
-
-    @RetryFor(value = IOException.class, attempts = RetryForAspectIntegrationTest.ATTEMPTS, intervalMs = 0)
-    public void incrementAndThrowException(final AtomicInteger counter, final Exception e) throws Exception {
-        counter.incrementAndGet();
-        throw e;
     }
 }
