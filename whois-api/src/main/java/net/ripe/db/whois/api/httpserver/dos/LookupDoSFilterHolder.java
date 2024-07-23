b782584f384d188a31738c9e70caf71ecd2150ea
mherran <mherran@ripe.net>
2024-07-23 11:15:26 +0200

    feaet: remove unused constant

--- whois-api/src/main/java/net/ripe/db/whois/api/httpserver/dos/LookupDoSFilterHolder.java
+++ whois-api/src/main/java/net/ripe/db/whois/api/httpserver/dos/LookupDoSFilterHolder.java
@@ -12,8 +12,6 @@ public class LookupDoSFilterHolder extends AbstractDoSFilterHolder {
 
     private final String dosQueriesMaxSecs;
 
-    private static final String MAX_REQUEST_PER_MS = "" + 10 * 60 * 1_000; // high default, 10 minutes
-
     public LookupDoSFilterHolder(@Value("${dos.filter.enabled:false}") final boolean dosFilterEnabled,
                                  @Value("${ipranges.trusted}") final String trustedIpRanges,
                                  @Value("${dos.filter.max.query:50}") final String dosQueriesMaxSecs) {
