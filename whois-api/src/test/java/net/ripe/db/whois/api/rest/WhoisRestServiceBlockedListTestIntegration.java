6eda2461735d84856575f0e81796e0a33c9f833c
mherran <mherran@ripe.net>
2024-07-25 13:52:13 +0200

    feat: add more ITs

--- whois-api/src/test/java/net/ripe/db/whois/api/rest/WhoisRestServiceBlockedListTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/rest/WhoisRestServiceBlockedListTestIntegration.java
@@ -47,6 +47,26 @@ public class WhoisRestServiceBlockedListTestIntegration extends AbstractIntegrat
             "mnt-by:      OWNER-MNT\n" +
             "source:      TEST");
 
+    private static final RpslObject PERSON_OBJECT = RpslObject.parse("" +
+            "person:    Pauleth Palthen\n" +
+            "address:   Singel 258\n" +
+            "phone:     +31-1234567890\n" +
+            "e-mail:    noreply@ripe.net\n" +
+            "mnt-by:    OWNER-MNT\n" +
+            "nic-hdl:   PP1-TEST\n" +
+            "remarks:   remark\n" +
+            "source:    TEST\n");
+
+    private static final RpslObject UPDATED_PERSON_OBJECT = RpslObject.parse("" +
+            "person:    Pauleth Palthen\n" +
+            "address:   Singel 258 updated\n" +
+            "phone:     +31-1234567890\n" +
+            "e-mail:    noreply@ripe.net\n" +
+            "mnt-by:    OWNER-MNT\n" +
+            "nic-hdl:   PP1-TEST\n" +
+            "remarks:   remark\n" +
+            "source:    TEST\n");
+
     @BeforeAll
     public static void beforeClass() throws MalformedObjectNameException {
         final String unsupportedIps = "193.0.0.0 - 193.0.23.255, 2001:67c:2e8::/48";
@@ -159,19 +179,9 @@ public class WhoisRestServiceBlockedListTestIntegration extends AbstractIntegrat
          // Add IP to blocked list
          mBeanServer.invoke(mBeanName, "addBlockedListAddress", new Object[]{"8.8.8.8"}, new String[]{String.class.getName()});
 
-         final RpslObject person = RpslObject.parse("" +
-                 "person:    Pauleth Palthen\n" +
-                 "address:   Singel 258\n" +
-                 "phone:     +31-1234567890\n" +
-                 "e-mail:    noreply@ripe.net\n" +
-                 "mnt-by:    OWNER-MNT\n" +
-                 "nic-hdl:   PP1-TEST\n" +
-                 "remarks:   remark\n" +
-                 "source:    TEST\n");
-
          final Response response = RestTest.target(getPort(), "whois/test/person?clientIp=8.8.8.8&password=test")
                  .request()
-                 .post(Entity.entity(map(person), MediaType.APPLICATION_XML));
+                 .post(Entity.entity(map(PERSON_OBJECT), MediaType.APPLICATION_XML));
 
          assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(response.getStatus()));
          assertThat(response.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
@@ -182,22 +192,214 @@ public class WhoisRestServiceBlockedListTestIntegration extends AbstractIntegrat
         // Add IP to blocked list
         mBeanServer.invoke(mBeanName, "addBlockedListAddress", new Object[]{"2001:fff:001::/48"}, new String[]{String.class.getName()});
 
-        final RpslObject person = RpslObject.parse("" +
-                "person:    Pauleth Palthen\n" +
-                "address:   Singel 258\n" +
-                "phone:     +31-1234567890\n" +
-                "e-mail:    noreply@ripe.net\n" +
-                "mnt-by:    OWNER-MNT\n" +
-                "nic-hdl:   PP1-TEST\n" +
-                "remarks:   remark\n" +
-                "source:    TEST\n");
-
         final Response response = RestTest.target(getPort(), "whois/test/person?clientIp=2001:fff:001::&password=test")
                 .request()
-                .post(Entity.entity(map(person), MediaType.APPLICATION_XML));
+                .post(Entity.entity(map(PERSON_OBJECT), MediaType.APPLICATION_XML));
 
         assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(response.getStatus()));
         assertThat(response.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+    }
+
+    @Test
+    public void remove_blocked_ipv4_post_request_then_200() throws Exception {
+        final String reformedClientRequest = "whois/test/person?clientIp=193.0.0.1&password=test";
+
+        final Response errorResponse = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .post(Entity.entity(map(PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(errorResponse.getStatus()));
+        assertThat(errorResponse.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+
+        // Remove IP from blocked list
+        mBeanServer.invoke(mBeanName, "removeBlockedListAddress", new Object[]{"193.0.0.0 - 193.0.23.255"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .post(Entity.entity(map(PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.OK_200, is(response.getStatus()));
+    }
+
+    @Test
+    public void remove_blocked_ipv6_post_request_then_200() throws Exception {
+        final String reformedClientRequest = "whois/test/person?clientIp=2001:67c:2e8::&password=test";
+
+        final Response errorResponse = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .post(Entity.entity(map(PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(errorResponse.getStatus()));
+        assertThat(errorResponse.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+
+        // Remove IP from blocked list
+        mBeanServer.invoke(mBeanName, "removeBlockedListAddress", new Object[]{"2001:67c:2e8::/48"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .post(Entity.entity(map(PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.OK_200, is(response.getStatus()));
+    }
+
+
+    /*
+       Update
+    */
+    @Test
+    public void add_blocked_ipv4_put_request_then_429_too_many_requests() throws Exception {
+        databaseHelper.addObject(PERSON_OBJECT);
+
+        // Add IP to blocked list
+        mBeanServer.invoke(mBeanName, "addBlockedListAddress", new Object[]{"8.8.8.8"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), "whois/test/person/PP1-TEST?clientIp=8.8.8.8&password=test")
+                .request()
+                .put(Entity.entity(map(UPDATED_PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(response.getStatus()));
+        assertThat(response.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+    }
+
+    @Test
+    public void add_blocked_ipv6_put_request_then_429_too_many_requests() throws Exception {
+        databaseHelper.addObject(PERSON_OBJECT);
+
+        // Add IP to blocked list
+        mBeanServer.invoke(mBeanName, "addBlockedListAddress", new Object[]{"2001:fff:001::/48"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), "whois/test/person/PP1-TEST?clientIp=2001:fff:001::&password=test")
+                .request()
+                .put(Entity.entity(map(UPDATED_PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(response.getStatus()));
+        assertThat(response.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+    }
+
+    @Test
+    public void remove_blocked_ipv4_put_request_then_200() throws Exception {
+        databaseHelper.addObject(PERSON_OBJECT);
+
+        final String reformedClientRequest = "whois/test/person/PP1-TEST?clientIp=193.0.0.1&password=test";
+
+        final Response errorResponse = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .put(Entity.entity(map(UPDATED_PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(errorResponse.getStatus()));
+        assertThat(errorResponse.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+
+        // Remove IP from blocked list
+        mBeanServer.invoke(mBeanName, "removeBlockedListAddress", new Object[]{"193.0.0.0 - 193.0.23.255"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .put(Entity.entity(map(UPDATED_PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.OK_200, is(response.getStatus()));
+    }
+
+    @Test
+    public void remove_blocked_ipv6_put_request_then_200() throws Exception {
+        databaseHelper.addObject(PERSON_OBJECT);
+
+        final String reformedClientRequest = "whois/test/person/PP1-TEST?clientIp=2001:67c:2e8::&password=test";
+
+        final Response errorResponse = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .put(Entity.entity(map(UPDATED_PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(errorResponse.getStatus()));
+        assertThat(errorResponse.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+
+        // Remove IP from blocked list
+        mBeanServer.invoke(mBeanName, "removeBlockedListAddress", new Object[]{"2001:67c:2e8::/48"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .put(Entity.entity(map(UPDATED_PERSON_OBJECT), MediaType.APPLICATION_XML));
+
+        assertThat(HttpStatus.OK_200, is(response.getStatus()));
+    }
+
+
+    /*
+        Delete
+    */
+    @Test
+    public void add_blocked_ipv4_delete_request_then_429_too_many_requests() throws Exception {
+        databaseHelper.addObject(PERSON_OBJECT);
+
+        // Add IP to blocked list
+        mBeanServer.invoke(mBeanName, "addBlockedListAddress", new Object[]{"8.8.8.8"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), "whois/test/person/PP1-TEST?clientIp=8.8.8.8&password=test")
+                .request()
+                .delete();
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(response.getStatus()));
+        assertThat(response.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+    }
+
+    @Test
+    public void add_blocked_ipv6_delete_request_then_429_too_many_requests() throws Exception {
+        databaseHelper.addObject(PERSON_OBJECT);
+
+        // Add IP to blocked list
+        mBeanServer.invoke(mBeanName, "addBlockedListAddress", new Object[]{"2001:fff:001::/48"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), "whois/test/person/PP1-TEST?clientIp=2001:fff:001::&password=test")
+                .request()
+                .delete();
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(response.getStatus()));
+        assertThat(response.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+    }
+
+    @Test
+    public void remove_blocked_ipv4_delete_request_then_200() throws Exception {
+        databaseHelper.addObject(PERSON_OBJECT);
+
+        final String reformedClientRequest = "whois/test/person/PP1-TEST?clientIp=193.0.0.1&password=test";
+
+        final Response errorResponse = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .delete();
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(errorResponse.getStatus()));
+        assertThat(errorResponse.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+
+        // Remove IP from blocked list
+        mBeanServer.invoke(mBeanName, "removeBlockedListAddress", new Object[]{"193.0.0.0 - 193.0.23.255"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .delete();
+
+        assertThat(HttpStatus.OK_200, is(response.getStatus()));
+    }
+
+    @Test
+    public void remove_blocked_ipv6_delete_request_then_200() throws Exception {
+        databaseHelper.addObject(PERSON_OBJECT);
+
+        final String reformedClientRequest = "whois/test/person/PP1-TEST?clientIp=2001:67c:2e8::&password=test";
+
+        final Response errorResponse = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .delete();
+
+        assertThat(HttpStatus.TOO_MANY_REQUESTS_429, is(errorResponse.getStatus()));
+        assertThat(errorResponse.readEntity(String.class), is("You have been permanently blocked. Please contact support"));
+
+        // Remove IP from blocked list
+        mBeanServer.invoke(mBeanName, "removeBlockedListAddress", new Object[]{"2001:67c:2e8::/48"}, new String[]{String.class.getName()});
+
+        final Response response = RestTest.target(getPort(), reformedClientRequest)
+                .request()
+                .delete();
+
+        assertThat(HttpStatus.OK_200, is(response.getStatus()));
     }
 
     //Helper methods
