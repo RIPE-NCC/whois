cdadd8e3837661ce21c16ad447704521cb2de33a
Ed Shryane <eshryane@ripe.net>
2024-09-23 13:42:22 +0200

    Tested internal schema changes

--- whois-commons/src/main/resources/patch/internals-1.115.sql
+++ whois-commons/src/main/resources/patch/internals-1.115.sql
@@ -6,14 +6,12 @@ ALTER TABLE version ADD PRIMARY KEY (version);
 --
 -- increase width of columns containing an email address to the maximum allowed by RFC 5321.
 --
-ALTER TABLE email_links CHANGE COLUMN email varchar(320) NOT NULL;
-ALTER TABLE forgot_password_audit_log CHANGE COLUMN email varchar(320) DEFAULT NULL;
-ALTER TABLE default_maintainer_history CHANGE COLUMN email varchar(320);
-ALTER TABLE default_maintainer_sync_history CHANGE COLUMN email varchar(320) NOT NULL;
-ALTER TABLE default_maintainer_sync CHANGE COLUMN email varchar(320) NOT NULL;
-ALTER TABLE abuse_email CHANGE COLUMN address varchar(320) NOT NULL;
-ALTER TABLE outgoing_message CHANGE COLUMN email varchar(320) NOT NULL;
-ALTER TABLE email_status CHANGE COLUMN email varchar(320) NOT NULL;
+ALTER TABLE email_links MODIFY COLUMN email varchar(320) NOT NULL;
+ALTER TABLE forgot_password_audit_log MODIFY COLUMN email varchar(320) DEFAULT NULL;
+ALTER TABLE default_maintainer_sync MODIFY COLUMN email varchar(320) NOT NULL;
+ALTER TABLE abuse_email MODIFY COLUMN address varchar(320) NOT NULL;
+ALTER TABLE outgoing_message MODIFY COLUMN email varchar(320) NOT NULL;
+ALTER TABLE email_status MODIFY COLUMN email varchar(320) NOT NULL;
 
 --
 -- Set version to Whois 1.115 release
