9a9b24f44f38a30e2d3e8afad2e737b9c65c9a9c
Ed Shryane <eshryane@ripe.net>
2024-09-23 12:39:40 +0200

    Fixed whois schema (do not allow empty values in index tables)

--- whois-commons/src/main/resources/patch/whois-1.115.sql
+++ whois-commons/src/main/resources/patch/whois-1.115.sql
@@ -6,14 +6,13 @@ ALTER TABLE version ADD PRIMARY KEY (version);
 --
 -- increase width of columns containing an email address to the maximum allowed by RFC 5321.
 --
-ALTER TABLE abuse_mailbox CHANGE COLUMN abuse_mailbox varchar(320) NOT NULL DEFAULT '';
-ALTER TABLE e_mail CHANGE COLUMN email varchar(320) NOT NULL DEFAULT '';
-ALTER TABLE irt_nfy CHANGE COLUMN irt_nfy varchar(320) NOT NULL DEFAULT '';
-ALTER TABLE mnt_nfy CHANGE COLUMN mnt_nfy varchar(320) NOT NULL DEFAULT '';
-ALTER TABLE notify CHANGE COLUMN notify varchar(320) NOT NULL DEFAULT '';
-ALTER TABLE ref_nfy CHANGE COLUMN ref_nfy varchar(320) NOT NULL DEFAULT '';
-ALTER TABLE upd_to CHANGE COLUMN upd_to varchar(320) NOT NULL DEFAULT '';
-
+ALTER TABLE abuse_mailbox MODIFY COLUMN abuse_mailbox varchar(320);
+ALTER TABLE e_mail MODIFY COLUMN e_mail varchar(320);
+ALTER TABLE irt_nfy MODIFY COLUMN irt_nfy varchar(320);
+ALTER TABLE mnt_nfy MODIFY COLUMN mnt_nfy varchar(320);
+ALTER TABLE notify MODIFY COLUMN notify varchar(320);
+ALTER TABLE ref_nfy MODIFY COLUMN ref_nfy varchar(320);
+ALTER TABLE upd_to MODIFY COLUMN upd_to varchar(320);
 --
 -- Set version to Whois 1.115 release
 --
