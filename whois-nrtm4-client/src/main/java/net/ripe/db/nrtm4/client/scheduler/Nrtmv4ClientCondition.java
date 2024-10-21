817dc641e713910695bbfe4b7e9cb48b99944982
mherran <mherran@ripe.net>
2024-10-21 16:17:11 +0200

    feat: change properties and put logs in the condition

--- whois-nrtm4-client/src/main/java/net/ripe/db/nrtm4/client/scheduler/Nrtmv4ClientCondition.java
+++ whois-nrtm4-client/src/main/java/net/ripe/db/nrtm4/client/scheduler/Nrtmv4ClientCondition.java
@@ -1,16 +1,22 @@
 package net.ripe.db.nrtm4.client.scheduler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class Nrtmv4ClientCondition implements Condition {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Nrtm4ClientSchedulerTask.class);
+
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         if( !context.getEnvironment().containsProperty("nrtm4.client.enabled")) {
+            LOGGER.info("Nrtm4 client is not enabled");
             return false;
         }
+        LOGGER.info("Nrtm4 client is enabled");
         return Boolean.parseBoolean(context.getEnvironment().getProperty("nrtm4.client.enabled"));
     }
 }
