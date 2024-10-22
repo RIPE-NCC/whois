9d5a63dbd7e2000751e68d1b4b78e2048c6d379f
mherran <mherran@ripe.net>
2024-10-22 11:46:38 +0200

    feat: try catch duplicated key exception when the version already exist

--- whois-nrtm4-client/src/main/java/net/ripe/db/nrtm4/client/scheduler/reader/UpdateNotificationFileReader.java
+++ whois-nrtm4-client/src/main/java/net/ripe/db/nrtm4/client/scheduler/reader/UpdateNotificationFileReader.java
@@ -7,6 +7,7 @@ import net.ripe.db.nrtm4.client.scheduler.dao.Nrtm4ClientMirrorRepository;
 import net.ripe.db.whois.api.rest.client.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -43,8 +44,11 @@ public class UpdateNotificationFileReader {
 
         //TODO: [MH] Review integrity of the data checking the signature using the public key
         notificationFilePerSource.forEach((source, updateNotificationFile) -> {
-            LOGGER.info("The source is {}", source);
-            nrtm4ClientMirrorDao.saveUpdateNotificationFileVersion(source, updateNotificationFile.getVersion(), updateNotificationFile.getSessionID());
+            try {
+                nrtm4ClientMirrorDao.saveUpdateNotificationFileVersion(source, updateNotificationFile.getVersion(), updateNotificationFile.getSessionID());
+            } catch (DuplicateKeyException ex){
+                LOGGER.info("There is no new version associated with the source {}", source);
+            }
         });
 
         //TODO: [MH] if last_mirror is empty, we need to store from scratch. Take snapshot the snapshot.
