d25d726c0396377b892a07c50edc157b2581d91b
mherran <mherran@ripe.net>
2024-12-05 10:51:45 +0100

    feat: fix issue with dependencies

--- whois-nrtm4-client/src/main/java/net/ripe/db/nrtm4/client/config/NrtmClientDataSourceConfigurations.java
+++ whois-nrtm4-client/src/main/java/net/ripe/db/nrtm4/client/config/NrtmClientDataSourceConfigurations.java
@@ -16,28 +16,28 @@ import static net.ripe.db.whois.common.DataSourceConfigurations.createDataSource
 @Configuration
 public class NrtmClientDataSourceConfigurations {
 
-    @Bean
+    @Bean(name = "nrtmClientMasterInfoSource")
     public ComboPooledDataSource nrtmClientMasterInfoSource(@Value("${nrtm.client.info.database.url}") final String jdbcUrl,
                                                             @Value("${nrtm.client.info.database.username}") final String jdbcUser,
                                                             @Value("${nrtm.client.info.database.password}") final String jdbcPass) throws PropertyVetoException {
         return createDataSource(DRIVER_CLASS_NAME, jdbcUrl, jdbcUser, jdbcPass);
     }
 
-    @Bean
+    @Bean(name = "nrtmClientSlaveInfoSource")
     public ComboPooledDataSource nrtmClientSlaveInfoSource(@Value("${nrtm.client.info.slave.database.url}") final String jdbcUrl,
                                                            @Value("${nrtm.client.info.slave.database.username}") final String jdbcUser,
                                                            @Value("${nrtm.client.info.slave.database.password}") final String jdbcPass) throws PropertyVetoException {
         return createDataSource(DRIVER_CLASS_NAME, jdbcUrl, jdbcUser, jdbcPass);
     }
 
-    @Bean
+    @Bean(name = "nrtmClientMasterDataSource")
     public ComboPooledDataSource nrtmClientMasterDataSource(@Value("${nrtm.client.database.url}") final String jdbcUrl,
                                                             @Value("${nrtm.client.database.username}") final String jdbcUser,
                                                             @Value("${nrtm.client.database.password}") final String jdbcPass) throws PropertyVetoException {
         return createDataSource(DRIVER_CLASS_NAME, jdbcUrl, jdbcUser, jdbcPass);
     }
 
-    @Bean
+    @Bean(name = "nrtmClientSlaveDataSource")
     public ComboPooledDataSource nrtmClientSlaveDataSource(@Value("${nrtm.client.slave.database.url}") final String jdbcUrl,
                                                            @Value("${nrtm.client.slave.database.username}") final String jdbcUser,
                                                            @Value("${nrtm.client.slave.database.password}") final String jdbcPass) throws PropertyVetoException {
--- whois-nrtm4-client/src/main/java/net/ripe/db/nrtm4/client/config/NrtmClientTransactionConfiguration.java
+++ whois-nrtm4-client/src/main/java/net/ripe/db/nrtm4/client/config/NrtmClientTransactionConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -18,11 +19,13 @@ public class NrtmClientTransactionConfiguration {
     public static final String NRTM_CLIENT_INFO_UPDATE_TRANSACTION = "nrtm-update-info-transaction-manager";
 
     @Bean(name = NRTM_CLIENT_UPDATE_TRANSACTION)
+    @DependsOn("nrtmClientMasterDataSource")
     public TransactionManager transactionManagerNrtmUpdate(@Qualifier("nrtmClientMasterDataSource") final DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean(name = NRTM_CLIENT_INFO_UPDATE_TRANSACTION)
+    @DependsOn("nrtmClientMasterInfoSource")
     public TransactionManager transactionManagerNrtmInfo(@Qualifier("nrtmClientMasterInfoSource") final DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
