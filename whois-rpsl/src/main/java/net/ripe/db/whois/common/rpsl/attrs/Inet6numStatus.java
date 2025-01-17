147334a9fe3220eb1fa7f76335cc6099c8f43f2e
mherran <mherran@ripe.net>
2025-01-17 15:04:34 +0100

    feat: unecessary changes

--- whois-rpsl/src/main/java/net/ripe/db/whois/common/rpsl/attrs/Inet6numStatus.java
+++ whois-rpsl/src/main/java/net/ripe/db/whois/common/rpsl/attrs/Inet6numStatus.java
@@ -81,11 +81,6 @@ public enum Inet6numStatus implements InetStatus {
     }
 
     @Override
-    public boolean isAdministrativeResource() {
-        return this.equals(ALLOCATED_BY_RIR);
-    }
-
-    @Override
     public boolean needsOrgReference() {
         return NEEDS_ORG_REFERENCE.contains(this);
     }
