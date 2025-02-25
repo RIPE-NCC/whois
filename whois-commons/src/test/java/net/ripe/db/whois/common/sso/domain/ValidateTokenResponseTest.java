c311485050304254dfa2c59c198cf5e2169bf1fb
Ed Shryane <eshryane@ripe.net>
2025-02-25 15:49:43 +0100

    Removed test code

--- whois-commons/src/test/java/net/ripe/db/whois/common/sso/domain/ValidateTokenResponseTest.java
+++ whois-commons/src/test/java/net/ripe/db/whois/common/sso/domain/ValidateTokenResponseTest.java
@@ -1,6 +1,5 @@
 package net.ripe.db.whois.common.sso.domain;
 
-import net.ripe.db.whois.common.sso.domain.ValidateTokenResponse;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -8,8 +7,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -41,17 +38,4 @@ class ValidateTokenResponseTest {
             return (ValidateTokenResponse)objectInputStream.readObject();
         }
     }
-
-    @Test
-    public void scale() {
-        System.out.println(divide(100,10));
-    }
-
-    private BigDecimal divide(final long val1, final long val2) {
-        return BigDecimal.valueOf(val1)
-                .divide(BigDecimal.valueOf(val2), 10, RoundingMode.HALF_UP)
-                .multiply(BigDecimal.valueOf(100));
-
-    }
-
 }
