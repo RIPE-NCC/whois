b485de969f29e9321eecf6f36973bb7a58b6a7f1
Miguel Herrán <106821523+MiguelAHM@users.noreply.github.com>
2025-02-25 13:39:49 +0100

    feat: Rename Archived Undeliverable Emails Date Columns (#1682)

--- whois-commons/src/main/resources/internals_schema.sql
+++ whois-commons/src/main/resources/internals_schema.sql
@@ -163,12 +163,12 @@ CREATE TABLE `email_status` (
 
 DROP TABLE IF EXISTS `email_status_history`;
 CREATE TABLE `email_status_history` (
-     `email` varchar(320) NOT NULL,
-     `status` varchar(120) NOT NULL,
-     `message` longblob,
-     `archived_date` datetime DEFAULT now(),
-     `reported_date` datetime NOT NULL,
-     PRIMARY KEY (`email`, `reported_date`)
+    `email` varchar(320) NOT NULL,
+    `status` varchar(120) NOT NULL,
+    `message` longblob,
+    `history_update` datetime DEFAULT now(),
+    `email_status_update` datetime NOT NULL,
+    PRIMARY KEY (`email`, `email_status_update`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
