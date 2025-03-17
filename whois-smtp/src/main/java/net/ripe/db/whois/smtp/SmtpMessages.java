dfc3ad90e0ad260eec70c70bc895ec53c1ed691d
Ed Shryane <eshryane@ripe.net>
2025-03-17 12:18:10 +0100

    SIZE is not implemented for now

--- whois-smtp/src/main/java/net/ripe/db/whois/smtp/SmtpMessages.java
+++ whois-smtp/src/main/java/net/ripe/db/whois/smtp/SmtpMessages.java
@@ -23,7 +23,6 @@ public class SmtpMessages {
     public static Message extendedHello(final String value) {
         return new Message(Messages.Type.INFO,
             "250-%s Hello %s\n" +
-//                "250-SIZE 52428800\n" +       // TODO
                 "250-8BITMIME\n" +
                 "250 HELP", Hosts.getInstanceName(), value);
     }
