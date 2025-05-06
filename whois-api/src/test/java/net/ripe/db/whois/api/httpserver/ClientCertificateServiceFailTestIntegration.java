8a50634f898aa5e660cc46a84ec1f9ed3504d7ee
Ed Shryane <eshryane@ripe.net>
2025-05-06 14:41:28 +0200

    Log more information about exception.

--- whois-api/src/test/java/net/ripe/db/whois/api/httpserver/ClientCertificateServiceFailTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/httpserver/ClientCertificateServiceFailTestIntegration.java
@@ -8,8 +8,12 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.TestExecutionListeners;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @Tag("IntegrationTest")
@@ -28,8 +32,19 @@ public class ClientCertificateServiceFailTestIntegration extends AbstractClientC
                 .get(String.class);
             fail();
         } catch (ProcessingException e) {
-            assertThat(e.getMessage(), containsString("javax.net.ssl.SSLHandshakeException: Received fatal alert: bad_certificate"));
+            if (e.getMessage().equals("java.io.IOException: Error writing to server")) {
+                assertThat(stackTrace(e), containsString("hduhduygnmckdmokojinn"));
+                assertThat(e.getClass().getName(), containsStringIgnoringCase("ProcessingException"));
+            } else {
+                assertThat(e.getMessage(), containsString("javax.net.ssl.SSLHandshakeException: Received fatal alert: bad_certificate"));
+            }
         }
+    }
+
+    private static String stackTrace(final Throwable t) {
+        final StringWriter stringWriter = new StringWriter();
+        t.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
     }
 
 }
