8bbdb3efa9dd678cd9da1b147b3e95192b89b7c6
Ed Shryane <eshryane@ripe.net>
2025-05-06 15:33:22 +0200

    Check exception class

--- whois-api/src/test/java/net/ripe/db/whois/api/httpserver/ClientCertificateServiceFailTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/httpserver/ClientCertificateServiceFailTestIntegration.java
@@ -4,16 +4,15 @@ import jakarta.ws.rs.ProcessingException;
 import jakarta.ws.rs.core.MediaType;
 import net.ripe.db.whois.api.SecureRestTest;
 import net.ripe.db.whois.common.support.DirtiesContextBeforeAndAfterClassTestExecutionListener;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.TestExecutionListeners;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.containsStringIgnoringCase;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @Tag("IntegrationTest")
@@ -33,18 +32,20 @@ public class ClientCertificateServiceFailTestIntegration extends AbstractClientC
             fail();
         } catch (ProcessingException e) {
             if (e.getMessage().equals("java.io.IOException: Error writing to server")) {
-                assertThat(stackTrace(e), containsString("hduhduygnmckdmokojinn"));
-                assertThat(e.getClass().getName(), containsStringIgnoringCase("ProcessingException"));
+                assertThat(e.getClass().getName(), Matchers.equalTo("ProcessingException"));
+                switch (e.getCause()) {
+                    case IOException ioException : {
+                        assertThat(ioException.getClass().getName(), Matchers.equalTo("IOException"));
+                    }
+                    default : {// do nothing throw new IllegalStateException("Unexpected value: " + e.getCause());
+                    }
+                }
+
             } else {
                 assertThat(e.getMessage(), containsString("javax.net.ssl.SSLHandshakeException: Received fatal alert: bad_certificate"));
             }
         }
     }
 
-    private static String stackTrace(final Throwable t) {
-        final StringWriter stringWriter = new StringWriter();
-        t.printStackTrace(new PrintWriter(stringWriter));
-        return stringWriter.toString();
-    }
 
 }
