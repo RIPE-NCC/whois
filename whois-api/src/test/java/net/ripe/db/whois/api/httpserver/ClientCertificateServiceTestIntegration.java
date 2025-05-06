34c82a1d03bd5c5a5c621aeadefe2bf2195051ed
Ed Shryane <eshryane@ripe.net>
2025-05-06 15:46:00 +0200

    Consider ProcessingException as expected, don't check cause or error message (good enough that it failed).

--- whois-api/src/test/java/net/ripe/db/whois/api/httpserver/ClientCertificateServiceTestIntegration.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/httpserver/ClientCertificateServiceTestIntegration.java
@@ -1,5 +1,7 @@
 package net.ripe.db.whois.api.httpserver;
 
+import jakarta.ws.rs.ProcessingException;
+import jakarta.ws.rs.core.MediaType;
 import net.ripe.db.whois.api.SecureRestTest;
 import net.ripe.db.whois.common.support.DirtiesContextBeforeAndAfterClassTestExecutionListener;
 import org.junit.jupiter.api.Tag;
@@ -8,13 +10,14 @@ import org.springframework.test.context.TestExecutionListeners;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @Tag("IntegrationTest")
 @TestExecutionListeners(
     listeners = DirtiesContextBeforeAndAfterClassTestExecutionListener.class,
     mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
 )
-public class ClientCertificateServiceSuccessTestIntegration extends AbstractClientCertificateIntegrationTest {
+public class ClientCertificateServiceTestIntegration extends AbstractClientCertificateIntegrationTest {
 
     @Test
     public void client_certificate() {
@@ -25,6 +28,19 @@ public class ClientCertificateServiceSuccessTestIntegration extends AbstractClie
         assertThat(response, containsString("Found 1 certificate(s)."));
         assertThat(response, containsString("-----BEGIN CERTIFICATE-----"));
         assertThat(response, containsString("-----END CERTIFICATE-----"));
+    }
+
+    @Test
+    public void fail_on_no_client_certificate() {
+        try {
+            SecureRestTest.target(getClientCertificatePort(), "whois/client")
+                .request()
+                .accept(MediaType.TEXT_PLAIN)
+                .get(String.class);
+            fail();
+        } catch (ProcessingException e) {
+            // expected
+        }
     }
 
 }
