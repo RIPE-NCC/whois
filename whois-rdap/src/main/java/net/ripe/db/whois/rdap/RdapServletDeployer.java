9fb491da23777fdbb606c7ed82b652cc981573f2
Ed Shryane <eshryane@ripe.net>
2025-07-04 10:31:10 +0200

    Upgrade to EE10 (#1637)
    * Ignore alpha/beta/milestone releases in versions-maven-plugin
    
    * Move sentry.version to parent POM
    
    * Update dependency versions to latest
    
    * Refactored NrtmServerDummy
    
    * Update Jetty dependencies
    
    * Add comments for EE10 API dependencies
    
    * Configure versions plugin to ignore EE11 dependencies and DB snapshots
    
    * Depend on correct HTTP2 ALPN dependency
    
    * Fix imports to use Jetty EE10 dependency
    
    * Update dependabot config for EE10.
    
    * Migrated RequestParamRegexRule to Jetty 12 (EE10) and renamed to QueryParamRegexRule
    
    * Migrated RemoteAddressCustomizer to EE10
    
    * fix handlers for EE10
    
    * fix dependency issue
    
    * Fix DummmyServers handlers
    
    * fix pom
    
    * fix No Server ALPNProcessors issue
    
    * fix dummyserver contentType issue
    
    * fix null issue with remoteaddr
    
    * fix webapp issue
    
    * add TODO and fix some tests
    
    * add debugging options
    
    * Replace Conscrypt with Bouncy Castle ALPN provider.
    
    * Revert "Replace Conscrypt with Bouncy Castle ALPN provider."
    
    This reverts commit f53c44fc70e4604bf66162b8a55e22d04b3c683a.
    
    * fix SSL issue
    
    * fix tests
    
    * fix multiple slashes in Url error message
    
    * Strip port from address properly (for /[0:0:0:0:0:0:0:1]:55798 return 0:0:0:0:0:0:0:1, and for /127.0.0.1:55798 return 127.0.0.1).
    
    * fixfix clientCerttificate issue
    
    * Use InetSocketAddress method if available instead of toString()
    
    * Jetty12 thrpws broken pipe exception for bad certificates
    
    * fix some rewrite tests
    
    * fix caseInsensitive regex rule
    
    * reformat rewrite rules
    
    * add a TODO
    
    * Disable default error for rewrite
    
    * fix test
    
    * fix merge issues
    
    * remove unused class
    
    * fix bad certificate tests
    
    * refactor
    
    * simplify remoteAddr
    
    * revert changes
    
    * add loggings
    
    * revert RestTest
    
    * add ipv4 and ipv6 tests instead of localhost
    
    * refactor
    
    * refactor
    
    * fix TODO
    
    * fix redirects issue
    
    * refactor rewrite rules
    
    * refactor
    
    * enable renegotiation with a limit 2
    
    * change base resource path
    
    * disable dir listing
    
    * ssl renegotiation retries property
    
    * use ServletContext rather than webContext
    
    * remove wrong tests
    
    * Fall back to remote address if client Ip is unparseable
    
    * Refactored get query param value
    
    * Use  CONTEXT_INIT = "org.eclipse.jetty.servlet.Default." is defined in DefaultServlet
    
    * Use ServletContextRequest.PEER_CERTIFICATES
    
    ---------
    
    Co-authored-by: Mahesh Aggarwal <maggarwal@ripe.net>
    Co-authored-by: maggarwal13 <50230916+maggarwal13@users.noreply.github.com>

--- whois-rdap/src/main/java/net/ripe/db/whois/rdap/RdapServletDeployer.java
+++ whois-rdap/src/main/java/net/ripe/db/whois/rdap/RdapServletDeployer.java
@@ -3,10 +3,10 @@ package net.ripe.db.whois.rdap;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import jakarta.servlet.DispatcherType;
 import net.ripe.db.whois.api.httpserver.ServletDeployer;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.servlets.CrossOriginFilter;
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.ee10.servlet.FilterHolder;
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.ee10.servlets.CrossOriginFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +31,7 @@ public class RdapServletDeployer implements ServletDeployer {
     }
 
     @Override
-    public void deploy(final WebAppContext context) {
+    public void deploy(final ServletContextHandler context) {
         final RdapJsonProvider rdapJsonProvider = new RdapJsonProvider();
         rdapJsonProvider.configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS, true);
         rdapJsonProvider.configure(SerializationFeature.INDENT_OUTPUT, true);
