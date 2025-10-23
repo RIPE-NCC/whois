53e20d15c297987ad09f0fbbc80b5c09cac2ede3
maggarwal13 <50230916+maggarwal13@users.noreply.github.com>
2025-10-23 10:51:50 +0200

    add warning in updates if apikey is getting expired in 2 weeks (#1891)
    * add warning in updates if apikey is getting expired in 2 weeks
    
    * add interface

---
+++ whois-commons/src/main/java/net/ripe/db/whois/common/oauth/ApiKeyDetailsCacheManager.java
@@ -0,0 +1,59 @@
+package net.ripe.db.whois.common.oauth;
+
+import jakarta.annotation.Nullable;
+import jakarta.annotation.PostConstruct;
+import net.ripe.db.whois.common.dao.KeyCloakApiKeyDao;
+import org.apache.commons.collections.MapUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+@Component
+public class ApiKeyDetailsCacheManager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiKeyDetailsCacheManager.class);
+
+    private final KeyCloakApiKeyDao keyCloakApiKeyDao;
+    private Map<String, LocalDate> keyIdWithExpiry;
+
+    @Autowired
+    public ApiKeyDetailsCacheManager(final KeyCloakApiKeyDao keyCloakApiKeyDao) {
+        this.keyCloakApiKeyDao = keyCloakApiKeyDao;
+    }
+
+    @PostConstruct
+    public void init() {
+        refreshApiKeyDetailsCaches();
+    }
+
+    @Scheduled(fixedDelay = 60 * 60 * 1000)
+    public void refreshApiKeyDetailsCaches() {
+        final Map<String, LocalDate> apikeyIdToExpiry;
+
+        try {
+            apikeyIdToExpiry = keyCloakApiKeyDao.getAllKeyIdWithExpiry();
+        } catch (final Exception e) {
+            LOGGER.warn("Failed to refresh apikey details due to {}: {}", e.getClass().getName(), e.getMessage());
+            return;
+        }
+
+        if (!MapUtils.isEmpty(apikeyIdToExpiry)) {
+            this.keyIdWithExpiry = apikeyIdToExpiry;
+        }
+    }
+
+    @Nullable
+    public LocalDate getExpiryForKeyId(final String keyId) {
+       if ( MapUtils.isEmpty(keyIdWithExpiry)) {
+           LOGGER.warn("No cache found for apikeys");
+           return null;
+       }
+
+       return this.keyIdWithExpiry.getOrDefault(keyId, null);
+    }
+}
--- whois-commons/src/main/resources/internals_schema.sql
+++ whois-commons/src/main/resources/internals_schema.sql
@@ -180,3 +180,11 @@ CREATE TABLE `transfer_update_lock` (
                                         PRIMARY KEY (`global_lock`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `keycloak_apikey_details`;
+
+CREATE TABLE `keycloak_apikey_details` (
+                                           `apikeyId` varchar(320) NOT NULL,
+                                           `expiresAt` datetime NOT NULL,
+                                           PRIMARY KEY (`apikeyId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
