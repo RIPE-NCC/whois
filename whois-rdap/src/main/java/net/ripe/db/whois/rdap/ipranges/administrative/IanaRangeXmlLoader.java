ef027656233e6d1723c53e1655438513469b2a61
Mahesh Aggarwal <maggarwal@ripe.net>
2025-11-06 11:33:33 +0100

    fix tests

--- whois-rdap/src/main/java/net/ripe/db/whois/rdap/ipranges/administrative/IanaRangeXmlLoader.java
+++ whois-rdap/src/main/java/net/ripe/db/whois/rdap/ipranges/administrative/IanaRangeXmlLoader.java
@@ -3,34 +3,49 @@ package net.ripe.db.whois.rdap.ipranges.administrative;
 import jakarta.annotation.PostConstruct;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Unmarshaller;
-import net.ripe.db.whois.common.profiles.WhoisProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Profile(WhoisProfile.DEPLOYED)
 public class IanaRangeXmlLoader {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IanaRangeXmlLoader.class);
     final private List<IanaRecord> ianaRecords = new ArrayList<>();
 
+    final private String ipv4Url;
+    final private String ipv6Url;
+
+    @Autowired
+    public IanaRangeXmlLoader(@Value("${ipv4.adminitrative.range:https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xml}") String ipv4Url,
+                              @Value("${ipv6.adminitrative.range:https://www.iana.org/assignments/ipv6-unicast-address-assignments/ipv6-unicast-address-assignments.xml}") String ipv6Url) {
+        this.ipv4Url = ipv4Url;
+        this.ipv6Url = ipv6Url;
+    }
+
     @PostConstruct
     public void init() {
-        loadDataFromXml("https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xml");
-        loadDataFromXml("https://www.iana.org/assignments/ipv6-unicast-address-assignments/ipv6-unicast-address-assignments.xml");
+        loadDataFromXml(ipv4Url);
+        loadDataFromXml(ipv6Url);
+
+        LOGGER.info("Loaded Iana Records");
+        ianaRecords.forEach(ianaRecord -> LOGGER.info(ianaRecord.getPrefix() + ":" + ianaRecord.getRdap().getServer()));
     }
 
     private void loadDataFromXml(final String url) {
         try {
 
-            try (final InputStream in = URI.create(url).toURL().openStream()) {
+            final ResourceLoader resourceLoader = new DefaultResourceLoader();
+
+            try (final InputStream in = resourceLoader.getResource(url).getInputStream()) {
                 final JAXBContext jaxbContext = JAXBContext.newInstance(IanaRegistry.class);
                 final Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
@@ -41,9 +56,6 @@ public class IanaRangeXmlLoader {
                 }
 
                 ianaRecords.addAll(registry.getRecords().stream().filter( ianaRecord -> ianaRecord.getRdap() != null).toList());
-
-                LOGGER.info("Loaded Iana Records");
-                ianaRecords.forEach(ianaRecord -> LOGGER.info(ianaRecord.getPrefix() + ":" + ianaRecord.getRdap().getServer()));
             }
 
         } catch (Exception e) {
