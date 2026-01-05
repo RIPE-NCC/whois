d5b7cc43f114e727ddbf1e0230a0dbe3ce11878b
Firdavs Murodov <fmurodov@ripe.net>
2026-01-05 11:49:23 +0100

    Update elasticsearch version for tests

--- whois-api/src/test/java/net/ripe/db/whois/api/elasticsearch/AbstractElasticSearchIntegrationTest.java
+++ whois-api/src/test/java/net/ripe/db/whois/api/elasticsearch/AbstractElasticSearchIntegrationTest.java
@@ -53,7 +53,7 @@ public abstract class AbstractElasticSearchIntegrationTest extends AbstractInteg
     public static void setUpElasticCluster() {
         if (StringUtils.isBlank(System.getProperty(ENV_DISABLE_TEST_CONTAINERS))) {
             if (elasticsearchContainer == null) {
-                elasticsearchContainer = new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.16.3");
+                elasticsearchContainer = new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.17.29");
                 elasticsearchContainer.start();
             }
 
